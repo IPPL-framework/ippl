bdfeeb0ba286e2a62f060308a2b5e65141a971b8
Matthias Frey <matthias.frey@psi.ch>
2020-09-30 10:23:02 +0200

    expression templates cross product

--- test/kokkos/TestVectorField.cpp
+++ test/kokkos/TestVectorField.cpp
@@ -1,41 +1,105 @@
 #include <Kokkos_Core.hpp>
 
+#include <initializer_list>
 #include <iostream>
+
+template <typename E>
+class VectorExpr {
+public:
+    double operator[](size_t i) const {
+        return static_cast<E const&>(*this)[i];
+    }
+};
 
 
 template <typename T, unsigned D>
-class Vector {
+class Vector : public VectorExpr<Vector<T, D>> {
 
 public:
-  KOKKOS_FUNCTION
-  Vector() : Vector(T(0)) {}
+    KOKKOS_FUNCTION
+    Vector() : Vector(T(0)) {}
 
-  KOKKOS_FUNCTION
-  Vector(T val) {
-    for (unsigned i = 0; i < D; ++i) {
-      data_m[i] = val;
+    KOKKOS_FUNCTION
+    Vector(T val) {
+        for (unsigned i = 0; i < D; ++i) {
+            data_m[i] = val;
+        }
     }
-  }
 
-  KOKKOS_FUNCTION
-  Vector& operator=(const Vector& rhs) {
-    for (unsigned i = 0; i < D; ++i) {
-      data_m[i] = rhs.data_m[i];
+    KOKKOS_FUNCTION
+    Vector(const std::initializer_list<T>& l) {
+        std::copy(l.begin(), l.end(), &data_m[0]);
     }
-    return *this;
-  }
 
-  KOKKOS_FUNCTION
-  T operator()(const int i) {
-    return data_m[i];
-  }
+    KOKKOS_FUNCTION
+    T operator[](const int i) const {
+        return data_m[i];
+    }
 
-  KOKKOS_FUNCTION
-  ~Vector() {}
+    KOKKOS_FUNCTION
+    T& operator[](const int i) {
+        return data_m[i];
+    }
+
+    KOKKOS_FUNCTION
+    ~Vector() {}
+
+
+    template <typename E>
+    KOKKOS_FUNCTION
+    Vector& operator=(VectorExpr<E> const& expr) {
+        for (unsigned i = 0; i < D; ++i) {
+            data_m[i] = expr[i];
+        }
+        return *this;
+    }
 
 private:
   T data_m[D];
 };
+
+
+template <typename E1, typename E2>
+class VecSum : public VectorExpr<VecSum<E1, E2> > {
+    E1 const& _u;
+    E2 const& _v;
+
+public:
+    VecSum(E1 const& u, E2 const& v) : _u(u), _v(v) { }
+    double operator[](size_t i) const { return _u[i] + _v[i]; }
+};
+
+
+
+template <typename E1, typename E2>
+VecSum<E1, E2>
+operator+(VectorExpr<E1> const& u, VectorExpr<E2> const& v) {
+   return VecSum<E1, E2>(*static_cast<const E1*>(&u), *static_cast<const E2*>(&v));
+}
+
+
+template<class T1, class T2> struct meta_cross {};
+
+template<class T1, class T2>
+struct meta_cross< Vector<T1, 3> , Vector<T2, 3> >
+{
+  inline static Vector<double, 3>
+  apply(const Vector<T1, 3>& a, const Vector<T2, 3>& b) {
+    Vector<double, 3> c;
+    c[0] = a[1]*b[2] - a[2]*b[1];
+    c[1] = a[2]*b[0] - a[0]*b[2];
+    c[2] = a[0]*b[1] - a[1]*b[0];
+    return c;
+  }
+};
+
+
+template < class T1, class T2, unsigned D >
+inline Vector<T1,D>
+cross(const Vector<T1,D> &lhs, const Vector<T2,D> &rhs)
+{
+  return meta_cross< Vector<T1,D> , Vector<T2,D> > :: apply(lhs,rhs);
+}
 
 
 
@@ -52,7 +116,19 @@ int main(int argc, char *argv[]) {
         vector_field_type vfield("vfield", length);
 
         Kokkos::parallel_for("assign", length, KOKKOS_LAMBDA(const int i) {
-            vfield(i) = vector_type(2.0);
+            vfield(i) = {1.0, 2.0, 3.0};
+        });
+
+
+        vector_field_type wfield("wfield", length);
+
+        Kokkos::parallel_for("assign", length, KOKKOS_LAMBDA(const int i) {
+            wfield(i) = {4.0, -5.0, 6.0};
+        });
+
+
+        Kokkos::parallel_for("assign", length, KOKKOS_LAMBDA(const int i) {
+            vfield(i) = cross(vfield(i), wfield(i)) + vfield(i);
         });
 
         Kokkos::fence();
@@ -62,7 +138,7 @@ int main(int argc, char *argv[]) {
 
 
         for (int i = 0; i < length; ++i) {
-            std::cout << host_view(i)(0) << " " << host_view(i)(1) << " " << host_view(i)(2) << std::endl;
+            std::cout << host_view(i)[0] << " " << host_view(i)[1] << " " << host_view(i)[2] << std::endl;
         }
     }
     Kokkos::finalize();
