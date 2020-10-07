53eff6486db35256045bb786e473a20044299f93
Matthias Frey <matthias.frey@psi.ch>
2020-10-07 16:20:04 +0200

    add Field test

---
+++ test/field/TestField.cpp
@@ -0,0 +1,31 @@
+#include "Ippl.h"
+
+#include <iostream>
+#include <typeinfo>
+
+int main(int argc, char *argv[]) {
+
+    Ippl ippl(argc,argv);
+
+    constexpr unsigned int dim = 3;
+
+
+    Index I(10);
+    NDIndex<dim> owned(I, I, I);
+
+    e_dim_tag allParallel[dim];    // Specifies SERIAL, PARALLEL dims
+    for (unsigned int d=0; d<dim; d++)
+        allParallel[d] = SERIAL;
+
+    // all parallel layout, standard domain, normal axis order
+    FieldLayout<dim> layout(owned,allParallel, 1);
+
+
+    typedef ippl::Kokkos_Field<double, dim> field_type;
+
+    field_type field(layout);
+
+
+
+    return 0;
+}
