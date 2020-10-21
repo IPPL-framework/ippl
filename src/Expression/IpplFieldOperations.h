7464a48911159b225a18c671da3449afc446d509
Matthias Frey <matthias.frey@psi.ch>
2020-10-21 16:45:13 +0200

    fix scalar-vector operations

--- src/Expression/IpplFieldOperations.h
+++ src/Expression/IpplFieldOperations.h
@@ -63,9 +63,9 @@ namespace ippl {
                                                                                 \
     template<typename E, typename T,                                            \
              typename = std::enable_if_t<std::is_scalar<T>::value>>             \
-    fun<E, detail::Scalar<T>> name(const T& u,                                  \
+    fun<detail::Scalar<T>, E> name(const T& u,                                  \
                                    const FieldExpression<E>& v) {               \
-        return fun<E, detail::Scalar<T>>(*static_cast<const E*>(&v), u);        \
+        return fun<detail::Scalar<T>, E>(u, *static_cast<const E*>(&v));        \
     }
 
 
