caf74c9ddf74755e3abd274b7ec895e319065127
Sriramkrishnan Muralikrishnan <sriramkrishnan.muralikrishnan@psi.ch>
2020-10-22 14:59:54 +0200

    Test file for non-trivial gradient added

---
+++ test/field/TestField3.cpp
@@ -0,0 +1,98 @@
+#include "Ippl.h"
+
+#include <iostream>
+#include <typeinfo>
+
+int main(int argc, char *argv[]) {
+
+    Ippl ippl(argc,argv);
+
+    constexpr unsigned int dim = 3;
+
+
+    int pt = 4;
+    Index I(pt);
+    NDIndex<dim> owned(I, I, I);
+
+    e_dim_tag allParallel[dim];    // Specifies SERIAL, PARALLEL dims
+    for (unsigned int d=0; d<dim; d++)
+        allParallel[d] = SERIAL;
+
+    // all parallel layout, standard domain, normal axis order
+    FieldLayout<dim> layout(owned,allParallel, 1);
+
+    //Unit box 
+    double dx = 1.0 / double(pt);
+    ippl::Vector<double, 3> hx = {dx, dx, dx};
+    ippl::Vector<double, 3> origin = {0, 0, 0};
+    ippl::UniformCartesian<double, 3> mesh(owned, hx, origin);
+
+    double pi = acos(-1.0);
+
+
+    typedef ippl::Field<double, dim> field_type;
+    typedef ippl::Field<ippl::Vector<double, dim>, dim> vector_field_type;
+
+    field_type field;
+    vector_field_type vfield;
+
+    field.initialize(mesh, layout);
+    
+    FieldLayout<dim> layout2(owned,allParallel, 1);
+
+    vfield.initialize(mesh, layout2);
+
+    typedef ippl::Field<double, dim> Field_t;
+
+    typename Field_t::LField_t::view_type view = field(0).getView();
+
+    typename Field_t::LField_t::view_type::HostMirror host_view = 
+                                                      Kokkos::create_mirror_view(view);
+
+    int length = pt+2;
+
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < length; ++j) {
+            for (int k = 0; k < length; ++k) {
+                    
+                double x = (i + 0.5) * hx[0] + origin[0];
+                double y = (j + 0.5) * hx[1] + origin[1];
+                double z = (k + 0.5) * hx[2] + origin[2];
+
+                host_view(i, j, k) = 3.0 * x + 4.0 * y + 5.0 * z;
+                //host_view(i, j, k) = sin(pi * x) * cos(pi * y) * exp(z);
+            }
+        }
+    }
+
+    Kokkos::deep_copy(view,host_view);
+
+
+    //Kokkos::parallel_for("Assign lfield", 
+    //                      Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0},
+    //                                                             {view.extent(0),view.extent(1),view.extent(2)}),
+    //                      KOKKOS_LAMBDA(const int i, const int j, const int k){
+    //        
+    //                        double x = (i + 0.5) * hx[0] + origin[0];
+    //                        double y = (j + 0.5) * hx[1] + origin[1];
+    //                        double z = (k + 0.5) * hx[2] + origin[2];
+
+    //                        view(i, j, k) = x + y + z;//sin(pi * x) * cos(pi * y) * exp(z);
+
+
+    //                      });
+
+
+
+    field.write();
+
+    vfield = grad(field);
+
+    //field = div(vfield);
+
+    vfield.write();
+
+
+
+    return 0;
+}
