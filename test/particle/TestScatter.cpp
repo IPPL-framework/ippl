8dfdd6f2f8deed5f33513d4f224112f8e05537da
Sriramkrishnan Muralikrishnan <sriramkrishnan.muralikrishnan@psi.ch>
2020-10-26 17:44:34 +0100

    summation including ghosts added

--- test/particle/TestScatter.cpp
+++ test/particle/TestScatter.cpp
@@ -77,7 +77,7 @@ int main(int argc, char *argv[]) {
     // Check charge conservation
     // We have not yet implemented periodic BCs so ghost layer charges are not
     // being summed up.
-    double Total_charge_field = field.sum();
+    double Total_charge_field = field.sum(1);
 
     std::cout << "Total charge in the field:" << Total_charge_field << std::endl;
     std::cout << "Total charge of the particles:" << n;
