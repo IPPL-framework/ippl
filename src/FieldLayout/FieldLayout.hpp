b9e347a48db9530bd531d717f9d25867e0ca4bcf
Matthias Frey <matthias.frey@psi.ch>
2020-11-18 08:47:01 +0100

    FieldLayout: clean up

--- src/FieldLayout/FieldLayout.hpp
+++ src/FieldLayout/FieldLayout.hpp
@@ -1,10 +1,9 @@
 //
 // Class FieldLayout
 //   FieldLayout describes how a given index space (represented by an NDIndex
-//   object) is distributed among MPI ranks (vnodes). It performs the initial
-//   partitioning, and stores a list of local and remote vnodes. The user may
-//   request that a particular dimension not be partitioned by flagging that
-//   axis as 'SERIAL' (instead of 'PARALLEL').
+//   object) is distributed among MPI ranks. It performs the initial
+//   partitioning. The user may request that a particular dimension not be
+//   partitioned by flagging that axis as 'SERIAL' (instead of 'PARALLEL').
 //
 // Copyright (c) 2020, Paul Scherrer Institut, Villigen PSI, Switzerland
 // All rights reserved
@@ -39,8 +38,10 @@ namespace ippl {
     : dLocalDomains_m("local domains (device)", 0)
     , hLocalDomains_m(Kokkos::create_mirror_view(dLocalDomains_m))
     {
-        for (unsigned int d = 0; d < Dim; ++d)
+        for (unsigned int d = 0; d < Dim; ++d) {
             requestedLayout_m[d] = PARALLEL;
+            minWidth_m[d] = 0;
+        }
     }
 
 
@@ -54,7 +55,6 @@ namespace ippl {
                                  e_dim_tag* userflags)
     {
         int nRanks = Ippl::Comm->size();
-//         int rank = Ippl::Comm->rank();
 
         gDomain_m = domain;
 
@@ -97,113 +97,50 @@ namespace ippl {
 
         Kokkos::deep_copy(dLocalDomains_m, hLocalDomains_m);
 
-        // Calculate the widths of all the vnodes
-//         calcWidths();
+        calcWidths();
     }
 
-//----------------------------------------------------------------------
-//
-// Completely repartition this FieldLayout and all of the Fields
-// defined on it.
-//
-template<unsigned Dim>
-void
-FieldLayout<Dim>::Repartition(const NDIndex<Dim>* /*idxBegin*/,
-			      const NDIndex<Dim>* /*idxEnd*/)
-{
-/*
 
-
-    // Build a temporary FieldLayout to declare the temporary arrays.
-    FieldLayout<Dim> tempLayout(Domain,idxBegin,idxEnd);
-
-    // Repartition each field.
-    iterator_if p, endp=end_if();
-    for (p=begin_if(); p!=endp; ++p) {
-        FieldLayoutUser *user = (FieldLayoutUser *)((*p).second);
-        user->Repartition(&tempLayout);
+    template<unsigned Dim>
+    const typename FieldLayout<Dim>::NDIndex_t&
+    FieldLayout<Dim>::getLocalNDIndex(int rank) const
+    {
+        return hLocalDomains_m(rank);
     }
 
-    // Copy back the layout information.
-    Local_ac = tempLayout.Local_ac;
-    Remotes_ac = tempLayout.Remotes_ac;
 
-    // Calculate the widths of all the vnodes
-    calcWidths();
+    template <unsigned Dim>
+    void FieldLayout<Dim>::write(std::ostream& out) const
+    {
+        if (Ippl::Comm->rank() > 0) {
+            return;
+        }
 
-    //INCIPPLSTAT(incRepartitions);*/
-}
+        out << "Domain = " << gDomain_m << "\n"
+            << "Total number of boxes = " << hLocalDomains_m.size() << "\n";
 
-
-//----------------------------------------------------------------------
-//
-// calculate the minimum vnode sizes in each dimension
-template<unsigned Dim>
-void FieldLayout<Dim>::calcWidths() {
-//     unsigned int d;
-//
-//     // initialize widths first
-//     for (d=0; d < Dim; ++d)
-//         MinWidth[d] = getDomain()[d].length();
-//
-//     // look for minimum width in local vnodes
-//     for  (const_iterator_iv v_i = begin_iv() ; v_i != end_iv(); ++v_i) {
-//         const NDIndex<Dim> &dom = (*v_i).second->getDomain();
-//         for (d=0; d < Dim; ++d) {
-//             if ((unsigned int) dom[d].length() < MinWidth[d])
-//                 MinWidth[d] = dom[d].length();
-//         }
-//     }
-//
-//     // look for minimum width in remove vnodes
-//     ac_domain_vnodes *v_ac = Remotes_ac[ gc0() ].get();
-//     for (iterator_dv dv_i = v_ac->begin(); dv_i != v_ac->end(); ++ dv_i) {
-//         const NDIndex<Dim> &dom = (*dv_i).first;
-//         for (d=0; d < Dim; ++d) {
-//             if ((unsigned int) dom[d].length() < MinWidth[d])
-//                 MinWidth[d] = dom[d].length();
-//         }
-//     }
-}
+        using size_type = typename host_mirror_type::size_type;
+        for (size_type i = 0; i < hLocalDomains_m.size(); ++i) {
+            out << "    Box " << i << " " << hLocalDomains_m(i) << "\n";
+        }
+    }
 
 
-template<unsigned Dim>
-const typename FieldLayout<Dim>::NDIndex_t&
-FieldLayout<Dim>::getLocalNDIndex(int rank) const
-{
-    return hLocalDomains_m(rank);
-}
+    template<unsigned Dim>
+    void FieldLayout<Dim>::calcWidths()
+    {
+        // initialize widths first
+        for (unsigned int d = 0; d < Dim; ++d) {
+            minWidth_m[d] = gDomain_m[d].length();
+        }
 
-
-template<unsigned Dim>
-void FieldLayout<Dim>::write(std::ostream& out) const
-{
-    Kokkos::fence();
-    detail::write<NDIndex_t>(dLocalDomains_m, out);
-/*
-    int icount;
-
-    the whole domain, and the number of users
-    out << "Domain = " << Domain << "\n";
-//     out << "FieldLayoutUsers = " << size_if() << "\n";
-
-    iterate over the local vnodes and print them out.
-//     out << "Total number of vnodes = " << numVnodes() << std::endl;
-//     out << "Local Vnodes = " << Local_ac.size() << "\n";
-    icount = 0;
-    tjw can now do operator<<() with whole Vnode objects
-    tjw  for(const_iterator_iv v_i = begin_iv() ; v_i != end_iv(); ++v_i)
-    tjw    out << " vnode " << icount++ <<" : "<< (*v_i).second->getDomain() << "\n";
-    for(const_iterator_iv v_i = begin_iv() ; v_i != end_iv(); ++v_i)
-        out << " vnode " << icount++ << ": " << *((*v_i).second) << std::endl;
-
-    iterate over the remote vnodes and print them out.
-    ac_domain_vnodes *v_ac = Remotes_ac[ gc0() ].get();
-    out << "Remote Vnodes = " << v_ac->size() << "\n";
-    icount = 0;
-    for (iterator_dv dv_i = v_ac->begin(); dv_i != v_ac->end(); ++ dv_i)
-        out << " vnode " << icount++ << " : " << *((*dv_i).second) << std::endl;
-}*/
-
-}
+        using size_type = typename host_mirror_type::size_type;
+        for (size_type i = 0; i < hLocalDomains_m.size(); ++i) {
+            const NDIndex_t &dom = hLocalDomains_m(i);
+            for (unsigned int d = 0; d < Dim; ++d) {
+                if ((unsigned int) dom[d].length() < minWidth_m[d])
+                    minWidth_m[d] = dom[d].length();
+            }
+        }
+    }
 }
