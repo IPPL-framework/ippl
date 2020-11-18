62d2b0fadd1347ed694f66514d1f2228b671418b
Matthias Frey <matthias.frey@psi.ch>
2020-11-18 09:27:41 +0100

    fix function call

--- test/particle/TestParticleSpatialLayout.cpp
+++ test/particle/TestParticleSpatialLayout.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     for (unsigned int d=0; d<dim; d++)
         allParallel[d] = ippl::SERIAL;
 
-    ippl::FieldLayout<dim> layout(owned,allParallel, 1);
+    ippl::FieldLayout<dim> layout(owned,allParallel);
 
     double dx = 1.0 / double(pt);
     ippl::Vector<double, 3> hx = {dx, dx, dx};
