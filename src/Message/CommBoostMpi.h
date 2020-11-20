9eca91eff1889aa3bd76222be2a0fcf2c7184939
Matthias Frey <matthias.frey@psi.ch>
2020-11-20 15:59:49 +0100

    add warning comment

--- src/Message/CommBoostMpi.h
+++ src/Message/CommBoostMpi.h
@@ -81,48 +81,40 @@ namespace ippl {
     };
 
 
+    /*!
+     * \warning Only works with default spaces!
+     */
     template <class Buffer>
-	void Communicate::send(int dest, int tag, Buffer& buffer)
+    void Communicate::send(int dest, int tag, Buffer& buffer)
     {
-	detail::Archive<> ar;
+        // Attention: only works with default spaces
+        detail::Archive<> ar;
 
-	buffer.serialize(ar);
+        buffer.serialize(ar);
         MPI_Send(ar.getBuffer(), ar.getSize(),
                  MPI_BYTE, dest, tag, MPI_COMM_WORLD);
-	
-//         buffer.serialize(ar);
-//         this->send(dest, tag, ar.getBuffer(), ar.getSize());
     }
 
 
+    /*!
+     * \warning Only works with default spaces!
+     */
     template <class Buffer>
-	void Communicate::recv(int src, int tag, Buffer& buffer)
+    void Communicate::recv(int src, int tag, Buffer& buffer)
     {
-	MPI_Status status;
+        MPI_Status status;
 
         MPI_Probe(src, tag, MPI_COMM_WORLD, &status);
 
         int msize = 0;
         MPI_Get_count(&status, MPI_BYTE, &msize);
 
+        // Attention: only works with default spaces
         detail::Archive<> ar(msize);
 
         MPI_Recv(ar.getBuffer(), ar.getSize(),
                 MPI_BYTE, src, tag, MPI_COMM_WORLD, &status);
 
-
-
-    //         boost::mpi::status status = this->probe(src, tag);
-//
-//         detail::Archive ar;
-//
-// //         if (msg.source() != src) {
-//
-// //         }
-//
-//         std::cout << "count = " << status.count() << std::endl;
-//         this->recv(src, tag, ar.getBuffer(), status.count);
-//
         buffer.deserialize(ar);
     }
 }
