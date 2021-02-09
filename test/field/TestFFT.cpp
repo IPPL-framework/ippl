f7a73f93ae45b20bd04e23448cdb33a36ce1b463
Sriramkrishnan Muralikrishnan <sriramkrishnan.muralikrishnan@psi.ch>
2021-02-09 11:56:19 +0100

    Test script for FFT added

---
+++ test/field/TestFFT.cpp
@@ -0,0 +1,99 @@
+#include "Ippl.h"
+
+#include <iostream>
+#include <typeinfo>
+#include <array>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+
+    Ippl ippl(argc,argv);
+
+    constexpr unsigned int dim = 3;
+
+    std::array<int, dim> pt = {8, 8, 8};
+    ippl::Index I(pt[0]);
+    ippl::Index J(pt[1]);
+    ippl::Index K(pt[2]);
+    ippl::NDIndex<dim> owned(I, J, K);
+
+    ippl::e_dim_tag allParallel[dim];    // Specifies SERIAL, PARALLEL dims
+    for (unsigned int d=0; d<dim; d++)
+        allParallel[d] = ippl::PARALLEL;
+
+    ippl::FieldLayout<dim> layout(owned, allParallel);
+
+    std::array<double, dim> dx = {
+        1.0 / double(pt[0]),
+        1.0 / double(pt[1]),
+        1.0 / double(pt[2]),
+    };
+    ippl::Vector<double, 3> hx = {dx[0], dx[1], dx[2]};
+    ippl::Vector<double, 3> origin = {0, 0, 0};
+    ippl::UniformCartesian<double, 3> mesh(owned, hx, origin);
+
+    typedef ippl::Field<Kokkos::complex<double>, dim> field_type;
+
+    field_type field(mesh, layout);
+
+    ippl::HeffteParams fftParams;
+
+    fftParams.setAlltoAll( true );
+    fftParams.setPencils( true );
+    fftParams.setReorder( true );
+
+    typedef FFT<CCTransform, 3, double> FFT_type;
+
+    std::unique_ptr<FFT_type> fft;
+
+    fft = std::make_unique<FFT_type>(layout, fftParams);
+
+    typename field_type::view_type& view = field.getView();    
+    typename field_type::HostMirror field_host = field.getHostMirror();
+
+    const int nghost = field.getNghost();
+
+    for (int i = nghost; i < view.extent(0) - nghost; ++i) {
+        for (int j = nghost; j < view.extent(1) - nghost; ++j) {
+            for (int k = nghost; k < view.extent(2) - nghost; ++k) {
+    
+                field_host(i, j, k).real() = 1.0; 
+                field_host(i, j, k).imag() = 1.0; 
+                              
+            }
+        }
+    }
+
+    Kokkos::deep_copy(view, field_host);
+
+    //Forward transform
+    fft->transform(1, field);
+    //Reverse transform
+    fft->transform(-1, field);
+    
+    auto field_result = Kokkos::create_mirror_view_and_copy( Kokkos::HostSpace(), field.getView() );
+
+    std::complex<double> max_error_local(0.0, 0.0);
+    for (int i = nghost; i < view.extent(0) - nghost; ++i) {
+        for (int j = nghost; j < view.extent(1) - nghost; ++j) {
+            for (int k = nghost; k < view.extent(2) - nghost; ++k) {
+    
+                double error_real = std::fabs(field_host(i, j, k).real() - field_result(i, j, k).real());
+                double error_imag = std::fabs(field_host(i, j, k).imag() - field_result(i, j, k).imag());
+
+                if(error_real > max_error_local.real()) max_error_local.real() = error_real;
+                if(error_imag > max_error_local.imag()) max_error_local.imag() = error_imag;
+                              
+            }
+        }
+    }
+
+    std::complex<double> max_error(0.0, 0.0);
+    MPI_Reduce(&max_error_local, &max_error, 1, 
+                    MPI_C_DOUBLE_COMPLEX, MPI_MAX, 0, Ippl::getComm());
+
+    if(Ippl::Comm->rank() == 0) {
+        std::cout << "Max. error " << std::setprecision(16) << max_error << std::endl;
+    }
+    return 0;
+}
