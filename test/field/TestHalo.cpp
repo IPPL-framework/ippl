537b500b054de3c05ef45d55db1f63b68afe88c9
Sriramkrishnan Muralikrishnan <sriramkrishnan.muralikrishnan@psi.ch>
2021-03-01 14:37:39 +0100

    Master merged and halo exchange is being checked

--- test/field/TestHalo.cpp
+++ test/field/TestHalo.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     constexpr unsigned int dim = 3;
 
 //     std::array<int, dim> pt = {8, 7, 13};
-    std::array<int, dim> pt = {8, 8, 8};
+    std::array<int, dim> pt = {2, 2, 2};
     ippl::Index I(pt[0]);
     ippl::Index J(pt[1]);
     ippl::Index K(pt[2]);
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]) {
     for (unsigned int d=0; d<dim; d++)
         allParallel[d] = ippl::PARALLEL;
 
+    typedef ippl::FieldLayout<dim> Layout_t;
     ippl::FieldLayout<dim> layout(owned, allParallel);
 
     std::array<double, dim> dx = {
@@ -39,6 +40,48 @@ int main(int argc, char *argv[]) {
     field_type field(mesh, layout);
 
     field = Ippl::Comm->rank();
+    int myRank = Ippl::Comm->rank();
+    int nRanks = Ippl::Comm->size();
+
+    
+    for (int rank = 0; rank < nRanks; ++rank) {
+        if (rank == Ippl::Comm->rank()) {
+            using face_neighbor_type = typename Layout_t::face_neighbor_type;
+            const face_neighbor_type& face_neighbors = layout.getFaceNeighbors();
+            
+            for (size_t face = 0; face < face_neighbors.size(); ++face) {
+                for (size_t i = 0; i < face_neighbors[face].size(); ++i) {
+
+                    int rank = face_neighbors[face][i];
+                    std::cout << "My Rank: " << myRank  
+                              << "face: " << face 
+                              << "neighbor rank: " << rank << std::endl;
+                }
+            }
+            
+            using edge_neighbor_type = typename Layout_t::edge_neighbor_type;
+            const edge_neighbor_type& edge_neighbors = layout.getEdgeNeighbors();
+            for (size_t edge = 0; edge < edge_neighbors.size(); ++edge) {
+                for (size_t i = 0; i < edge_neighbors[edge].size(); ++i) {
+
+                    int rank = edge_neighbors[edge][i];
+                    std::cout << "My Rank: " << myRank  
+                              << "edge: " << edge 
+                              << "neighbor rank: " << rank << std::endl;
+                }
+            }
+            using vertex_neighbor_type = typename Layout_t::vertex_neighbor_type;
+            const vertex_neighbor_type& vertex_neighbors = layout.getVertexNeighbors();
+            for (size_t vertex = 0; vertex < vertex_neighbors.size(); ++vertex) {
+
+                    int rank = vertex_neighbors[vertex];
+                    std::cout << "My Rank: " << myRank  
+                              << "vertex: " << vertex 
+                              << "neighbor rank: " << rank << std::endl;
+            }
+        }
+        Ippl::Comm->barrier();
+    }
 
 //     auto& domains = layout.getHostLocalDomains();
 //
@@ -85,7 +128,6 @@ int main(int argc, char *argv[]) {
 //
 //     field.fillLocalHalo(10.0);
 //
-    int nRanks = Ippl::Comm->size();
 
     for (int rank = 0; rank < nRanks; ++rank) {
         if (rank == Ippl::Comm->rank()) {
