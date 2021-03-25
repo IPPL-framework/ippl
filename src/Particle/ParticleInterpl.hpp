ae6f23d772e55792bbd5dcbb8b21b623b5f3ee06
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-25 18:33:05 +0100

    add file header; add NGP methods

--- src/Particle/ParticleInterpl.hpp
+++ src/Particle/ParticleInterpl.hpp
@@ -1,3 +1,23 @@
+//
+// File ParticleInterpl
+//   Global scatter and gather functions. These functions call the appropriate
+//   ParticleAttrib member functions. A user doesn't need to specify the scatter/gather
+//   method, in this case the cloud-in-cell (CIC) is used.
+//
+//
+// Copyright (c) 2021, Matthias Frey, University of St Andrews, St Andrews, Scotland
+// All rights reserved
+//
+// This file is part of IPPL.
+//
+// IPPL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with IPPL. If not, see <https://www.gnu.org/licenses/>.
+//
 #ifndef IPPL_PARTICLE_INTERPL
 #define IPPL_PARTICLE_INTERPL
 
@@ -24,6 +44,21 @@ namespace ippl {
                  const ParticleAttrib<Vector<P2, Dim>, Properties...>& pp)
     {
         attrib.scatterCIC(f, pp);
+    }
+
+    template <Interpl_t I,
+              typename P1,
+              unsigned Dim,
+              class M,
+              class C,
+              typename P2,
+              class... Properties,
+              std::enable_if_t<I == NGP_t, bool> = true>
+    inline
+    void scatter(const ParticleAttrib<P1, Properties...>& attrib, Field<P1, Dim, M, C>& f,
+                 const ParticleAttrib<Vector<P2, Dim>, Properties...>& pp)
+    {
+        attrib.scatterNGP(f, pp);
     }
 
     template <typename P1,
@@ -53,6 +88,22 @@ namespace ippl {
                 const ParticleAttrib<Vector<P2, Dim>, Properties...>& pp)
     {
         attrib.gatherCIC(f, pp);
+    }
+
+
+    template <Interpl_t I,
+              typename P1,
+              unsigned Dim,
+              class M,
+              class C,
+              typename P2,
+              class... Properties,
+              std::enable_if_t<I == NGP_t, bool> = true>
+    inline
+    void gather(ParticleAttrib<P1, Properties...>& attrib, Field<P1, Dim, M, C>& f,
+                const ParticleAttrib<Vector<P2, Dim>, Properties...>& pp)
+    {
+        attrib.gatherNGP(f, pp);
     }
 
     template<typename P1,
