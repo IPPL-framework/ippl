c293224808005ce70a4503b2c9af928a019c7970
Matthias Frey <mf248@st-andrews.ac.uk>
2021-03-27 13:04:11 +0100

    UniformCartesian: cleanup; add unit test

--- src/Meshes/UniformCartesian.h
+++ src/Meshes/UniformCartesian.h
@@ -28,89 +28,20 @@ class UniformCartesian : public Mesh<T, Dim> {
 
 public:
     typedef typename Mesh<T, Dim>::vector_type vector_type;
-    typedef BareField<vector_type, Dim> BareField_t;
-
     typedef Cell DefaultCentering;
 
 
     UniformCartesian();
 
-    // Non-default constructors
-    UniformCartesian(const NDIndex<Dim>& ndi,
-                     bool evalCellVolume = true);
-
-    UniformCartesian(const NDIndex<Dim>& ndi,
-                     const vector_type& hx);
-
     UniformCartesian(const NDIndex<Dim>& ndi,
                      const vector_type& hx,
                      const vector_type& origin);
 
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    UniformCartesian(const Indices&... indices,
-                     bool evalCellVolume = true);
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    UniformCartesian(const Indices&... indices,
-                     const vector_type& hx);
-
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    UniformCartesian(const Indices&... indices,
-                     const vector_type& hx,
-                     const vector_type& origin);
 
     ~UniformCartesian() = default;
 
-
-    // initialize functions
-    void initialize(const NDIndex<Dim>& ndi);
-    void initialize(const NDIndex<Dim>& ndi, const vector_type& hx);
-    void initialize(const NDIndex<Dim>& ndi, const vector_type& hx,
-                    const vector_type& origin);
-
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    void initialize(const Indices&... indices);
-
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    void initialize(const Indices&... indices, const vector_type& hx);
-
-
-    template <class... Indices,
-              std::enable_if_t<
-                std::conjunction<
-                    std::is_same<Index, Indices>...>::value
-                >
-             >
-    void initialize(const Indices&... indices, const vector_type& hx,
+    void initialize(const NDIndex<Dim>& ndi,
+                    const vector_type& hx,
                     const vector_type& origin);
 
 
@@ -118,10 +49,6 @@ public:
     T getMeshSpacing(unsigned dim) const;
 
     const vector_type& getMeshSpacing() const;
-
-    // Set the spacings of mesh vertex positions (recompute Dvc, cell volume):
-    void setMeshSpacing(const vector_type& meshSpacing);
-
 
     T getCellVolume() const;
 
@@ -143,90 +70,9 @@ public:
     }
 
 private:
-    UniformCartesian(std::initializer_list<Index> indices,
-                     bool evalCellVolume);
-
-    UniformCartesian(std::initializer_list<Index> indices,
-                     const vector_type& hx);
-
-    UniformCartesian(std::initializer_list<Index> indices,
-                     const vector_type& hx,
-                     const vector_type& origin);
-
-    void initialize(std::initializer_list<Index> indices);
-
-    void initialize(std::initializer_list<Index> indices,
-                    const vector_type& hx);
-
-
     vector_type meshSpacing_m;     // delta-x, delta-y (>1D), delta-z (>2D)
-    T volume_m;                     // Cell length(1D), area(2D), or volume (>2D)
-
-
-
-
-
-  std::shared_ptr<FieldLayout<Dim>> FlCell;  // Layouts for BareField* CellSpacings
-  std::shared_ptr<FieldLayout<Dim>> FlVert;  // Layouts for BareField* VertSpacings
-
-
-
-  // Set only the derivative constants, using pre-set spacings:
-  void set_Dvc();
-
-  void updateCellVolume_m();
-
-  void setup_m();
-
-public:
-
-  // Public member data:
-  vector_type Dvc[1<<Dim]; // Constants for derivatives.
-  bool hasSpacingFields_m;              // Flags allocation of the following:
-  std::shared_ptr<BareField_t> VertSpacings;
-  std::shared_ptr<BareField_t> CellSpacings;
-
-  // Public member functions:
-
-  // Create BareField's of vertex and cell spacings; allow for specifying
-  // layouts via the FieldLayout e_dim_tag and vnodes parameters (these
-  // get passed in to construct the FieldLayout used to construct the
-  // BareField's).
-  void storeSpacingFields(); // Default; will have default layout
-
-  // Next we have one for arbitrary dimension, ala FieldLayout ctor:
-  // All the others call this one internally:
-  void storeSpacingFields(e_dim_tag *p);
-
-  // Formatted output of UniformCartesian object:
-  void print(std::ostream&);
-
-  void print(Inform &);
-
+    T volume_m;                    // Cell length(1D), area(2D), or volume (>2D)
 };
-
-// // I/O
-//
-// // Stream formatted output of UniformCartesian object:
-// template< unsigned Dim, class T >
-// inline
-// std::ostream& operator<<(std::ostream& out, const UniformCartesian<Dim,T>& mesh)
-// {
-//   UniformCartesian<Dim,T>& ncmesh =
-//     const_cast<UniformCartesian<Dim,T>&>(mesh);
-//   ncmesh.print(out);
-//   return out;
-// }
-//
-// template< unsigned Dim, class T >
-// inline
-// Inform& operator<<(Inform& out, const UniformCartesian<Dim,T>& mesh)
-// {
-//   UniformCartesian<Dim,T>& ncmesh =
-//     const_cast<UniformCartesian<Dim,T>&>(mesh);
-//   ncmesh.print(out);
-//   return out;
-// }
 
 }
 
