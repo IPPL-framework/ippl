3158e3bb25bcf104de7a714fcf19c5547955aea4
Sriramkrishnan Muralikrishnan <sriramkrishnan.muralikrishnan@psi.ch>
2021-08-04 12:26:38 +0200

    serialize test cases removed

--- test/particle/UniformPlasmaTest.cpp
+++ test/particle/UniformPlasmaTest.cpp
@@ -466,7 +466,7 @@ int main(int argc, char *argv[]){
     Inform msg("PenningTrap");
     Inform msg2all(argv[0],INFORM_ALL_NODES);
 
-    Ippl::Comm->setDefaultOverallocation(2);
+    Ippl::Comm->setDefaultOverallocation(1);
 
 
     auto start = std::chrono::high_resolution_clock::now();
