aedfe9e919f4c06f424bee966fbcf5657fe826d6
mayani_s <sonali.mayani@psi.ch>
2022-09-16 16:30:33 +0200

    change header

--- test/field/TestCurl.cpp
+++ test/field/TestCurl.cpp
@@ -1,9 +1,29 @@
-// This program tests the Curl operator on a vector field.
-// The problem size can be given by the user (N^3), and a bool (0 or 1)
-// indicates whether the vector field is A=(xyz, xyz, xyz) or a Gaussian 
-// field in all three dimensions. 
-// Usage:
-//   srun ./TestCurl N 0 --info 10
+//
+////
+//// TestCurl.cpp
+////   This program tests the Curl operator on a vector field.
+////   The problem size can be given by the user (N^3), and a bool (0 or 1)
+////   indicates whether the vector field is A=(xyz, xyz, xyz) or a Gaussian 
+////   field in all three dimensions. 
+////
+//// Usage:
+////   srun ./TestCurl N 0 --info 10
+////
+//// Copyright (c) 2022, Sonali Mayani,
+//// Paul Scherrer Institut, Villigen, Switzerland
+//// All rights reserved
+////
+//// This file is part of IPPL.
+////
+//// IPPL is free software: you can redistribute it and/or modify
+//// it under the terms of the GNU General Public License as published by
+//// the Free Software Foundation, either version 3 of the License, or
+//// (at your option) any later version.
+////
+//// You should have received a copy of the GNU General Public License
+//// along with IPPL. If not, see <https://www.gnu.org/licenses/>.
+////
+//
 
 #include "Ippl.h"
 
