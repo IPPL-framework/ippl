0a942fa1983258992caa9e1a199966a92ac26bb6
klappr_s <severin.klapproth@psi.ch>
2022-12-05 13:58:07 +0000

    Update run.sh

--- alpine/run.sh
+++ alpine/run.sh
@@ -1,14 +1,14 @@
 KE=3.05e9
 NM=64
-NV=32
+NV=16
 NP=156055
-vmax=1e7
-COLLISION=0
+VMAX=4e7
+COLLISION=1
 # QE=1
 # ME=1
 
 srun ./Langevin FFT 1.0 2.0 \
-${NM} 0.001774 0.01 ${NP} 0.0 1e6 2.15623e-13 1000 1 1 1.0 1  ${KE} ${NV} 4e7 \
+${NM} 0.001774 0.01 ${NP} 0.0 1e6 2.15623e-13 1000 1 1 1.0 1  ${KE} ${NV} ${VMAX} \
 0 1 1 1 1 1 1 ${COLLISION}   --info 26   1>data/langevin.out 2>data/langevin.err 
 
 
