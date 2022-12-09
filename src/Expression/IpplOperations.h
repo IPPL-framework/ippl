6ef771977eda29d0b19225160f9814fcd9469856
mayani_s <sonali.mayani@psi.ch>
2022-12-09 16:43:28 +0100

    remove pow, causes errors

--- src/Expression/IpplOperations.h
+++ src/Expression/IpplOperations.h
@@ -432,17 +432,17 @@ namespace ippl {
             auto operator()(size_t i, size_t j, size_t k) const {
                 vector_type row_1, row_2, row_3;
 
-                row_1 = xvector_m * ((u_m(i+1,j,k) - 2.0*u_m(i,j,k) + u_m(i-1,j,k))/pow(hvector_m[0], 2.0)) +
+                row_1 = xvector_m * ((u_m(i+1,j,k) - 2.0*u_m(i,j,k) + u_m(i-1,j,k))/(hvector_m[0]*hvector_m[0])) +
                         yvector_m * ((u_m(i+1,j+1,k) - u_m(i-1,j+1,k) - u_m(i+1,j-1,k) + u_m(i-1,j-1,k))/(4.0*hvector_m[0]*hvector_m[1])) +
                         zvector_m * ((u_m(i+1,j,k+1) - u_m(i-1,j,k+1) - u_m(i+1,j,k-1) + u_m(i-1,j,k-1))/(4.0*hvector_m[0]*hvector_m[2]));
 
                 row_2 = xvector_m * ((u_m(i+1,j+1,k) - u_m(i+1,j-1,k) - u_m(i-1,j+1,k) + u_m(i-1,j-1,k))/(4.0*hvector_m[1]*hvector_m[0])) +
-                        yvector_m * ((u_m(i,j+1,k) - 2.0*u_m(i,j,k) + u_m(i,j-1,k))/pow(hvector_m[1], 2.0)) +
+                        yvector_m * ((u_m(i,j+1,k) - 2.0*u_m(i,j,k) + u_m(i,j-1,k))/(hvector_m[1]*hvector_m[1])) +
                         zvector_m * ((u_m(i,j+1,k+1) - u_m(i,j-1,k+1) - u_m(i,j+1,k-1) + u_m(i,j-1,k-1))/(4.0*hvector_m[1]*hvector_m[2]));
 
                 row_3 = xvector_m * ((u_m(i+1,j,k+1) - u_m(i+1,j,k-1) - u_m(i-1,j,k+1) + u_m(i-1,j,k-1))/(4.0*hvector_m[2]*hvector_m[0])) +
                         yvector_m * ((u_m(i,j+1,k+1) - u_m(i,j+1,k-1) - u_m(i,j-1,k+1) + u_m(i,j-1,k-1))/(4.0*hvector_m[2]*hvector_m[1])) +
-                        zvector_m * ((u_m(i,j,k+1) - 2.0*u_m(i,j,k) + u_m(i,j,k-1))/pow(hvector_m[2], 2.0));
+                        zvector_m * ((u_m(i,j,k+1) - 2.0*u_m(i,j,k) + u_m(i,j,k-1))/(hvector_m[2]*hvector_m[2]));
 
                 return xvector_m * row_1 +
                        yvector_m * row_2 +
