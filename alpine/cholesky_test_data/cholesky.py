7f91229b48dcb70a72f1f5d4b0b88f637f045e9a
Klapproth Severin <klappr_s@merlin-l-001.psi.ch>
2023-01-06 01:03:56 +0100

    cholesky test

---
+++ alpine/cholesky_test_data/cholesky.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random
+
+
+with open("results.csv", "w") as f:
+
+    random.seed(0)
+
+    for i in range(-10, 10, 1):
+        for j in range(10):
+
+            A = np.random.rand(3, 3)
+            B = np.dot(A, A.transpose()) #semi positive definite
+            B = pow(10, i)*B
+            L = np.linalg.cholesky(B)
+
+            np.savetxt(f, np.reshape(B, (1,9)), fmt='%e', delimiter=' ')
+            np.savetxt(f, np.reshape(L, (1,9)), fmt='%e', delimiter=' ')
