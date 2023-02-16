6e2eb78881ddd200752abe3e18ae86c7097cde12
Klapproth Severin <klappr_s@merlin-l-001.psi.ch>
2023-02-16 14:24:01 +0100

    minor improvements to cholesky test

--- alpine/cholesky.cpp
+++ alpine/cholesky.cpp
@@ -27,7 +27,7 @@ Matrix_t choleskyb( V& d0, V& d1, V& d2){
         
         Matrix_t LL;
         V* D[] = {&d0, &d1, &d2}; 
-        double epszero = 1e-20; // ??
+        double epszero = __DBL_EPSILON__;
         
         assert(     (fabs((*D[0])(1)-(*D[1])(0))) < epszero &&
                     (fabs((*D[0])(2)-(*D[2])(0))) < epszero &&
@@ -35,53 +35,68 @@ Matrix_t choleskyb( V& d0, V& d1, V& d2){
         );
 
         auto finish_LL = [&](const unsigned i0, const unsigned i1, const unsigned i2){
-                // LL(0)(0) = sqrt(*D[i0])(i0));
-                // LL(0)(1) = *D[i0])(i1)/LL(i0)(i0);
+                LL(0)(0) = sqrt((*D[i0])(i0)); //
+                LL(0)(1) = (*D[i0])(i1)/LL(i0)(i0); //
                 LL(i0)(i2) = (*D[i0])(i2)/LL(i0)(i0);
                 LL(i1)(i0) = 0.0;
-                // LL(1)(1) = sqrt(*D[i1])(i1)- pow(LL(i0)(i1), 2));
+                LL(1)(1) = sqrt((*D[i1])(i1)- pow(LL(i0)(i1), 2)); //
                 LL(i1)(i2) = ((*D[i1])(i2) - LL(i0)(i1)*LL(i0)(i2))/LL(i1)(i1);
                 LL(i2)(i0) = 0.0;
                 LL(i2)(i1) = 0.0;
                 LL(i2)(i2) = sqrt( (*D[i2])(i2) - pow(LL(i0)(i2), 2) - pow(LL(i1)(i2), 2)) ;
         };
 
-        auto get_2_diag = [&](const unsigned i0, const unsigned i1){//, const unsigned i2){
-            return sqrt((*D[i1])(i1)- pow( LL(i0)(i1)=(*D[i0])(i1)/LL(i0)(i0), 2));
-        };
+        // auto get_2_diag = [&](const unsigned i0, const unsigned i1){//, const unsigned i2){
+            // return sqrt((*D[i1])(i1)- pow( LL(i0)(i1)=(*D[i0])(i1)/LL(i0)(i0), 2));
+        // };
 
+            // if     (( epszero <=(LL(0)(0)=sqrt(d0(0)))     )&&(    epszero <= ( LL(1)(1) = get_2_diag(0,1/*,2*/) )       ))finish_LL(0, 1, 2);
+            // // else if(( epszero <= LL(0)(0)                  )&&(    epszero <= ( LL(2)(2) = get_2_diag(0,2/*,1*/) )       ))finish_LL(0, 2, 1);
+            // // else if(( epszero <=(LL(1)(1)=sqrt(d1(1)))     )&&(    epszero <= ( LL(0)(0) = get_2_diag(1,0/*,2*/) )       ))finish_LL(1, 0, 2);
+            // // else if(( epszero <= LL(1)(1)                  )&&(    epszero <= ( LL(2)(2) = get_2_diag(1,2/*,0*/) )       ))finish_LL(1, 2, 0);
+            // // else if(( epszero <=(LL(2)(2)=sqrt(d2(2)))     )&&(    epszero <= ( LL(1)(1) = get_2_diag(2,1/*,0*/) )       ))finish_LL(2, 1, 0);
+            // // else if(( epszero <= LL(2)(2)                  )&&(    epszero <= ( LL(0)(0) = get_2_diag(2,0/*,1*/) )       ))finish_LL(2, 0, 1);
+            // else{
+            //     // how often
+            //     assert(false && "no cholesky decomposition possible for at least one particle");
+            //     for(unsigned di = 0; di<Dim; ++di)
+            //     for(unsigned dj = 0; dj<Dim; ++dj){
+            //         LL(di)(dj)=0.0;
+            //     }
+            // }
 
-
-            if     (( epszero <=(LL(0)(0)=sqrt(d0(0)))     )&&(    epszero <= ( LL(1)(1) = get_2_diag(0,1/*,2*/) )       ))finish_LL(0, 1, 2);
-            // else if(( epszero <= LL(0)(0)                  )&&(    epszero <= ( LL(2)(2) = get_2_diag(0,2/*,1*/) )       ))finish_LL(0, 2, 1);
-            // else if(( epszero <=(LL(1)(1)=sqrt(d1(1)))     )&&(    epszero <= ( LL(0)(0) = get_2_diag(1,0/*,2*/) )       ))finish_LL(1, 0, 2);
-            // else if(( epszero <= LL(1)(1)                  )&&(    epszero <= ( LL(2)(2) = get_2_diag(1,2/*,0*/) )       ))finish_LL(1, 2, 0);
-            // else if(( epszero <=(LL(2)(2)=sqrt(d2(2)))     )&&(    epszero <= ( LL(1)(1) = get_2_diag(2,1/*,0*/) )       ))finish_LL(2, 1, 0);
-            // else if(( epszero <= LL(2)(2)                  )&&(    epszero <= ( LL(0)(0) = get_2_diag(2,0/*,1*/) )       ))finish_LL(2, 0, 1);
-            else{
-                // how often
-                // assert(false && "no cholesky decomposition possible for at least one particle");
-                for(unsigned di = 0; di<Dim; ++di)
-                for(unsigned dj = 0; dj<Dim; ++dj){
-                    LL(di)(dj)=0.0;
-                }
-            }
+        finish_LL(0, 1, 2);
 
         return LL;
 }
 
 
+template<typename V>
+Matrix_t Msquare( const V& L){
+    Matrix_t res;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            res[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    res[i][j] += L[k][i] * L[k][j];
+                }
+            }
+    }
+    return res;
+}
+
 int main(){
 
     Vector_t D0, D1, D2;
-    Matrix_t L, L_sol;
-    double avgerror=0, error=0;
+    Matrix_t L, L2, L_sol;
+    double avgerror=0, error=0, error2=0;
 
     std::ifstream fin;
-    // std::ofstream fout;
+    std::ofstream fout;
 
-    fin.open("./cholesky_test_data/cholesky.csv");
-    // fout.open("./cholesky_test_data/results.csv");
+    fin.open("./cholesky_test_data/np_out.csv");
+    fout.open("./cholesky_test_data/results.csv");
 
     std::string tmp;
 
@@ -107,15 +122,40 @@ int main(){
         }
         
         L = choleskyb(D0, D1, D2);
+        L2 = Msquare(L);
         
         error = 0.0;
+        // for(int d = 0; d < 3 ; ++d){
+        //     for(int c = 0; c<=d; ++c){
+        //         error += std::abs( (L[c][d] - L_sol[c][d]) /L_sol[c][d] );
+        //         // fout << L[c][d] <<"=?"<<  L_sol[c][d] << "   ";
+        //         fout << std::abs(L[c][d] - L_sol[c][d])/ L_sol[c][d] << "   ";
+        //     }
+        // } 
+        // fout << std::endl;
+
+        error2 += 0;
         for(int d = 0; d < 3 ; ++d){
-            for(int c = d; c<=d; ++c)
-                error += std::abs( (L[c][d] - L_sol[c][d]) /L_sol[c][d] );
-                // fout << L[0][d] <<"=?"<<  L_sol[0][d] << "   ";
-                // fout << L[1][d] <<"=?"<<  L_sol[1][d] << "   ";
-                // fout << L[2][d] <<"=?"<<  L_sol[2][d] << "   ";
-        }
+
+                fout << L2[d][0]   << " " 
+                     << L2[d][1]   << " " 
+                     << L2[d][2]   << "     " 
+                     << D0[d]      << " "
+                     << D1[d]      << " "
+                     << D2[d]      << " ";
+
+                // error2 += std::abs( (L2[0][d] - D0[d]) /D0[d] );
+                // error2 += std::abs( (L2[1][d] - D1[d]) /D1[d] );
+                // error2 += std::abs( (L2[2][d] - D2[d]) /D2[d] );
+                fout << std::abs( (L2[d][0] - D0[d]) /D0[d] ) << " "
+                     << std::abs( (L2[d][1] - D1[d]) /D1[d] ) << " "
+                     << std::abs( (L2[d][2] - D2[d]) /D2[d] ) << " " << std::endl;
+        } 
+        fout << std::endl;
+
+
+
+
         // fout << std::endl;
         // fout << error << std::endl;
         // fout << std::endl;
@@ -124,10 +164,10 @@ int main(){
 
     }
 
-    avgerror  /= 200;
-    std::cout << "average error(sum of relative errors over all elements of L) over 200stof tests is:  " << std::endl;
-    std::cout << avgerror << std::endl;
-    std::cout << "but the given solution from python is in single precision soo ... " <<  std::endl;
+    // avgerror  /= 6*200;
+    // std::cout << "average relative error(sum of relative errors over all elements of L) over 200stof tests is:  " << std::endl;
+    // std::cout << avgerror << std::endl;
+    // std::cout << "but the given solution from python is in single precision soo ... " <<  std::endl;
     
     fin.close();
 
