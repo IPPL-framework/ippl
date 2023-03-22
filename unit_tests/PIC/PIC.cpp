44585705ffda8fc75ac7adb4e1f3dd0fb370ed12
Alessandro Vinciguerra <alessandro.vinciguerra@psi.ch>
2023-03-23 00:42:59 +0100

    Generalize PIC unit test
    Reduce problem size

--- unit_tests/PIC/PIC.cpp
+++ unit_tests/PIC/PIC.cpp
@@ -20,15 +20,22 @@
 #include <cmath>
 #include <random>
 
+#include "MultirankUtils.h"
 #include "gtest/gtest.h"
 
-class PICTest : public ::testing::Test {
+class PICTest : public ::testing::Test, public MultirankUtils<1, 2, 3, 4, 5, 6> {
 public:
-    static constexpr size_t dim = 3;
-    typedef ippl::Field<double, dim> field_type;
-    typedef ippl::FieldLayout<dim> flayout_type;
-    typedef ippl::UniformCartesian<double, dim> mesh_type;
-    typedef ippl::ParticleSpatialLayout<double, dim> playout_type;
+    template <unsigned Dim>
+    using field_type = ippl::Field<double, Dim>;
+
+    template <unsigned Dim>
+    using flayout_type = ippl::FieldLayout<Dim>;
+
+    template <unsigned Dim>
+    using mesh_type = ippl::UniformCartesian<double, Dim>;
+
+    template <unsigned Dim>
+    using playout_type = ippl::ParticleSpatialLayout<double, Dim>;
 
     template <class PLayout>
     struct Bunch : public ippl::ParticleBase<PLayout> {
@@ -43,35 +50,41 @@ public:
         charge_container_type Q;
     };
 
-    typedef Bunch<playout_type> bunch_type;
+    template <unsigned Dim>
+    using bunch_type = Bunch<playout_type<Dim>>;
 
     PICTest()
-        : nParticles(std::pow(256, 3))
-        , nPoints(512) {
-        setup();
+        : nParticles(32)
+        , nPoints(16) {
+        setup(this);
     }
 
-    void setup() {
+    template <unsigned Idx, unsigned Dim>
+    void setupDim() {
         ippl::Index I(nPoints);
-        ippl::NDIndex<dim> owned(I, I, I);
+        std::array<ippl::Index, Dim> args;
+        args.fill(I);
+        auto owned = std::make_from_tuple<ippl::NDIndex<Dim>>(args);
 
-        ippl::e_dim_tag domDec[dim];  // Specifies SERIAL, PARALLEL dims
-        for (unsigned int d = 0; d < dim; d++)
+        double dx = 1.0 / double(nPoints);
+        ippl::Vector<double, Dim> hx;
+        ippl::Vector<double, Dim> origin;
+
+        ippl::e_dim_tag domDec[Dim];  // Specifies SERIAL, PARALLEL dims
+        for (unsigned int d = 0; d < Dim; d++) {
             domDec[d] = ippl::PARALLEL;
+            hx[d]     = dx;
+            origin[d] = 0;
+        }
 
-        layout_m = flayout_type(owned, domDec);
+        auto& layout = std::get<Idx>(layouts) = flayout_type<Dim>(owned, domDec);
+        auto& mesh = std::get<Idx>(meshes) = mesh_type<Dim>(owned, hx, origin);
 
-        double dx                        = 1.0 / double(nPoints);
-        ippl::Vector<double, dim> hx     = {dx, dx, dx};
-        ippl::Vector<double, dim> origin = {0, 0, 0};
+        std::get<Idx>(fields) = std::make_unique<field_type<Dim>>(mesh, layout);
 
-        mesh_m = mesh_type(owned, hx, origin);
+        auto& pl = std::get<Idx>(playouts) = playout_type<Dim>(layout, mesh);
 
-        field = std::make_unique<field_type>(mesh_m, layout_m);
-
-        pl = playout_type(layout_m, mesh_m);
-
-        bunch = std::make_unique<bunch_type>(pl);
+        auto& bunch = std::get<Idx>(bunches) = std::make_unique<bunch_type<Dim>>(pl);
 
         int nRanks = Ippl::Comm->size();
         if (nParticles % nRanks > 0) {
@@ -89,9 +102,9 @@ public:
         eng.discard(nloc * Ippl::Comm->rank());
         std::uniform_real_distribution<double> unif(hx[0] / 2, 1 - (hx[0] / 2));
 
-        typename bunch_type::particle_position_type::HostMirror R_host = bunch->R.getHostMirror();
+        auto R_host = bunch->R.getHostMirror();
         for (size_t i = 0; i < nloc; ++i) {
-            for (int d = 0; d < 3; d++) {
+            for (unsigned d = 0; d < Dim; d++) {
                 R_host(i)[d] = unif(eng);
             }
         }
@@ -99,45 +112,57 @@ public:
         Kokkos::deep_copy(bunch->R.getView(), R_host);
     }
 
-    std::unique_ptr<field_type> field;
-    std::unique_ptr<bunch_type> bunch;
+    PtrCollection<std::shared_ptr, field_type> fields;
+    PtrCollection<std::shared_ptr, bunch_type> bunches;
     size_t nParticles;
     size_t nPoints;
-    playout_type pl;
+    Collection<playout_type> playouts;
 
 private:
-    flayout_type layout_m;
-    mesh_type mesh_m;
+    Collection<flayout_type> layouts;
+    Collection<mesh_type> meshes;
 };
 
 TEST_F(PICTest, Scatter) {
-    *field = 0.0;
+    auto check = [&]<unsigned Dim>(std::shared_ptr<field_type<Dim>>& field,
+                                   std::shared_ptr<bunch_type<Dim>>& bunch, playout_type<Dim>& pl) {
+        *field = 0.0;
 
-    double charge = 0.5;
+        double charge = 0.5;
 
-    bunch->Q = charge;
+        bunch->Q = charge;
 
-    bunch_type bunchBuffer(pl);
-    pl.update(*bunch, bunchBuffer);
+        bunch_type<Dim> bunchBuffer(pl);
+        pl.update(*bunch, bunchBuffer);
 
-    scatter(bunch->Q, *field, bunch->R);
+        scatter(bunch->Q, *field, bunch->R);
 
-    double totalcharge = field->sum();
+        double totalcharge = field->sum();
 
-    ASSERT_NEAR((nParticles * charge - totalcharge) / (nParticles * charge), 0.0, 1e-13);
+        ASSERT_NEAR((nParticles * charge - totalcharge) / (nParticles * charge), 0.0, 1e-13);
+    };
+
+    auto args = zip(fields, bunches, playouts);
+    apply(check, args);
 }
 
 TEST_F(PICTest, Gather) {
-    *field = 1.0;
+    auto check = [&]<unsigned Dim>(std::shared_ptr<field_type<Dim>>& field,
+                                   std::shared_ptr<bunch_type<Dim>>& bunch, playout_type<Dim>& pl) {
+        *field = 1.0;
 
-    bunch->Q = 0.0;
+        bunch->Q = 0.0;
 
-    bunch_type bunchBuffer(pl);
-    pl.update(*bunch, bunchBuffer);
+        bunch_type<Dim> bunchBuffer(pl);
+        pl.update(*bunch, bunchBuffer);
 
-    gather(bunch->Q, *field, bunch->R);
+        gather(bunch->Q, *field, bunch->R);
 
-    ASSERT_DOUBLE_EQ((nParticles - bunch->Q.sum()) / nParticles, 0.0);
+        ASSERT_DOUBLE_EQ((nParticles - bunch->Q.sum()) / nParticles, 0.0);
+    };
+
+    auto args = zip(fields, bunches, playouts);
+    apply(check, args);
 }
 
 int main(int argc, char* argv[]) {
