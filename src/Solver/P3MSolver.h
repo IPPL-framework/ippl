051931f719ef3c37cef1b64e43d04ecf5d94f10a
mayani_s <sonali.mayani@psi.ch>
2023-05-09 16:14:11 +0200

    make initializeFields and greensFunction private

--- src/Solver/P3MSolver.h
+++ src/Solver/P3MSolver.h
@@ -61,12 +61,6 @@ namespace ippl {
         // more specifically, compute the scalar potential given a density field rho
         void solve() override;
 
-        // compute standard Green's function
-        void greensFunction();
-
-        // function called in the constructor to initialize the fields
-        void initializeFields();
-
     private:
         Field_t grn_m;  // the Green's function
 
@@ -94,6 +88,12 @@ namespace ippl {
         // mesh spacing and mesh size
         Vector_t hr_m;
         Vector<int, Dim> nr_m;
+
+        // function called in the constructor to initialize the fields
+        void initializeFields();
+
+        // compute standard Green's function
+        void greensFunction();
 
     protected:
         virtual void setDefaultParameters() override {
