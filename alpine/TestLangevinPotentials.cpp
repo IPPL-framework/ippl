43272ad8727b28b1ab1c333b3bc6bf6020d80645
claglu_t <tobia.clagluena@psi.ch>
2023-05-16 16:44:02 +0200

    Pack everything in loop to run for range of gridsizes

--- alpine/TestLangevinPotentials.cpp
+++ alpine/TestLangevinPotentials.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]){
     //const double FOCUS_FORCE     = std::atof(argv[12]);
     //const int PRINT_INTERVAL     = std::atoi(argv[13]);
     const double EPS_INV         = std::atof(argv[14]);
-    const size_t NV              = std::atoi(argv[15]);
+    const size_t NV_MAX              = std::atoi(argv[15]);
     const double VMAX            = std::atof(argv[16]);
     // const double REL_BUFF        = std::atof(argv[17]);
     // const bool VMESH_ADAPT_B     = std::atoi(argv[18]);
@@ -83,207 +83,210 @@ int main(int argc, char *argv[]){
     double numberDensity = 1.0;
     //double numberDensity = NP / (BOXL*BOXL*BOXL);
 
-    /////////////////////////
-    // CONFIGURATION SPACE //
-    /////////////////////////
+    for(size_t nv = 8; nv <= NV_MAX; nv *= 2) {
 
-    const ippl::NDIndex<Dim> configSpaceIdxDomain(NR, NR, NR);
-    ippl::e_dim_tag configSpaceDecomp[Dim] = {ippl::PARALLEL, ippl::PARALLEL, ippl::PARALLEL};
+        /////////////////////////
+        // CONFIGURATION SPACE //
+        /////////////////////////
 
-    const double L = BOXL * 0.5;
-    const VectorD_t configSpaceLowerBound({-L,-L,-L});
-    const VectorD_t configSpaceUpperBound({L,L,L});
-    const VectorD_t configSpaceOrigin({-L,-L,-L});
-    VectorD_t hr({BOXL / NR, BOXL / NR, BOXL / NR});  // spacing
-    VectorD<size_t> nr({NR, NR, NR});
+        const ippl::NDIndex<Dim> configSpaceIdxDomain(NR, NR, NR);
+        ippl::e_dim_tag configSpaceDecomp[Dim] = {ippl::PARALLEL, ippl::PARALLEL, ippl::PARALLEL};
 
-    Mesh_t<Dim> configSpaceMesh(configSpaceIdxDomain, hr, configSpaceOrigin);
-    const bool isAllPeriodic = true;
-    FieldLayout_t<Dim> configSpaceFieldLayout(configSpaceIdxDomain, configSpaceDecomp, isAllPeriodic);
-    PLayout_t<Dim> PL(configSpaceFieldLayout, configSpaceMesh);
+        const double L = BOXL * 0.5;
+        const VectorD_t configSpaceLowerBound({-L,-L,-L});
+        const VectorD_t configSpaceUpperBound({L,L,L});
+        const VectorD_t configSpaceOrigin({-L,-L,-L});
+        VectorD_t hr({BOXL / NR, BOXL / NR, BOXL / NR});  // spacing
+        VectorD<size_t> nr({NR, NR, NR});
 
-    const double Q = NP * PARTICLE_CHARGE;
+        Mesh_t<Dim> configSpaceMesh(configSpaceIdxDomain, hr, configSpaceOrigin);
+        const bool isAllPeriodic = true;
+        FieldLayout_t<Dim> configSpaceFieldLayout(configSpaceIdxDomain, configSpaceDecomp, isAllPeriodic);
+        PLayout_t<Dim> PL(configSpaceFieldLayout, configSpaceMesh);
 
-    msg << "Initialized Configuration Space" << endl;
+        const double Q = NP * PARTICLE_CHARGE;
 
-    /////////////////////////////////
-    // LANGEVIN PARTICLE CONTAINER //
-    /////////////////////////////////
+        msg << "Initialized Configuration Space" << endl;
 
-    std::shared_ptr P = std::make_shared<bunch_type>(PL, hr,
-                                          configSpaceLowerBound, configSpaceUpperBound, configSpaceDecomp,
-                                          SOLVER_T, PARTICLE_CHARGE, PARTICLE_MASS, EPS_INV, Q, NP, DT,
-                                          NV, VMAX);
+        /////////////////////////////////
+        // LANGEVIN PARTICLE CONTAINER //
+        /////////////////////////////////
 
-    // Initialize Particle Fields in Particles Class
-    P->nr_m = {int(NR), int(NR), int(NR)};
-    P->E_m.initialize(configSpaceMesh, configSpaceFieldLayout);
-    P->rho_m.initialize(configSpaceMesh, configSpaceFieldLayout);
+        std::shared_ptr P = std::make_shared<bunch_type>(PL, hr,
+                                              configSpaceLowerBound, configSpaceUpperBound, configSpaceDecomp,
+                                              SOLVER_T, PARTICLE_CHARGE, PARTICLE_MASS, EPS_INV, Q, NP, DT,
+                                              nv, VMAX);
 
-    // Set Periodic BCs for rho
-    typedef ippl::BConds<double, Dim, Mesh_t<Dim>, Centering_t<Dim>> bc_type;
+        // Initialize Particle Fields in Particles Class
+        P->nr_m = {int(NR), int(NR), int(NR)};
+        P->E_m.initialize(configSpaceMesh, configSpaceFieldLayout);
+        P->rho_m.initialize(configSpaceMesh, configSpaceFieldLayout);
 
-    bc_type bcField;
-    for (unsigned int i=0; i < 6; ++i) {
-        bcField[i] = std::make_shared<ippl::PeriodicFace<double, Dim, Mesh_t<Dim>, Centering_t<Dim>>>(i);
+        // Set Periodic BCs for rho
+        typedef ippl::BConds<double, Dim, Mesh_t<Dim>, Centering_t<Dim>> bc_type;
+
+        bc_type bcField;
+        for (unsigned int i=0; i < 6; ++i) {
+            bcField[i] = std::make_shared<ippl::PeriodicFace<double, Dim, Mesh_t<Dim>, Centering_t<Dim>>>(i);
+        }
+        P->rho_m.setFieldBC(bcField);
+
+        bunch_type bunchBuffer(PL);
+        std::string frictionSolverName = "HOCKNEY";
+        P->initAllSolvers(frictionSolverName);
+
+        P->loadbalancethreshold_m = LB_THRESHOLD;
+
+        msg << "Initialized Particle Bunch" << endl;
+
+        //////////////////////////////////////////////
+        // REFERENCE SOLUTION FIELDS FOR MAXWELLIAN //
+        //////////////////////////////////////////////
+
+        // Create scalar Field for Rosenbluth Potential
+        auto HfieldExact = P->fv_m.deepCopy();
+        auto GfieldExact = P->fv_m.deepCopy();
+        
+        //////////////////////////////////////////////
+        // PARTICLE CREATION & INITIAL SPACE CHARGE //
+        //////////////////////////////////////////////
+
+        size_t nloc = NP / comm_size;
+
+        // Last rank might have to initialize more Particles if `NP` not evenly divisble
+        if ( rank == comm_size-1){ nloc = NP - (comm_size-1)*nloc; }
+
+        P->create(nloc);
+
+        // Initialize Cold Sphere (positions only)
+        Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42 + 100*rank));
+        Kokkos::parallel_for(nloc,
+                GenerateRandomBoxPositions<VectorD_t, Kokkos::Random_XorShift64_Pool<> >(
+                    P->R.getView(), BOXL, rand_pool64));
+
+        // Initialize constant particle attributes
+        P->q = PARTICLE_CHARGE;
+
+        Kokkos::fence();
+        Ippl::Comm->barrier();
+
+        // Initialize Maxwellian Velocity Distribution
+        const ippl::NDIndex<Dim>& lDom = P->velocitySpaceFieldLayout_m.getLocalNDIndex();
+        const int nghost               = P->fv_m.getNghost();
+        auto fvView                    = P->fv_m.getView();
+        auto HviewExact                = HfieldExact.getView();
+        auto GviewExact                = GfieldExact.getView();
+        VectorD_t hv                   = P->hv_m;
+        VectorD_t vOrigin              = P->vmin_m;
+
+        using index_array_type = typename ippl::RangePolicy<Dim>::index_array_type;
+        ippl::parallel_for(
+                "Assign initial velocity PDF and reference solution for H", ippl::getRangePolicy<Dim>(fvView, nghost),
+                KOKKOS_LAMBDA(const index_array_type& args) {
+                // local to global index conversion
+                Vector_t<Dim> xvec = args;
+                for (unsigned d = 0; d < Dim; d++) {
+                    xvec[d] = (xvec[d] + lDom[d].first() - nghost + 0.5) * hv[d] + vOrigin[d];
+                }
+
+                // ippl::apply<unsigned> accesses the view at the given indices and obtains a
+                // reference; see src/Expression/IpplOperations.h
+                ippl::apply<Dim>(fvView, args) = maxwellianPDF(xvec, numberDensity, vth);
+                ippl::apply<Dim>(HviewExact, args) = HexactDistribution(xvec, numberDensity, vth);
+                });
+
+        Kokkos::fence();
+
+        dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "fvInit");
+        
+        //////////////////////////////////////
+        // COMPUTE 1st ROSENBLUTH POTENTIAL //
+        //////////////////////////////////////
+
+        // Need to scatter rho as we use it as $f(\vec r)$
+        P->runSpaceChargeSolver(0);
+
+        // Multiply with prefactor
+        P->fv_m = - 8.0 * P->pi_m * P->fv_m;
+
+        // Set origin of velocity space mesh to zero (for FFT)
+        P->velocitySpaceMesh_m.setOrigin(0.0);
+
+        // Solve for $\nabla_v H(\vec v)$, is stored in `Fd_m`
+        // $H(\vec v)$, is stored in `fv_m`
+        P->frictionSolver_mp->solve();
+
+        // Set origin of velocity space mesh to vmin (for scatter / gather)
+        P->velocitySpaceMesh_m.setOrigin(P->vmin_m);
+        
+
+        // Dump resulting Fields
+        dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Happr");
+        dumpVTKScalar(HfieldExact, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Hexact");
+        dumpVTKVector(P->Fd_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "gradHappr");
+        auto Hdiff = P->fv_m.deepCopy();
+        Hdiff = Hdiff - HfieldExact;
+        dumpVTKScalar(Hdiff, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Hdiff");
+
+        P->gatherFd();
+
+        // Multiply with prob. density in configuration space $f(\vec r)$
+        // Can be done as we use normalized particle charge $q = -1$
+        P->p_Fd_m = P->p_Fd_m * P->gamma_m * (P->q / P->Q_m);
+
+        P->dumpFdStatistics(0, OUT_DIR);
+        
+        //////////////////////////////////////
+        // COMPUTE 2nd ROSENBLUTH POTENTIAL //
+        //////////////////////////////////////
+
+        using index_array_type = typename ippl::RangePolicy<Dim>::index_array_type;
+        ippl::parallel_for(
+                "Assign initial velocity PDF and reference solution for G", ippl::getRangePolicy<Dim>(fvView, nghost),
+                KOKKOS_LAMBDA(const index_array_type& args) {
+                // local to global index conversion
+                Vector_t<Dim> xvec = args;
+                for (unsigned d = 0; d < Dim; d++) {
+                    xvec[d] = (xvec[d] + lDom[d].first() - nghost + 0.5) * hv[d] + vOrigin[d];
+                }
+
+                // ippl::apply<unsigned> accesses the view at the given indices and obtains a
+                // reference; see src/Expression/IpplOperations.h
+                ippl::apply<Dim>(fvView, args) = maxwellianPDF(xvec, numberDensity, vth);
+                ippl::apply<Dim>(GviewExact, args) = GexactDistribution(xvec, numberDensity, vth);
+                });
+        
+        // Need to scatter rho as we use it as $f(\vec r)$
+        P->runSpaceChargeSolver(0);
+
+        // Multiply with prefactors defined in RHS of Rosenbluth equations
+        // FFTPoissonSolver returns $ \Delta_v \Delta_v G(\vec v)$ in `fv_m`
+        P->fv_m = -8.0 * P->pi_m * P->fv_m;
+
+        // Set origin of velocity space mesh to zero (for FFT)
+        P->velocitySpaceMesh_m.setOrigin(0.0);
+
+        // Solve for $\Delta_v \Delta_v G(\vec v)$, is stored in `fv_m`
+        P->diffusionSolver_mp->solve();
+
+        // Set origin of velocity space mesh to vmin (for scatter / gather)
+        P->velocitySpaceMesh_m.setOrigin(P->vmin_m);
+
+        dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gappr");
+        dumpVTKScalar(GfieldExact, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gexact");
+        auto Gdiff = P->fv_m.deepCopy();
+        Gdiff = Gdiff - GfieldExact;
+        dumpVTKScalar(Gdiff, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gdiff");
+
+        ////////////////////////////
+        // COMPUTE RELATIVE ERROR //
+        ////////////////////////////
+
+        double HrelError = norm(Hdiff) / norm(HfieldExact);
+        double GrelError = norm(Gdiff) / norm(GfieldExact);
+        msg << "h(v) rel. error (" << nv << "^3)" << ": " << HrelError << endl;
+        msg << "g(v) rel. error (" << nv << "^3)" << ": " << GrelError << endl;
     }
-    P->rho_m.setFieldBC(bcField);
-
-    bunch_type bunchBuffer(PL);
-    std::string frictionSolverName = "HOCKNEY";
-    P->initAllSolvers(frictionSolverName);
-
-    P->loadbalancethreshold_m = LB_THRESHOLD;
-
-    msg << "Initialized Particle Bunch" << endl;
-
-    //////////////////////////////////////////////
-    // REFERENCE SOLUTION FIELDS FOR MAXWELLIAN //
-    //////////////////////////////////////////////
-
-    // Create scalar Field for Rosenbluth Potential
-    auto HfieldExact = P->fv_m.deepCopy();
-    auto GfieldExact = P->fv_m.deepCopy();
-    
-    //////////////////////////////////////////////
-    // PARTICLE CREATION & INITIAL SPACE CHARGE //
-    //////////////////////////////////////////////
-
-    size_t nloc = NP / comm_size;
-
-    // Last rank might have to initialize more Particles if `NP` not evenly divisble
-    if ( rank == comm_size-1){ nloc = NP - (comm_size-1)*nloc; }
-
-    P->create(nloc);
-
-    // Initialize Cold Sphere (positions only)
-    Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42 + 100*rank));
-    Kokkos::parallel_for(nloc,
-            GenerateRandomBoxPositions<VectorD_t, Kokkos::Random_XorShift64_Pool<> >(
-                P->R.getView(), BOXL, rand_pool64));
-
-    // Initialize constant particle attributes
-    P->q = PARTICLE_CHARGE;
-
-    Kokkos::fence();
-    Ippl::Comm->barrier();
-
-    // Initialize Maxwellian Velocity Distribution
-    const ippl::NDIndex<Dim>& lDom = P->velocitySpaceFieldLayout_m.getLocalNDIndex();
-    const int nghost               = P->fv_m.getNghost();
-    auto fvView                    = P->fv_m.getView();
-    auto HviewExact                = HfieldExact.getView();
-    auto GviewExact                = GfieldExact.getView();
-    VectorD_t hv                   = P->hv_m;
-    VectorD_t vOrigin              = P->vmin_m;
-
-    using index_array_type = typename ippl::RangePolicy<Dim>::index_array_type;
-    ippl::parallel_for(
-            "Assign initial velocity PDF and reference solution for H", ippl::getRangePolicy<Dim>(fvView, nghost),
-            KOKKOS_LAMBDA(const index_array_type& args) {
-            // local to global index conversion
-            Vector_t<Dim> xvec = args;
-            for (unsigned d = 0; d < Dim; d++) {
-                xvec[d] = (xvec[d] + lDom[d].first() - nghost + 0.5) * hv[d] + vOrigin[d];
-            }
-
-            // ippl::apply<unsigned> accesses the view at the given indices and obtains a
-            // reference; see src/Expression/IpplOperations.h
-            ippl::apply<Dim>(fvView, args) = maxwellianPDF(xvec, numberDensity, vth);
-            ippl::apply<Dim>(HviewExact, args) = HexactDistribution(xvec, numberDensity, vth);
-            });
-
-    Kokkos::fence();
-
-    dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "fvInit");
-    
-    //////////////////////////////////////
-    // COMPUTE 1st ROSENBLUTH POTENTIAL //
-    //////////////////////////////////////
-
-    // Need to scatter rho as we use it as $f(\vec r)$
-    P->runSpaceChargeSolver(0);
-
-    // Multiply with prefactor
-    P->fv_m = - 8.0 * P->pi_m * P->fv_m;
-
-    // Set origin of velocity space mesh to zero (for FFT)
-    P->velocitySpaceMesh_m.setOrigin(0.0);
-
-    // Solve for $\nabla_v H(\vec v)$, is stored in `Fd_m`
-    // $H(\vec v)$, is stored in `fv_m`
-    P->frictionSolver_mp->solve();
-
-    // Set origin of velocity space mesh to vmin (for scatter / gather)
-    P->velocitySpaceMesh_m.setOrigin(P->vmin_m);
-    
-
-    // Dump resulting Fields
-    dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Happr");
-    dumpVTKScalar(HfieldExact, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Hexact");
-    dumpVTKVector(P->Fd_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "gradHappr");
-    auto Hdiff = P->fv_m.deepCopy();
-    Hdiff = Hdiff - HfieldExact;
-    dumpVTKScalar(Hdiff, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Hdiff");
-
-    P->gatherFd();
-
-    // Multiply with prob. density in configuration space $f(\vec r)$
-    // Can be done as we use normalized particle charge $q = -1$
-    P->p_Fd_m = P->p_Fd_m * P->gamma_m * (P->q / P->Q_m);
-
-    P->dumpFdStatistics(0, OUT_DIR);
-    
-    //////////////////////////////////////
-    // COMPUTE 2nd ROSENBLUTH POTENTIAL //
-    //////////////////////////////////////
-
-    using index_array_type = typename ippl::RangePolicy<Dim>::index_array_type;
-    ippl::parallel_for(
-            "Assign initial velocity PDF and reference solution for H", ippl::getRangePolicy<Dim>(fvView, nghost),
-            KOKKOS_LAMBDA(const index_array_type& args) {
-            // local to global index conversion
-            Vector_t<Dim> xvec = args;
-            for (unsigned d = 0; d < Dim; d++) {
-                xvec[d] = (xvec[d] + lDom[d].first() - nghost + 0.5) * hv[d] + vOrigin[d];
-            }
-
-            // ippl::apply<unsigned> accesses the view at the given indices and obtains a
-            // reference; see src/Expression/IpplOperations.h
-            ippl::apply<Dim>(fvView, args) = maxwellianPDF(xvec, numberDensity, vth);
-            ippl::apply<Dim>(GviewExact, args) = GexactDistribution(xvec, numberDensity, vth);
-            });
-    
-    // Need to scatter rho as we use it as $f(\vec r)$
-    P->runSpaceChargeSolver(0);
-
-    // Multiply with prefactors defined in RHS of Rosenbluth equations
-    // FFTPoissonSolver returns $ \Delta_v \Delta_v G(\vec v)$ in `fv_m`
-    P->fv_m = -8.0 * P->pi_m * P->fv_m;
-
-    // Set origin of velocity space mesh to zero (for FFT)
-    P->velocitySpaceMesh_m.setOrigin(0.0);
-
-    // Solve for $\Delta_v \Delta_v G(\vec v)$, is stored in `fv_m`
-    P->diffusionSolver_mp->solve();
-
-    // Set origin of velocity space mesh to vmin (for scatter / gather)
-    P->velocitySpaceMesh_m.setOrigin(P->vmin_m);
-
-    dumpVTKScalar(P->fv_m, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gappr");
-    dumpVTKScalar(GfieldExact, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gexact");
-    auto Gdiff = P->fv_m.deepCopy();
-    Gdiff = Gdiff - GfieldExact;
-    dumpVTKScalar(Gdiff, P->hv_m, P->nv_m, P->vmin_m, 0, 1.0, OUT_DIR, "Gdiff");
-
-    ////////////////////////////
-    // COMPUTE RELATIVE ERROR //
-    ////////////////////////////
-
-    double HrelError = norm(Hdiff) / norm(HfieldExact);
-    double GrelError = norm(Gdiff) / norm(GfieldExact);
-    msg << "h(v) rel. error (" << NV << "^3)" << ": " << HrelError << endl;
-    msg << "g(v) rel. error (" << NV << "^3)" << ": " << GrelError << endl;
 
     msg << "LangevinPotentials: End." << endl;
     IpplTimings::stopTimer(mainTimer);
