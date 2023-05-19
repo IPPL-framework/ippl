422d7a18f44d9300c3fb46834875807d4435ae8c
claglu_t <tobia.clagluena@psi.ch>
2023-05-19 10:37:39 +0200

    Check for NaNs (not relying on std::isnan). Reformat with clang-format

--- alpine/LangevinHelpers.hpp
+++ alpine/LangevinHelpers.hpp
@@ -13,7 +13,7 @@ template <typename T>
 using VectorD = Vector<T, Dim>;
 
 template <typename T>
-using Vector2D = Vector<T, 2*Dim>;
+using Vector2D = Vector<T, 2 * Dim>;
 
 using VectorD_t = VectorD<double>;
 
@@ -23,79 +23,74 @@ using MatrixD_t = VectorD<VectorD<double>>;
 
 using Matrix2D_t = Vector2D<Vector2D<double>>;
 
-
-template<typename T>
-KOKKOS_INLINE_FUNCTION
-typename T::value_type L2Norm(T &x) {
-  return sqrt(dot(x, x).apply());
+template <typename T>
+KOKKOS_INLINE_FUNCTION typename T::value_type L2Norm(T& x) {
+    return sqrt(dot(x, x).apply());
 }
-
 
 // Generate random numbers in Sphere given by `beamRadius`, centered at the origin
 template <typename T, class GeneratorPool>
 struct GenerateBoxMuller {
+    using view_type  = typename ippl::detail::ViewType<T, 1>::view_type;
+    using value_type = typename T::value_type;
+    // Output View for the random positions in the sphere
+    view_type r;
+    const value_type beamRadius;
+    const value_type oneThird = 1.0 / 3.0;
+    const T origin            = {0.0, 0.0, 0.0};
 
-  using view_type = typename ippl::detail::ViewType<T, 1>::view_type;
-  using value_type  = typename T::value_type;
-  // Output View for the random positions in the sphere
-  view_type r;
-  const value_type beamRadius;
-  const value_type oneThird = 1.0 / 3.0;
-  const T origin = {0.0,0.0,0.0};
+    // The GeneratorPool
+    GeneratorPool pool;
 
-  // The GeneratorPool
-  GeneratorPool pool;
+    // Initialize all members
+    GenerateBoxMuller(view_type r_, value_type beamRadius_, GeneratorPool pool_)
+        : r(r_)
+        , beamRadius(beamRadius_)
+        , pool(pool_) {}
 
-  // Initialize all members
-  GenerateBoxMuller(view_type r_, value_type beamRadius_, GeneratorPool pool_) :
-                    r(r_), beamRadius(beamRadius_), pool(pool_) {}
+    KOKKOS_INLINE_FUNCTION void operator()(const size_t i) const {
+        // Get a random number state from the pool for the active thread
+        typename GeneratorPool::generator_type rand_gen = pool.get_state();
 
-  KOKKOS_INLINE_FUNCTION
-  void operator()(const size_t i) const {
-    // Get a random number state from the pool for the active thread
-    typename GeneratorPool::generator_type rand_gen = pool.get_state();
+        T X({rand_gen.normal(), rand_gen.normal(), rand_gen.normal()});
+        value_type uniform(rand_gen.drand(0.0, 1.0));
 
-    T X({rand_gen.normal(), rand_gen.normal(), rand_gen.normal()});
-    value_type uniform(rand_gen.drand(0.0, 1.0));
+        r(i) = origin + beamRadius * std::pow(uniform, oneThird) / L2Norm(X) * X;
 
-    r(i) = origin + beamRadius * std::pow(uniform, oneThird) / L2Norm(X) * X;
-
-    // Give the state back, which will allow another thread to acquire it
-    pool.free_state(rand_gen);
-  }
+        // Give the state back, which will allow another thread to acquire it
+        pool.free_state(rand_gen);
+    }
 };
 
 // Generate random numbers in Sphere given by `beamRadius`, centered at the origin
 template <typename T, class GeneratorPool>
 struct GenerateRandomBoxPositions {
+    using view_type  = typename ippl::detail::ViewType<T, 1>::view_type;
+    using value_type = typename T::value_type;
+    // Output View for the random positions in the sphere
+    view_type r;
+    const value_type halfBoxL;
 
-  using view_type = typename ippl::detail::ViewType<T, 1>::view_type;
-  using value_type  = typename T::value_type;
-  // Output View for the random positions in the sphere
-  view_type r;
-  const value_type halfBoxL;
+    // The GeneratorPool
+    GeneratorPool pool;
 
-  // The GeneratorPool
-  GeneratorPool pool;
+    // Initialize all members
+    GenerateRandomBoxPositions(view_type r_, value_type boxL_, GeneratorPool pool_)
+        : r(r_)
+        , halfBoxL(0.5 * boxL_)
+        , pool(pool_) {}
 
-  // Initialize all members
-  GenerateRandomBoxPositions(view_type r_, value_type boxL_, GeneratorPool pool_) :
-                    r(r_), halfBoxL(0.5*boxL_), pool(pool_) {}
+    KOKKOS_INLINE_FUNCTION void operator()(const size_t i) const {
+        // Get a random number state from the pool for the active thread
+        typename GeneratorPool::generator_type rand_gen = pool.get_state();
 
-  KOKKOS_INLINE_FUNCTION
-  void operator()(const size_t i) const {
-    // Get a random number state from the pool for the active thread
-    typename GeneratorPool::generator_type rand_gen = pool.get_state();
+        r(i) = {rand_gen.drand(-halfBoxL, halfBoxL), rand_gen.drand(-halfBoxL, halfBoxL),
+                rand_gen.drand(-halfBoxL, halfBoxL)};
 
-    r(i) = {rand_gen.drand(-halfBoxL, halfBoxL),
-            rand_gen.drand(-halfBoxL, halfBoxL),
-            rand_gen.drand(-halfBoxL, halfBoxL)};
-
-    // Give the state back, which will allow another thread to acquire it
-    pool.free_state(rand_gen);
-  }
+        // Give the state back, which will allow another thread to acquire it
+        pool.free_state(rand_gen);
+    }
 };
-
 
 ///////////////////////
 // DUMPING FUNCTIONS //
@@ -105,105 +100,96 @@ struct GenerateRandomBoxPositions {
 // TODO Allow dumping from multiple ranks
 void dumpVTKScalar(Field_t<Dim>& F, VectorD_t cellSpacing, VectorD<size_t> nCells, VectorD_t origin,
                    int iteration, double scalingFactor, std::string out_dir, std::string label) {
+    int nx = nCells[0];
+    int ny = nCells[1];
+    int nz = nCells[2];
 
-  int nx = nCells[0];
-  int ny = nCells[1];
-  int nz = nCells[2];
-  
-  typename Field_t<Dim>::view_type::host_mirror_type host_view = F.getHostMirror();
+    typename Field_t<Dim>::view_type::host_mirror_type host_view = F.getHostMirror();
 
-  std::stringstream fname;
-  fname << out_dir;
-  fname << "/";
-  fname << label;
-  fname << "_it";
-  fname << std::setw(4) << std::setfill('0') << iteration;
-  fname << ".vtk";
+    std::stringstream fname;
+    fname << out_dir;
+    fname << "/";
+    fname << label;
+    fname << "_it";
+    fname << std::setw(4) << std::setfill('0') << iteration;
+    fname << ".vtk";
 
-  Kokkos::deep_copy(host_view, F.getView());
+    Kokkos::deep_copy(host_view, F.getView());
 
-  Inform vtkout(NULL, fname.str().c_str(), Inform::OVERWRITE);
-  vtkout.precision(10);
-  vtkout.setf(std::ios::scientific, std::ios::floatfield);
+    Inform vtkout(NULL, fname.str().c_str(), Inform::OVERWRITE);
+    vtkout.precision(10);
+    vtkout.setf(std::ios::scientific, std::ios::floatfield);
 
-  // start with header
-  vtkout << "# vtk DataFile Version 2.0" << endl;
-  vtkout << TestName << endl;
-  vtkout << "ASCII" << endl;
-  vtkout << "DATASET STRUCTURED_POINTS" << endl;
-  vtkout << "DIMENSIONS " << nx+1 << " " << ny+1 << " " << nz+1 << endl;
-  vtkout << "ORIGIN " << origin[0] << " " 
-                      << origin[1] << " " 
-                      << origin[2] << endl;
-  vtkout << "SPACING " << cellSpacing[0] << " "
-                       << cellSpacing[1] << " "
-                       << cellSpacing[2] << endl;
-  vtkout << "CELL_DATA " << nx * ny * nz << endl;
-  vtkout << "SCALARS " << label << " float" << endl;
-  vtkout << "LOOKUP_TABLE default" << endl;
-  for (int z=1; z<nz+1; z++) {
-    for (int y=1; y<ny+1; y++) {
-      for (int x=1; x<nx+1; x++) {
-        vtkout << scalingFactor*host_view(x,y,z) << endl;
-      }
+    // start with header
+    vtkout << "# vtk DataFile Version 2.0" << endl;
+    vtkout << TestName << endl;
+    vtkout << "ASCII" << endl;
+    vtkout << "DATASET STRUCTURED_POINTS" << endl;
+    vtkout << "DIMENSIONS " << nx + 1 << " " << ny + 1 << " " << nz + 1 << endl;
+    vtkout << "ORIGIN " << origin[0] << " " << origin[1] << " " << origin[2] << endl;
+    vtkout << "SPACING " << cellSpacing[0] << " " << cellSpacing[1] << " " << cellSpacing[2]
+           << endl;
+    vtkout << "CELL_DATA " << nx * ny * nz << endl;
+    vtkout << "SCALARS " << label << " float" << endl;
+    vtkout << "LOOKUP_TABLE default" << endl;
+    for (int z = 1; z < nz + 1; z++) {
+        for (int y = 1; y < ny + 1; y++) {
+            for (int x = 1; x < nx + 1; x++) {
+                vtkout << scalingFactor * host_view(x, y, z) << endl;
+            }
+        }
     }
-  }
 }
 
+void dumpVTKVector(VField_t<Dim>& F, VectorD_t cellSpacing, VectorD<size_t> nCells,
+                   VectorD_t origin, int iteration, double scalingFactor, std::string out_dir,
+                   std::string label) {
+    int nx = nCells[0];
+    int ny = nCells[1];
+    int nz = nCells[2];
 
-void dumpVTKVector(VField_t<Dim>& F, VectorD_t cellSpacing, VectorD<size_t> nCells, VectorD_t origin,
-                   int iteration, double scalingFactor, std::string out_dir, std::string label) {
+    typename VField_t<Dim>::view_type::host_mirror_type host_view = F.getHostMirror();
 
-  int nx = nCells[0];
-  int ny = nCells[1];
-  int nz = nCells[2];
-  
-  typename VField_t<Dim>::view_type::host_mirror_type host_view = F.getHostMirror();
+    std::stringstream fname;
+    fname << out_dir;
+    fname << "/";
+    fname << label;
+    fname << "_it";
+    fname << std::setw(4) << std::setfill('0') << iteration;
+    fname << ".vtk";
 
-  std::stringstream fname;
-  fname << out_dir;
-  fname << "/";
-  fname << label;
-  fname << "_it";
-  fname << std::setw(4) << std::setfill('0') << iteration;
-  fname << ".vtk";
+    Kokkos::deep_copy(host_view, F.getView());
 
-  Kokkos::deep_copy(host_view, F.getView());
+    Inform vtkout(NULL, fname.str().c_str(), Inform::OVERWRITE);
+    vtkout.precision(10);
+    vtkout.setf(std::ios::scientific, std::ios::floatfield);
 
-  Inform vtkout(NULL, fname.str().c_str(), Inform::OVERWRITE);
-  vtkout.precision(10);
-  vtkout.setf(std::ios::scientific, std::ios::floatfield);
-
-  // start with header
-  vtkout << "# vtk DataFile Version 2.0" << endl;
-  vtkout << TestName << endl;
-  vtkout << "ASCII" << endl;
-  vtkout << "DATASET STRUCTURED_POINTS" << endl;
-  vtkout << "DIMENSIONS " << nx+1 << " " << ny+1 << " " << nz+1 << endl;
-  vtkout << "ORIGIN " << origin[0] << " " 
-                      << origin[1] << " " 
-                      << origin[2] << endl;
-  vtkout << "SPACING " << cellSpacing[0] << " "
-                       << cellSpacing[1] << " "
-                       << cellSpacing[2] << endl;
-  vtkout << "CELL_DATA " << nx * ny * nz << endl;
-  vtkout << "VECTORS " << label << " float" << endl;
-  for (int z=1; z<nz+1; z++) {
-    for (int y=1; y<ny+1; y++) {
-      for (int x=1; x<nx+1; x++) {
-        vtkout << scalingFactor*host_view(x,y,z)[0] << "\t"
-               << scalingFactor*host_view(x,y,z)[1] << "\t"
-               << scalingFactor*host_view(x,y,z)[2] << endl;
-      }
+    // start with header
+    vtkout << "# vtk DataFile Version 2.0" << endl;
+    vtkout << TestName << endl;
+    vtkout << "ASCII" << endl;
+    vtkout << "DATASET STRUCTURED_POINTS" << endl;
+    vtkout << "DIMENSIONS " << nx + 1 << " " << ny + 1 << " " << nz + 1 << endl;
+    vtkout << "ORIGIN " << origin[0] << " " << origin[1] << " " << origin[2] << endl;
+    vtkout << "SPACING " << cellSpacing[0] << " " << cellSpacing[1] << " " << cellSpacing[2]
+           << endl;
+    vtkout << "CELL_DATA " << nx * ny * nz << endl;
+    vtkout << "VECTORS " << label << " float" << endl;
+    for (int z = 1; z < nz + 1; z++) {
+        for (int y = 1; y < ny + 1; y++) {
+            for (int x = 1; x < nx + 1; x++) {
+                vtkout << scalingFactor * host_view(x, y, z)[0] << "\t"
+                       << scalingFactor * host_view(x, y, z)[1] << "\t"
+                       << scalingFactor * host_view(x, y, z)[2] << endl;
+            }
+        }
     }
-  }
 }
-
 
 // Store all BeamStatistics to be gathered for file dumping
 // Not in use yet (could improve readibility of dumping later on)
 // TODO Write sensible comments
-struct BeamStatistics{
+struct BeamStatistics {
     double fieldEnergy;
     double ExAmplitude;
     double lorentzAvg;
@@ -211,10 +197,10 @@ struct BeamStatistics{
 
     VectorD_t temperature;
 
-    VectorD_t vmax; 
-    VectorD_t vmin; 
+    VectorD_t vmax;
+    VectorD_t vmin;
 
-    VectorD_t rmax; 
+    VectorD_t rmax;
     VectorD_t rmin;
 
     // Moments
@@ -251,46 +237,44 @@ struct BeamStatistics{
     VectorD_t Neps2;
 };
 
-KOKKOS_INLINE_FUNCTION
-MatrixD_t cholesky3x3(const MatrixD_t& M) {
-  MatrixD_t L;
-  L[0][0] = sqrt(M[0][0]);
-  L[1][0] = M[1][0] / L[0][0];
-  L[1][1] = sqrt(M[1][1] - L[1][0] * L[1][0]);
-  L[2][0] = M[2][0] / L[0][0];
-  L[2][1] = (M[2][1] - L[2][0] * L[1][0]) / L[1][1];
-  L[2][2] = sqrt(M[2][2] - L[2][0] * L[2][0] - L[2][1] * L[2][1]);
+KOKKOS_INLINE_FUNCTION MatrixD_t cholesky3x3(const MatrixD_t& M) {
+    MatrixD_t L;
+    L[0][0] = sqrt(M[0][0]);
+    L[1][0] = M[1][0] / L[0][0];
+    L[1][1] = sqrt(M[1][1] - L[1][0] * L[1][0]);
+    L[2][0] = M[2][0] / L[0][0];
+    L[2][1] = (M[2][1] - L[2][0] * L[1][0]) / L[1][1];
+    L[2][2] = sqrt(M[2][2] - L[2][0] * L[2][0] - L[2][1] * L[2][1]);
 
-  // Check that there has been no NaN computed
-  bool foundNaN = false;
-  for(int i = 0; i < 3; ++i) {
-    for(int j = 0; j <= i; ++j) {
-      if(isnan(L[i][j])){
-        foundNaN = true;
-      }
+    // Check that there has been no NaN computed
+    bool foundNaN = false;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            if (L[i][j] == L[i][j]) {
+                foundNaN = true;
+            }
+        }
     }
-  }
 
-  // Print input Matrix M
-  if(foundNaN) {
-    for(int i = 0; i < 3; ++i) {
-      for(int j = 0; j < 3; ++j) {
-        std::cout << M[i][j] << ' ';
-      }
-      std::cout << '\n';
+    // Print input Matrix M
+    if (foundNaN) {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                std::cout << M[i][j] << ' ';
+            }
+            std::cout << '\n';
+        }
     }
-  }
-  PAssert(foundNaN == false);
-  return L;
+    PAssert(foundNaN == false);
+    return L;
 }
 
-KOKKOS_INLINE_FUNCTION
-VectorD_t matrixVectorMul3x3(const MatrixD_t& M, const VectorD_t& v) {
-  VectorD_t res;
-  res[0] = M[0][0] * v[0] + M[0][1] * v[1] + M[0][2] * v[2];
-  res[1] = M[1][0] * v[0] + M[1][1] * v[1] + M[1][2] * v[2];
-  res[2] = M[2][0] * v[0] + M[2][1] * v[1] + M[2][2] * v[2];
-  return res;
+KOKKOS_INLINE_FUNCTION VectorD_t matrixVectorMul3x3(const MatrixD_t& M, const VectorD_t& v) {
+    VectorD_t res;
+    res[0] = M[0][0] * v[0] + M[0][1] * v[1] + M[0][2] * v[2];
+    res[1] = M[1][0] * v[0] + M[1][1] * v[1] + M[1][2] * v[2];
+    res[2] = M[2][0] * v[0] + M[2][1] * v[1] + M[2][2] * v[2];
+    return res;
 }
 
 #endif /* LANGEVINHELPERS_HPP */
