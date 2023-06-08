05b8e8e2d5e1de0c919468d28dd6f0cae48e7490
Matthias Frey <mf248@st-andrews.ac.uk>
2023-06-08 18:58:33 +0100

    add Request.cpp

--- src/Communicate/Request.h
+++ src/Communicate/Request.h
@@ -12,48 +12,20 @@ namespace ippl {
 
             ~Request();
 
-            operator MPI_Request*() noexcept { return &request_m; }
+            operator MPI_Request&() noexcept { return request_m; }
 
-            operator const MPI_Request*() const noexcept { return &request_m; }
+            operator const MPI_Request&() const noexcept { return request_m; }
 
             bool completed();
 
-            void wait();
-
             void free() { MPI_Request_free(&request_m); }
+
+            void wait() { MPI_Wait(&request_m, MPI_STATUS_IGNORE); }
 
         private:
             MPI_Request request_m;
         };
-
-        Request::~Request() {
-            if (request_m != MPI_REQUEST_NULL) {
-                this->free();
-            }
-        }
-
-        bool Request::completed() {
-            int flag = 0;
-
-            Status status;
-
-            // MPI_STATUS_IGNORE
-            MPI_Request_get_status(request_m, &flag, &status);
-
-            if (flag != 0) {
-                // valid Status instance
-                MPI_Test(&request_m, &flag, &status);
-            } else {
-                // Although we free the request, any ongoing communication
-                // associated with this request is allowed to complete.
-                this->free();
-            }
-
-            return (flag != 0);
-        }
-
-        void Request::wait() {
-            MPI_Wait(&request_m, MPI_STATUS_IGNORE);
-        }
     }  // namespace mpi
 }  // namespace ippl
+
+#endif
