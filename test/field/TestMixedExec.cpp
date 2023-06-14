0917f3306652c642b56f461875adfa18e44d6f7e
Alessandro Vinciguerra <alessandro.vinciguerra@psi.ch>
2023-06-14 15:49:27 +0200

    Update mixed exec test to use new IPPL init

--- test/field/TestMixedExec.cpp
+++ test/field/TestMixedExec.cpp
@@ -3,29 +3,31 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    Ippl ippl(argc, argv);
+    ippl::initialize(argc, argv);
 
-    constexpr unsigned int dim = 3;
+    {
+        constexpr unsigned int dim = 3;
 
-    ippl::Index I(16);
-    ippl::NDIndex<dim> owned(I, I, I);
+        ippl::Index I(16);
+        ippl::NDIndex<dim> owned(I, I, I);
 
-    ippl::e_dim_tag decomp[dim];  // Specifies SERIAL, PARALLEL dims
-    for (unsigned int d = 0; d < dim; d++) {
-        decomp[d] = ippl::PARALLEL;
+        ippl::e_dim_tag decomp[dim];  // Specifies SERIAL, PARALLEL dims
+        for (unsigned int d = 0; d < dim; d++) {
+            decomp[d] = ippl::PARALLEL;
+        }
+
+        ippl::FieldLayout<dim> layout(owned, decomp);
+
+        typedef ippl::BareField<double, dim, Kokkos::Cuda> cuda_field;
+        typedef ippl::BareField<double, dim, Kokkos::OpenMP> omp_field;
+
+        cuda_field cf{layout};
+        omp_field of{layout};
+
+        cf = 1;
+        of = 1;
     }
 
-    ippl::FieldLayout<dim> layout(owned, decomp);
-
-    typedef ippl::BareField<double, dim, Kokkos::Cuda> cuda_field;
-    typedef ippl::BareField<double, dim, Kokkos::OpenMP> omp_field;
-
-    cuda_field cf{layout};
-    omp_field of{layout};
-
-    cf = 1;
-    of = 1;
-
-    Kokkos::fence();
+    ippl::finalize();
     return 0;
 }
