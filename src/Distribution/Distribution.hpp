a4946673802fb467dcfd61609d9854a2f666457c
Andreas Adelmann <andreas,adelmann@psi.ch>
2023-07-28 13:21:53 +0200

    add doc

--- src/Distribution/Distribution.hpp
+++ src/Distribution/Distribution.hpp
@@ -9,19 +9,35 @@
 
 #include "PICManager/PICManager.hpp"
 
+/**
+ * @namespace Distribution
+ * @brief Contains functions and structures for handling distributions.
+ */
 namespace Distribution {
 
     const unsigned Dim = 3;
 
+    /**
+     * @brief Compute the cumulative distribution function (CDF) of a distribution.
+     * @param x The input value.
+     * @param alpha The alpha parameter of the distribution.
+     * @param k The k parameter of the distribution.
+     * @return The value of the CDF at x.
+     */
     double CDF(const double& x, const double& alpha, const double& k) {
         double cdf = x + (alpha / k) * std::sin(k * x);
         return cdf;
     }
 
-    /*
-      FixMe: this needs to go to Distributions
-    */
-
+    /**
+     * @brief Compute the probability density function (PDF) of a distribution in multiple
+     * dimensions.
+     * @param xvec The input vector containing the coordinates in each dimension.
+     * @param alpha The alpha parameter of the distribution.
+     * @param kw The k parameter for each dimension of the distribution.
+     * @param Dim The number of dimensions.
+     * @return The value of the PDF at the given coordinates.
+     */
     KOKKOS_FUNCTION
     double PDF(const Vector_t<double, Dim>& xvec, const double& alpha,
                const Vector_t<double, Dim>& kw, const unsigned Dim) {
@@ -33,6 +49,10 @@ namespace Distribution {
         return pdf;
     }
 
+    /**
+     * @brief A structure representing the Newton method for finding roots of 1D functions.
+     * @tparam T The type of the parameters and variables used in the Newton method.
+     */
     template <typename T>
     struct Newton1D {
         double tol   = 1e-12;
@@ -41,163 +61,80 @@ namespace Distribution {
 
         T k, alpha, u;
 
+        /**
+         * @brief Default constructor for the Newton1D struct.
+         */
         KOKKOS_INLINE_FUNCTION Newton1D() {}
 
+        /**
+         * @brief Constructor for the Newton1D struct.
+         * @param k_ The k parameter of the distribution.
+         * @param alpha_ The alpha parameter of the distribution.
+         * @param u_ The u parameter of the distribution.
+         */
         KOKKOS_INLINE_FUNCTION Newton1D(const T& k_, const T& alpha_, const T& u_)
             : k(k_)
             , alpha(alpha_)
             , u(u_) {}
 
+        /**
+         * @brief Destructor for the Newton1D struct.
+         */
         KOKKOS_INLINE_FUNCTION ~Newton1D() {}
 
+        /**
+         * @brief Compute the function value for the given variable in the Newton method.
+         * @param x The variable for which to compute the function value.
+         * @return The value of the function at x.
+         */
         KOKKOS_INLINE_FUNCTION T f(T& x) {
             T F;
             F = x + (alpha * (Kokkos::sin(k * x) / k)) - u;
             return F;
         }
-
-        KOKKOS_INLINE_FUNCTION T fprime(T& x) {
-            T Fprime;
-            Fprime = 1 + (alpha * Kokkos::cos(k * x));
-            return Fprime;
-        }
-
-        KOKKOS_FUNCTION
-        void solve(T& x) {
-            int iterations = 0;
-            while (iterations < max_iter && Kokkos::fabs(f(x)) > tol) {
-                x = x - (f(x) / fprime(x));
-                iterations += 1;
-            }
-        }
     };
 
-    template <typename T, class GeneratorPool, unsigned Dim>
-    struct generate_random {
-        using view_type  = typename ippl::detail::ViewType<T, 1>::view_type;
-        using value_type = typename T::value_type;
-        // Output View for the random numbers
-        view_type x, v;
+    /**
+     * @brief Function to create particles based on the distribution.
+     * @param P Particle object where the generated particles will be stored.
+     * @param RLayout Layout for generating the particles.
+     */
+    void createParticles(auto P, auto RLayout) {
+        auto Regions = RLayout.gethLocalRegions();
+        Vector_t<double, Dim> Nr, Dr, minU, maxU;
+        int myRank    = ippl::Comm->rank();
+        double factor = 1;
 
-        // The GeneratorPool
-        GeneratorPool rand_pool;
-
-        value_type alpha;
-
-        T k, minU, maxU;
-
-        // Initialize all members
-        generate_random(view_type x_, view_type v_, GeneratorPool rand_pool_, value_type& alpha_,
-                        T& k_, T& minU_, T& maxU_)
-            : x(x_)
-            , v(v_)
-            , rand_pool(rand_pool_)
-            , alpha(alpha_)
-            , k(k_)
-            , minU(minU_)
-            , maxU(maxU_) {}
-
-        KOKKOS_INLINE_FUNCTION void operator()(const size_t i) const {
-            // Get a random number state from the pool for the active thread
-            typename GeneratorPool::generator_type rand_gen = rand_pool.get_state();
-
-            value_type u;
-            for (unsigned d = 0; d < Dim; ++d) {
-                u       = rand_gen.drand(minU[d], maxU[d]);
-                x(i)[d] = u / (1 + alpha);
-                Newton1D<value_type> solver(k[d], alpha, u);
-                solver.solve(x(i)[d]);
-                v(i)[d] = rand_gen.normal(0.0, 1.0);
-            }
-
-            // Give the state back, which will allow another thread to acquire it
-            rand_pool.free_state(rand_gen);
-        }
-    };
-
-    template <typename T, unsigned Dim = 3>
-    class Distribution {
-        double alpha_m;
-        Vector_t<double, Dim> kw_m;
-        Vector_t<double, Dim> rmax_m;
-        Vector_t<double, Dim> rmin_m;
-        Vector_t<double, Dim> hr_m;
-        Vector_t<double, Dim> origin_m;
-        const size_type totalP_m;
-
-    public:
-        Distribution(double alpha, Vector_t<double, Dim> kw, Vector_t<double, Dim> rmax,
-                     Vector_t<double, Dim> rmin, Vector_t<double, Dim> hr,
-                     Vector_t<double, Dim> origin, const size_type totalP)
-            : alpha_m(alpha)
-            , kw_m(kw)
-            , rmax_m(rmax)
-            , rmin_m(rmin)
-            , hr_m(hr)
-            , origin_m(origin)
-            , totalP_m(totalP) {}
-
-        ~Distribution() {}
-
-        void repartitionRhs(auto P, const ippl::NDIndex<Dim>& lDom) {
-            using index_array_type = typename ippl::RangePolicy<Dim>::index_array_type;
-
-            const int nghost = P->rhs_m.getNghost();
-            auto rhoview     = P->rhs_m.getView();
-            auto rangePolicy = P->rhs_m.getFieldRangePolicy();
-
-            ippl::parallel_for(
-                "Assign initial rho based on PDF", rangePolicy,
-                KOKKOS_LAMBDA(const index_array_type& args) {
-                    // local to global index conversion
-                    Vector_t<double, Dim> xvec =
-                        (args + lDom.first() - nghost + 0.5) * hr_m + origin_m;
-
-                    // ippl::apply accesses the view at the given indices and obtains a
-                    // reference; see src/Expression/IpplOperations.h
-                    ippl::apply(rhoview, args) = PDF(xvec, alpha_m, kw_m, Dim);
-                });
-            Kokkos::fence();
+        for (unsigned d = 0; d < Dim; ++d) {
+            Nr[d] = CDF(Regions(myRank)[d].max(), alpha_m, kw_m[d])
+                    - CDF(Regions(myRank)[d].min(), alpha_m, kw_m[d]);
+            Dr[d]   = CDF(rmax_m[d], alpha_m, kw_m[d]) - CDF(rmin_m[d], alpha_m, kw_m[d]);
+            minU[d] = CDF(Regions(myRank)[d].min(), alpha_m, kw_m[d]);
+            maxU[d] = CDF(Regions(myRank)[d].max(), alpha_m, kw_m[d]);
+            factor *= Nr[d] / Dr[d];
         }
 
-        void createParticles(auto P, auto RLayout) {
-            auto Regions = RLayout.gethLocalRegions();
-            Vector_t<double, Dim> Nr, Dr, minU, maxU;
-            int myRank    = ippl::Comm->rank();
-            double factor = 1;
+        size_type nloc            = (size_type)(factor * totalP_m);
+        size_type Total_particles = 0;
 
-            for (unsigned d = 0; d < Dim; ++d) {
-                Nr[d] = CDF(Regions(myRank)[d].max(), alpha_m, kw_m[d])
-                        - CDF(Regions(myRank)[d].min(), alpha_m, kw_m[d]);
-                Dr[d]   = CDF(rmax_m[d], alpha_m, kw_m[d]) - CDF(rmin_m[d], alpha_m, kw_m[d]);
-                minU[d] = CDF(Regions(myRank)[d].min(), alpha_m, kw_m[d]);
-                maxU[d] = CDF(Regions(myRank)[d].max(), alpha_m, kw_m[d]);
-                factor *= Nr[d] / Dr[d];
-            }
+        MPI_Allreduce(&nloc, &Total_particles, 1, MPI_UNSIGNED_LONG, MPI_SUM,
+                      ippl::Comm->getCommunicator());
 
-            size_type nloc            = (size_type)(factor * totalP_m);
-            size_type Total_particles = 0;
+        int rest = (int)(totalP_m - Total_particles);
 
-            MPI_Allreduce(&nloc, &Total_particles, 1, MPI_UNSIGNED_LONG, MPI_SUM,
-                          ippl::Comm->getCommunicator());
-
-            int rest = (int)(totalP_m - Total_particles);
-
-            if (ippl::Comm->rank() < rest) {
-                ++nloc;
-            }
-
-            P->create(nloc);
-
-            Kokkos::Random_XorShift64_Pool<> rand_pool64(
-                (size_type)(42 + 100 * ippl::Comm->rank()));
-            Kokkos::parallel_for(
-                nloc, generate_random<Vector_t<double, Dim>, Kokkos::Random_XorShift64_Pool<>, Dim>(
-                          P->R.getView(), P->V.getView(), rand_pool64, alpha_m, kw_m, minU, maxU));
-
-            Kokkos::fence();
+        if (ippl::Comm->rank() < rest) {
+            ++nloc;
         }
-    };
+
+        P->create(nloc);
+
+        Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42 + 100 * ippl::Comm->rank()));
+        Kokkos::parallel_for(
+            nloc, generate_random<Vector_t<double, Dim>, Kokkos::Random_XorShift64_Pool<>, Dim>(
+                      P->R.getView(), P->V.getView(), rand_pool64, alpha_m, kw_m, minU, maxU));
+
+        Kokkos::fence();
+    }
 
 }  // namespace Distribution
 #endif
