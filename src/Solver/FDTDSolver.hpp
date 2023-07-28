26a80977d9c80af5d8b9569559bf67216b3490ff
mayani_s <sonali.mayani@psi.ch>
2023-07-29 01:15:50 +0200

    fix typo

--- src/Solver/FDTDSolver.hpp
+++ src/Solver/FDTDSolver.hpp
@@ -326,7 +326,7 @@ namespace ippl {
 
         // electric field is the time derivative of the vector potential
         // minus the gradient of the scalar potential
-        (*En_mp) = -(aNp1_m - aN_m) / dt - grad(phiNp1_m);
+        (*En_mp) = (*En_mp) - (aNp1_m - aN_m) / dt - grad(phiN_m);
     };
 
     template <typename Tfields, unsigned Dim, class M, class C>
