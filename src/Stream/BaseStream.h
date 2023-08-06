94ead0d7dcf57154db9887200fb33fda92b25c4c
Matthias Frey <mf248@st-andrews.ac.uk>
2023-08-06 10:20:28 +0100

    add close and open functions

--- src/Stream/BaseStream.h
+++ src/Stream/BaseStream.h
@@ -6,6 +6,11 @@ namespace ippl {
     template <class Object>
     class BaseStream {
     public:
+
+        virtual void open(std::string filename) = 0;
+
+        virtual void close() = 0;
+
         virtual void operator<<(const Object& obj) = 0;
 
         virtual void operator>>(Object& obj) = 0;
