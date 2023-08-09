3b4697fc76510041f935715daac6a341c6039260
carl_j <carl.jolly@stfc.ac.uk>
2023-08-09 14:48:55 +0100

    Add cmake lists change. Fixed mistake with initialising and getting the mesh in scatterToSlices, rho and E fields are now declared in the main function kokkos arrays of views, one view for each slice.

--- test/solver/SliceTo2D.cpp
+++ test/solver/SliceTo2D.cpp
@@ -8,26 +8,62 @@
 #include <vector>
 
 #include "Utility/IpplTimings.h"
-//template <unsigned Dim = 3>
-//using Mesh_t = ippl::UniformCartesian<double, Dim>;
-constexpr unsigned Dim = 3;
 
-typedef ippl::ParticleSpatialLayout<double, Dim> PLayout_t;
-typedef ippl::UniformCartesian<double, Dim> Mesh_t;
-typedef ippl::FieldLayout<Dim> FieldLayout_t;
+template <unsigned Dim>
+using Mesh_t = ippl::UniformCartesian<double, Dim>;
+
+template <typename T, unsigned Dim>
+using PLayout_t = typename ippl::ParticleSpatialLayout<T, Dim, Mesh_t<Dim>>;
+
+template <unsigned Dim>
+using Centering_t = typename Mesh_t<Dim>::DefaultCentering;
+
+template <unsigned Dim>
+using FieldLayout_t = ippl::FieldLayout<Dim>;
+
+using size_type = ippl::detail::size_type;
+
+template <typename T, unsigned Dim>
+using Vector = ippl::Vector<T, Dim>;
+
+template <typename T, unsigned Dim, class... ViewArgs>
+using Field = ippl::Field<T, Dim, Mesh_t<Dim>, Centering_t<Dim>, ViewArgs...>;
 
 template <typename T>
 using ParticleAttrib = ippl::ParticleAttrib<T>;
 
-//template <typename T, unsigned Dim = 2>
-//using Vector_t = ippl::Vector<T, Dim>;
 template <typename T, unsigned Dim>
-using Vector = ippl::Vector<T, Dim>;
+using Vector_t = ippl::Vector<T, Dim>;
 
-typedef Vector<double, Dim> Vector_t;
+template <unsigned Dim, class... ViewArgs>
+using Field_t = Field<double, Dim, ViewArgs...>;
 
-template <typename T, unsigned Dim>
-using playout_type = ippl::ParticleSpatialLayout<T, Dim>;
+template <typename T, unsigned Dim, class... ViewArgs>
+using VField_t = Field<Vector_t<T, Dim>, Dim, ViewArgs...>;
+
+template <typename T>
+using ScalarField_t = typename ippl::Field<T, 2, Mesh_t<2>, Centering_t<2>>;
+
+
+/*
+//template <unsigned Dim = 3>
+//using Mesh_t = ippl::UniformCartesian<double, Dim>;
+
+//template <typename T, unsigned Dim>
+//using playout_type = ippl::ParticleSpatialLayout<T, Dim>;
+
+//using Centering_t = typename Mesh_t::DefaultCentering;
+
+
+template <typename T, unsigned Dim = 3, class... ViewArgs>
+using Field = ippl::Field<T, Dim, Mesh_t, Centering_t, ViewArgs...>;
+
+template <unsigned Dim = 3, class... ViewArgs>
+using Field_t = Field<double, Dim, ViewArgs...>;
+
+template <typename T = double, unsigned Dim = 3, class... ViewArgs>
+using VField_t = Field<Vector<T, Dim>, Dim, ViewArgs...>;
+*/
 
 //    constexpr unsigned sliceCount;
 //    std::array<Field2D_t, sliceCount> slices;
@@ -41,13 +77,26 @@ class Solve25D : public ippl::ParticleBase<PLayout> {
     using Base = ippl::ParticleBase<PLayout>;
 
 public:
-    Vector<int, 3> nr_m;
+    // kokkos array of these types below, but not phi because, I just use that once but I need to keep E_m and rho_m
+    //VField_t<double, 2> E_m; // have an array of these, one for each slice Kokkos::Array<Field_t::view_type, slice_count> 
+    // define the R (rho) field
+
+    // need to define a Vfield and scalar field for E and rho
+    // also define a kokkos array of vfields and scalarviews' views ie
+    //Kokkos::Array<Field_t::view_type, slice_count> rhoViews; 
+
+    // std::array<Scarlfield, slices> rhos; ie relace below with <- and similarly for E field
+    //ScalarField_t<double> rho_m; // have an array of these, one for each slice Kokkos::Array<Field_t::view_type, slice_count>
+
+    Field<double, 2> phi_m;
+
+    //Vector<int, 3> nr_m;
 
     ippl::e_dim_tag decomp_m[3];
 
-    Vector_t hr_m;
-    Vector_t rmin_m;
-    Vector_t rmax_m;
+    Vector_t<double, 3> hr_m;
+    Vector_t<double, 3> rmin_m;
+    Vector_t<double, 3> rmax_m;
 
     std::string stype_m;
 
@@ -74,7 +123,7 @@ public:
         registerAttributes();
     }
 
-    Solve25D(PLayout& pl, Vector_t  hr, Vector_t  rmin, Vector_t  rmax,
+    Solve25D(PLayout& pl, Vector_t<double, 3>  hr, Vector_t<double, 3>  rmin, Vector_t<double, 3>  rmax,
                      ippl::e_dim_tag decomp[3], double Q, std::string solver)
         : Base(pl) 
         , hr_m(hr)
@@ -98,83 +147,85 @@ public:
 
     ~Solve25D() {}
 
-    template <typename Field>
-    void scatterToSlices(Field& f, int numSlices) {
+    template <size_t nSlices>
+    void scatterToSlices(int numSlices, std::array<ScalarField_t<double>, nSlices>& rhos, Kokkos::Array<Field_t<2>::view_type, nSlices>& rhoViews) {
 
         // copy stuff from particleattrib.hpp
         static IpplTimings::TimerRef scatterTimer = IpplTimings::getTimer("scatter");
         IpplTimings::startTimer(scatterTimer);
 
-        using mesh_type       = typename Field::Mesh_t;
-        const mesh_type& mesh = f.get_mesh();
+        using mesh_type = typename Field_t<2>::Mesh_t;
+        mesh_type& mesh = rhos[0].get_mesh(); // mesh is the same for all rho
 
         using vector_type = typename mesh_type::vector_type;
+        const vector_type& dx = mesh.getMeshSpacing();
 
-        const vector_type& dx     = mesh.getMeshSpacing();
+        FieldLayout_t<2>& layout = rhos[0].getLayout();
+
         const vector_type& origin = mesh.getOrigin();
-        const vector_type invdx   = 1.0 / dx;
 
-        const FieldLayout_t& layout = f.getLayout();
-        const ippl::NDIndex<3>& lDom   = layout.getLocalNDIndex();
-        const int nghost                 = f.getNghost();
+        const vector_type invdx      = 1.0 / dx;
+        const ippl::NDIndex<2>& lDom = layout.getLocalNDIndex();
+        const int nghost             = rhos[0].getNghost();
+
 
         // loop to find min and max
         double sMin, sMax;
         sMin = sMax = R(0)[2]; // Initialize min and max with the first element
 
-        for (size_t i = 1; i <= P->getLocalNum(); ++i) {
+        for (size_t i = 1; i <= this->getLocalNum(); i++) {
             if (R(i)[2] < sMin) {
                 sMin = R(i)[2];
             } else if (R(i)[2] > sMax) {
                 sMax = R(i)[2];
             }
         }
-        
+        std::cerr << "sMin = " << sMin << std::endl;
+        std::cerr << "sMax = " << sMax << std::endl;
         double binWidth = (sMax - sMin) / numSlices;
-        Vector_t binBoundaries;
+        Kokkos::View<double*> binBoundaries;
 
-        Vector_t binViews;
+        //Vector<Kokkos::View<double**>, numSlices>  binViews;
+        //Vector_t binViews;
 
         for (int i = 0; i < numSlices; i++){
             binBoundaries[i] = sMin + i * binWidth;
         }
 
-        Vector_t mapping;
+        Kokkos::View<int*> mapping;
 
         Kokkos::parallel_for(
-            "Slice and scatter", P->getLocalNum(), KOKKOS_LAMBDA(const size_t i, const size_t j) {
+            "Slice and scatter", this->getLocalNum(), KOKKOS_LAMBDA(const size_t i) {
                 // Alex's suggestion on how to slice and project - not sure how this works
                 // how does the unsigned n line determine an integer slice index
                 // projection (and get slice index)
-                Vector_t pos2D = {R(0), R(1)}; // ie cut off the 3rd axis, frenet - serret s, to tructate the slice to 2D
+                 // ie cut off the 3rd axis, frenet - serret s, to tructate the slice to 2D
                 //unsigned n = (P(2) - sMin) / binWidth; // stuff to define
                // mapping(idx) = n;
 
                 // R(2) is the s coord of the bunch, bunch will need to be defined with this in mind when the bunch is made/ parsed
                 for (int binIndx = 1; binIndx <= numSlices; binIndx++){ // don't need this if you do analytically?? see above 
-                    if ((binBoundaries[binIndx - 1] <= R(i)[2]) && (R(i)[2] <= binBoundaries[binIndx])){
+                    if ((binBoundaries(binIndx - 1) <= R(i)[2]) && (R(i)[2] <= binBoundaries(binIndx))){
 
-                        // fix this
-                        //binViews[binIndx - 1](i, j) = R(i)[0], R(i)[1];
+                        //binViews[binIndx - 1](i, j) = R(i)[0], R(i)[1];-
 
-                        mapping[i] = binIndx; // map which particle belongs in which longitudinal bin
+                        mapping(i) = binIndx; // map which particle belongs in which longitudinal bin
                     }
                 }
 
+                ippl::Vector<double, 2> pos2D = {R(i)[0], R(i)[1]}; 
+
                 // interpolation stuff
-                vector_type l = (pos2D - origin) * invdx + 0.5;
-                ippl::Vector<int, Field::dim> index = l;
-                ippl::Vector<double, Field::dim> whi = l - index;
-                ippl::Vector<double, Field::dim> wlo = 1.0 - whi;
-                ippl::Vector<size_t, Field::dim> args = index - lDom.first() + nghost;
+                ippl::Vector<double, 2> l = (pos2D - origin) * invdx + 0.5;
+                Vector<int, 2> index = l;
+                Vector<double, 2> whi = l - index;
+                Vector<double, 2> wlo = 1.0 - whi;
+                Vector<size_t, 2> args = index - lDom.first() + nghost;
 
                 const value_type &val = q(i);
 
-                for (int n = 0; n <= numSlices; n++){
-                    ippl::detail::scatterToField(
-                        std::make_index_sequence<1 << Field::dim>{}, 
-                        binViews[n],
-                        wlo, whi, args, val);
+                for (int n = 0; n < numSlices; n++){
+                    ippl::detail::scatterToField(std::make_index_sequence<1 << 2>{}, rhoViews[n], wlo, whi, args ,val);
                 }
             });
     }
@@ -186,22 +237,21 @@ int main(int argc, char* argv[]) {
         Inform msg(argv[0]);
         Inform msg2all(argv[0], INFORM_ALL_NODES);
 
-        ippl::Vector<int, 3> nr = {std::atoi(argv[1]), std::atoi(argv[2]), std::atoi(argv[3])};
+        ippl::Vector<int, 2> nr = {std::atoi(argv[1]), std::atoi(argv[2])};
 
         static IpplTimings::TimerRef mainTimer = IpplTimings::getTimer("mainTimer");
         IpplTimings::startTimer(mainTimer);
         //auto start                = std::chrono::high_resolution_clock::now();
         const unsigned int totalP = std::atoi(argv[4]);
-        const unsigned int nt     = std::atoi(argv[5]);
 
         msg << "benchmarkUpdate" << endl
-            << "nt " << nt << " Np= " << totalP << " grid = " << nr << endl;
-        using bunch_type = Solve25D<PLayout_t>;
+            << " Np= " << totalP << " grid = " << nr << endl;
+        using bunch_type = Solve25D<PLayout_t<double, 3>>;
 
         std::unique_ptr<bunch_type> P;
 
         ippl::NDIndex<3> domain;
-        for (unsigned i = 0; i < 3; i++) {
+        for (unsigned i = 0; i < 2; i++) {
             domain[i] = ippl::Index(nr[i]);
         }
 
@@ -211,25 +261,25 @@ int main(int argc, char* argv[]) {
         }
 
         // create mesh and layout objects for this problem domain
-        Vector_t rmin(0.0);
-        Vector_t rmax(1.0);
+        Vector_t<double, 3> rmin(0.0);
+        Vector_t<double, 3> rmax(1.0);
         double dx       = rmax[0] / double(nr[0]);
         double dy       = rmax[1] / double(nr[1]);
         double dz       = rmax[2] / double(nr[2]);
-        Vector_t hr     = {dx, dy, dz};
-        Vector_t origin = {rmin[0], rmin[1], rmin[2]};
+        Vector_t<double, 3> hr     = {dx, dy, dz};
+        Vector_t<double, 3> origin = {rmin[0], rmin[1], rmin[2]};
         //double hr_min   = std::min({dx, dy, dz});
         //const double dt = 1.0;  // size of timestep
 
-        Mesh_t mesh(domain, hr, origin);
-        FieldLayout_t FL(domain, decomp);
-        PLayout_t PL(FL, mesh);
+        Mesh_t<3> mesh(domain, hr, origin); // this has dim 3 as this is the 3D particle distribution
+        FieldLayout_t<3> FL(domain, decomp);
+        PLayout_t<double, 3> PL(FL, mesh);
 
         /*
          * In case of periodic BC's define
          * the domain with hr and rmin
          */
-        std::string solver = argv[6];
+        std::string solver = argv[5];   
 
         double Q = 1e6;
         P        = std::make_unique<bunch_type>(PL, hr, rmin, rmax, decomp, Q, solver);
@@ -276,6 +326,49 @@ int main(int argc, char* argv[]) {
         IpplTimings::stopTimer(UpdateTimer);
 
         msg << "particles created and initial conditions assigned " << endl;
+
+        const size_t numSlices = 10;//std::atoi(argv[3]);
+
+        // create mesh and initialise fields
+        ippl::Index I(nr[0]);
+        ippl::NDIndex<2> owned(I, I);
+
+        // unit box
+        double dxRho                      = 1.0 / nr[0];
+        ippl::Vector<double, 2> hxRho     = {dxRho, dxRho};
+        ippl::Vector<double, 2> originRho = {0.0, 0.0};
+        Mesh_t<2> meshRho(owned, hxRho, originRho);
+
+        // all parallel layout, standard domain, normal axis order
+        ippl::FieldLayout<2> layout(owned, decomp);
+        
+        std::array<ScalarField_t<double>, numSlices> rhos; // hard coded 10 slices -  repalce later
+        Kokkos::Array<Field_t<2>::view_type, numSlices> rhoViews;
+
+        std::array<VField_t<double, 2>, numSlices> Efields;
+        Kokkos::Array<VField_t<double, 2>::view_type, numSlices> EViews;
+
+        for (size_t i = 0; i < numSlices; i++) {
+            rhos[i] = 0.0; // reset rho field 
+
+            rhos[i].initialize(meshRho, layout);
+            rhoViews[i] = rhos[i].getView();
+
+            Efields[i].initialize(meshRho, layout);
+            EViews[i] = Efields[i].getView();
+        }
+
+        // call slice and scatter function
+        P->scatterToSlices(numSlices, rhos, rhoViews); 
+
+        // 2d solve 
+
+        // 1d solve before gather!!
+
+        // for each particle, add the 1d solve component to the P->E abribute
+        // for each particle, add the 2 E field compoents from the interpolated hockney E feld to the P-> attribute
+        // gather 
+
     }
     ippl::finalize();
     return 0;
