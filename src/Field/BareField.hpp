e842e602000573e778df782ac53cc4b46adb5523
Alessandro Vinciguerra <alessandro.vinciguerra@psi.ch>
2023-08-25 11:35:55 +0200

    Use proper range policy types

--- src/Field/BareField.hpp
+++ src/Field/BareField.hpp
@@ -130,23 +130,23 @@ namespace ippl {
         write(inf.getDestination());
     }
 
-#define DefineReduction(fun, name, op, MPI_Op)                                       \
-    template <typename T, unsigned Dim, class... ViewArgs>                           \
-    T BareField<T, Dim, ViewArgs...>::name(int nghost) const {                       \
-        PAssert_LE(nghost, nghost_m);                                                \
-        T temp                 = 0.0;                                                \
-        using index_array_type = typename RangePolicy<Dim>::index_array_type;        \
-        ippl::parallel_reduce(                                                       \
-            "fun", getRangePolicy(dview_m, nghost_m - nghost),                       \
-            KOKKOS_CLASS_LAMBDA(const index_array_type& args, T& valL) {             \
-                T myVal = apply(dview_m, args);                                      \
-                op;                                                                  \
-            },                                                                       \
-            Kokkos::fun<T>(temp));                                                   \
-        T globaltemp      = 0.0;                                                     \
-        MPI_Datatype type = get_mpi_datatype<T>(temp);                               \
-        MPI_Allreduce(&temp, &globaltemp, 1, type, MPI_Op, Comm->getCommunicator()); \
-        return globaltemp;                                                           \
+#define DefineReduction(fun, name, op, MPI_Op)                                                 \
+    template <typename T, unsigned Dim, class... ViewArgs>                                     \
+    T BareField<T, Dim, ViewArgs...>::name(int nghost) const {                                 \
+        PAssert_LE(nghost, nghost_m);                                                          \
+        T temp                 = 0.0;                                                          \
+        using index_array_type = typename RangePolicy<Dim, execution_space>::index_array_type; \
+        ippl::parallel_reduce(                                                                 \
+            "fun", getRangePolicy(dview_m, nghost_m - nghost),                                 \
+            KOKKOS_CLASS_LAMBDA(const index_array_type& args, T& valL) {                       \
+                T myVal = apply(dview_m, args);                                                \
+                op;                                                                            \
+            },                                                                                 \
+            Kokkos::fun<T>(temp));                                                             \
+        T globaltemp      = 0.0;                                                               \
+        MPI_Datatype type = get_mpi_datatype<T>(temp);                                         \
+        MPI_Allreduce(&temp, &globaltemp, 1, type, MPI_Op, Comm->getCommunicator());           \
+        return globaltemp;                                                                     \
     }
 
     DefineReduction(Sum, sum, valL += myVal, MPI_SUM)
