c9ddd5b57a96fc0e40297f0e95786d3e91811b47
Felix Schurk <felix.schurk@rwth-aachen.de>
2023-09-07 11:48:02 +0200

    change pipeline to produce pictures for TestCatalystAdaptor
    fixing the pipeline to now produce the data after catalyst live
    connection

--- test/stream/catalyst2_pipeline.py
+++ test/stream/catalyst2_pipeline.py
@@ -10,17 +10,22 @@ paraview.simple._DisableFirstRenderCameraReset()
 # setup views used in the visualization
 # ----------------------------------------------------------------
 
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
 # Create a new 'Render View'
-renderView1 = CreateView('RenderView')
-renderView1.ViewSize = [862, 839]
-renderView1.AxesGrid = 'GridAxes3DActor'
-renderView1.CenterOfRotation = [0.5, 0.5, 0.5]
-renderView1.StereoType = 'Crystal Eyes'
-renderView1.CameraPosition = [-8.321539336674517, 5.118013528880978, 10.826590265546251]
-renderView1.CameraFocalPoint = [0.5, 0.5, 0.5]
-renderView1.CameraViewUp = [0.24312654349512303, 0.9458058854041994, -0.21526892711883117]
-renderView1.CameraFocalDisk = 1.0
-renderView1.CameraParallelScale = 1.7320508075688772
+renderView3 = CreateView('RenderView')
+renderView3.ViewSize = [936, 893]
+renderView3.AxesGrid = 'GridAxes3DActor'
+renderView3.CenterOfRotation = [0.5, 0.5, 0.5]
+renderView3.StereoType = 'Crystal Eyes'
+renderView3.CameraPosition = [5.357750529134775, 4.872085250907219, 5.280823925206746]
+renderView3.CameraFocalPoint = [0.5, 0.5, 0.5]
+renderView3.CameraViewUp = [-0.11343363691620603, 0.7878938103825996, -0.6052736187681985]
+renderView3.CameraFocalDisk = 1.0
+renderView3.CameraParallelScale = 1.7320508075688772
+renderView3.BackEnd = 'OSPRay raycaster'
+renderView3.OSPRayMaterialLibrary = materialLibrary1
 
 SetActiveView(None)
 
@@ -30,111 +35,136 @@ SetActiveView(None)
 
 # create new layout object 'Layout #1'
 layout1 = CreateLayout(name='Layout #1')
-layout1.AssignView(0, renderView1)
-layout1.SetSize(862, 839)
+layout1.SplitHorizontal(0, 0.500000)
+layout1.AssignView(2, renderView3)
+layout1.SetSize(1873, 893)
 
 # ----------------------------------------------------------------
 # restore active view
-SetActiveView(renderView1)
+SetActiveView(renderView3)
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
 # setup the data processing pipelines
 # ----------------------------------------------------------------
 
-# create a new 'Legacy VTK Reader'
-scalar_0000vtk = LegacyVTKReader(registrationName='scalar_0000.vtk*', FileNames=['/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0000.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0001.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0002.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0003.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0004.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0005.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0006.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0007.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0008.vtk', '/home/felix/data/juelich/ippl_toolchain/ippl/data/scalar_0009.vtk'])
-
-# create a new 'Cell Data to Point Data'
-cellDatatoPointData1 = CellDatatoPointData(registrationName='CellDatatoPointData1', Input=scalar_0000vtk)
-cellDatatoPointData1.CellDataArraytoprocess = ['Rho']
-
-# create a new 'Contour'
-contour1 = Contour(registrationName='Contour1', Input=cellDatatoPointData1)
-contour1.ContourBy = ['POINTS', 'Rho']
-contour1.Isosurfaces = [0.0, 0.1111111111111111, 0.2222222222222222, 0.3333333333333333, 0.4444444444444444, 0.5555555555555556, 0.6666666666666666, 0.7777777777777777, 0.8888888888888888, 1.0]
-contour1.PointMergeMethod = 'Uniform Binning'
+# create a new 'PVTrivialProducer'
+# extractippl_field = PVTrivialProducer(registrationName='Extract: ippl_field')
+extractippl_field = PVTrivialProducer(registrationName='ippl_field')
 
 # create a new 'Clip'
-clip1 = Clip(registrationName='Clip1', Input=contour1)
+clip1 = Clip(registrationName='Clip1', Input=extractippl_field)
 clip1.ClipType = 'Plane'
 clip1.HyperTreeGridClipper = 'Plane'
-clip1.Scalars = ['POINTS', 'Rho']
-clip1.Value = 0.5555555559694767
+clip1.Scalars = ['CELLS', 'density']
+clip1.Value = 0.5
 
 # init the 'Plane' selected for 'ClipType'
 clip1.ClipType.Origin = [0.5, 0.5, 0.5]
-clip1.ClipType.Normal = [0.0, 0.0, 1.0]
 
 # init the 'Plane' selected for 'HyperTreeGridClipper'
 clip1.HyperTreeGridClipper.Origin = [0.5, 0.5, 0.5]
 
 # ----------------------------------------------------------------
-# setup the visualization in view 'renderView1'
+# setup the visualization in view 'renderView3'
 # ----------------------------------------------------------------
 
+# show data from extractippl_field
+extractippl_fieldDisplay = Show(extractippl_field, renderView3, 'UniformGridRepresentation')
+
+# get 2D transfer function for 'density'
+densityTF2D = GetTransferFunction2D('density')
+densityTF2D.ScalarRangeInitialized = 1
+
+# get color transfer function/color map for 'density'
+densityLUT = GetColorTransferFunction('density')
+densityLUT.TransferFunction2D = densityTF2D
+densityLUT.ScalarRangeInitialized = 1.0
+
+# get opacity transfer function/opacity map for 'density'
+densityPWF = GetOpacityTransferFunction('density')
+densityPWF.ScalarRangeInitialized = 1
+
+# trace defaults for the display properties.
+extractippl_fieldDisplay.Representation = 'Outline'
+extractippl_fieldDisplay.ColorArrayName = ['CELLS', 'density']
+extractippl_fieldDisplay.LookupTable = densityLUT
+extractippl_fieldDisplay.SelectTCoordArray = 'None'
+extractippl_fieldDisplay.SelectNormalArray = 'None'
+extractippl_fieldDisplay.SelectTangentArray = 'None'
+extractippl_fieldDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+extractippl_fieldDisplay.SelectOrientationVectors = 'None'
+extractippl_fieldDisplay.ScaleFactor = 0.2
+extractippl_fieldDisplay.SelectScaleArray = 'None'
+extractippl_fieldDisplay.GlyphType = 'Arrow'
+extractippl_fieldDisplay.GlyphTableIndexArray = 'None'
+extractippl_fieldDisplay.GaussianRadius = 0.01
+extractippl_fieldDisplay.SetScaleArray = [None, '']
+extractippl_fieldDisplay.ScaleTransferFunction = 'PiecewiseFunction'
+extractippl_fieldDisplay.OpacityArray = [None, '']
+extractippl_fieldDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+extractippl_fieldDisplay.DataAxesGrid = 'GridAxesRepresentation'
+extractippl_fieldDisplay.PolarAxes = 'PolarAxesRepresentation'
+extractippl_fieldDisplay.ScalarOpacityUnitDistance = 0.8660254037844386
+extractippl_fieldDisplay.ScalarOpacityFunction = densityPWF
+extractippl_fieldDisplay.TransferFunction2D = densityTF2D
+extractippl_fieldDisplay.OpacityArrayName = ['CELLS', 'density']
+extractippl_fieldDisplay.ColorArray2Name = ['CELLS', 'density']
+extractippl_fieldDisplay.SliceFunction = 'Plane'
+extractippl_fieldDisplay.Slice = 2
+extractippl_fieldDisplay.SelectInputVectors = [None, '']
+extractippl_fieldDisplay.WriteLog = ''
+
+# init the 'Plane' selected for 'SliceFunction'
+extractippl_fieldDisplay.SliceFunction.Origin = [0.5, 0.5, 0.5]
+
 # show data from clip1
-clip1Display = Show(clip1, renderView1, 'UnstructuredGridRepresentation')
-
-# get 2D transfer function for 'Rho'
-rhoTF2D = GetTransferFunction2D('Rho')
-
-# get color transfer function/color map for 'Rho'
-rhoLUT = GetColorTransferFunction('Rho')
-rhoLUT.TransferFunction2D = rhoTF2D
-rhoLUT.RGBPoints = [0.1111111119389534, 0.231373, 0.298039, 0.752941, 0.5555555559694767, 0.865003, 0.865003, 0.865003, 1.0, 0.705882, 0.0156863, 0.14902]
-rhoLUT.ScalarRangeInitialized = 1.0
-
-# get opacity transfer function/opacity map for 'Rho'
-rhoPWF = GetOpacityTransferFunction('Rho')
-rhoPWF.Points = [0.1111111119389534, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0]
-rhoPWF.ScalarRangeInitialized = 1
+clip1Display = Show(clip1, renderView3, 'UnstructuredGridRepresentation')
 
 # trace defaults for the display properties.
 clip1Display.Representation = 'Surface'
-clip1Display.ColorArrayName = ['POINTS', 'Rho']
-clip1Display.LookupTable = rhoLUT
+clip1Display.ColorArrayName = ['CELLS', 'density']
+clip1Display.LookupTable = densityLUT
 clip1Display.SelectTCoordArray = 'None'
-clip1Display.SelectNormalArray = 'Normals'
+clip1Display.SelectNormalArray = 'None'
 clip1Display.SelectTangentArray = 'None'
-clip1Display.OSPRayScaleArray = 'Rho'
 clip1Display.OSPRayScaleFunction = 'PiecewiseFunction'
 clip1Display.SelectOrientationVectors = 'None'
 clip1Display.ScaleFactor = 0.2
-clip1Display.SelectScaleArray = 'Rho'
+clip1Display.SelectScaleArray = 'None'
 clip1Display.GlyphType = 'Arrow'
-clip1Display.GlyphTableIndexArray = 'Rho'
+clip1Display.GlyphTableIndexArray = 'None'
 clip1Display.GaussianRadius = 0.01
-clip1Display.SetScaleArray = ['POINTS', 'Rho']
+clip1Display.SetScaleArray = [None, '']
 clip1Display.ScaleTransferFunction = 'PiecewiseFunction'
-clip1Display.OpacityArray = ['POINTS', 'Rho']
+clip1Display.OpacityArray = [None, '']
 clip1Display.OpacityTransferFunction = 'PiecewiseFunction'
 clip1Display.DataAxesGrid = 'GridAxesRepresentation'
 clip1Display.PolarAxes = 'PolarAxesRepresentation'
-clip1Display.ScalarOpacityFunction = rhoPWF
-clip1Display.ScalarOpacityUnitDistance = 0.49106951261706133
-clip1Display.OpacityArrayName = ['POINTS', 'Rho']
-clip1Display.SelectInputVectors = ['POINTS', 'Normals']
+clip1Display.ScalarOpacityFunction = densityPWF
+clip1Display.ScalarOpacityUnitDistance = 0.9449407874211548
+clip1Display.OpacityArrayName = ['CELLS', 'density']
+clip1Display.SelectInputVectors = [None, '']
 clip1Display.WriteLog = ''
-
-# init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
-clip1Display.ScaleTransferFunction.Points = [0.1111111119389534, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0]
-
-# init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
-clip1Display.OpacityTransferFunction.Points = [0.1111111119389534, 0.0, 0.5, 0.0, 1.0, 1.0, 0.5, 0.0]
 
 # setup the color legend parameters for each legend in this view
 
-# get color legend/bar for rhoLUT in view renderView1
-rhoLUTColorBar = GetScalarBar(rhoLUT, renderView1)
-rhoLUTColorBar.Title = 'Rho'
-rhoLUTColorBar.ComponentTitle = ''
+# get color legend/bar for densityLUT in view renderView3
+densityLUTColorBar = GetScalarBar(densityLUT, renderView3)
+densityLUTColorBar.WindowLocation = 'Any Location'
+densityLUTColorBar.Position = [0.8376068376068376, 0.027995520716685318]
+densityLUTColorBar.Title = 'density'
+densityLUTColorBar.ComponentTitle = ''
+densityLUTColorBar.ScalarBarLength = 0.32999999999999996
 
 # set color bar visibility
-rhoLUTColorBar.Visibility = 1
+densityLUTColorBar.Visibility = 1
 
 # show color legend
-clip1Display.SetScalarBarVisibility(renderView1, True)
+extractippl_fieldDisplay.SetScalarBarVisibility(renderView3, True)
+
+# show color legend
+clip1Display.SetScalarBarVisibility(renderView3, True)
 
 # ----------------------------------------------------------------
 # setup color maps and opacity mapes used in the visualization
@@ -146,13 +176,13 @@ clip1Display.SetScalarBarVisibility(renderView1, True)
 # ----------------------------------------------------------------
 
 # create extractor
-pNG1 = CreateExtractor('PNG', renderView1, registrationName='ippl_field')
+pNG1 = CreateExtractor('PNG', renderView3, registrationName='PNG1')
 # trace defaults for the extractor.
 pNG1.Trigger = 'TimeStep'
 
 # init the 'PNG' selected for 'Writer'
-pNG1.Writer.FileName = 'RenderView1_{timestep:06d}{camera}.png'
-pNG1.Writer.ImageResolution = [862, 839]
+pNG1.Writer.FileName = 'RenderView3_{timestep:06d}{camera}.png'
+pNG1.Writer.ImageResolution = [936, 893]
 pNG1.Writer.Format = 'PNG'
 
 # ----------------------------------------------------------------
