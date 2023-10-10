667e5fca95692caace097310272cc8f71aa435fc
Bob Schreiner <bobschreiner@student-net-vpn-2528.intern.ethz.ch>
2023-10-10 12:58:03 +0200

    Added poisson preconditioner

---
+++ build_serial/src/CMakeFiles/ippl.dir/Utility/User.cpp.o.d
@@ -0,0 +1,4 @@
+src/CMakeFiles/ippl.dir/Utility/User.cpp.o: \
+  /Users/bobschreiner/ETH/Thesis/ippl/src/Utility/User.cpp \
+  /Users/bobschreiner/ETH/Thesis/ippl/src/Utility/User.h \
+  /Users/bobschreiner/ETH/Thesis/ippl/src/Utility/Unique.h
---
+++ build_serial/src/CMakeFiles/ippl.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ippl.dir/Communicate/Buffers.cpp.o"
+  "CMakeFiles/ippl.dir/Communicate/Buffers.cpp.o.d"
+  "CMakeFiles/ippl.dir/Communicate/Communicate.cpp.o"
+  "CMakeFiles/ippl.dir/Communicate/Communicate.cpp.o.d"
+  "CMakeFiles/ippl.dir/FieldLayout/FieldLayout.cpp.o"
+  "CMakeFiles/ippl.dir/FieldLayout/FieldLayout.cpp.o.d"
+  "CMakeFiles/ippl.dir/Ippl.cpp.o"
+  "CMakeFiles/ippl.dir/Ippl.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/Inform.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/Inform.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/IpplInfo.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/IpplInfo.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/IpplTimings.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/IpplTimings.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/PAssert.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/PAssert.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/Timer.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/Timer.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/Unique.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/Unique.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/User.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/User.cpp.o.d"
+  "CMakeFiles/ippl.dir/Utility/UserList.cpp.o"
+  "CMakeFiles/ippl.dir/Utility/UserList.cpp.o.d"
+  "libippl.a"
+  "libippl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ippl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
