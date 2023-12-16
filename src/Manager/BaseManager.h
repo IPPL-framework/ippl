fde22e682a7621d5478db4b05275b5fd93fd4fc4
Sadr Mohsen <sadr_m@merlin-l-001.psi.ch>
2023-12-16 11:06:20 +0100

    Add def. impl for dump, etc. Add doc

--- src/Manager/BaseManager.h
+++ src/Manager/BaseManager.h
@@ -6,26 +6,61 @@
 
 namespace ippl {
 
+    /**
+    * @class BaseManager
+    * @brief A base class for managing simulations using IPPL.
+    *
+    * The BaseManager class provides a basic structure for running simulations with common steps.
+    */
     class BaseManager {
     public:
         BaseManager()          = default;
         virtual ~BaseManager() = default;
 
+       /**
+        * @brief A method that should be used for setting up the simulation.
+        *
+        * Derived classes can override this method to allocate memory, initialize variables, etc.
+        * The default implementation does nothing.
+        */
         virtual void pre_run() { /* default does nothing */
         }
 
-        virtual void post_run() { /* default does nothing */
-        }
+       /**
+        * @brief A method that should be used for preparation before perfoming a step of simulation.
+        *
+        * The default implementation does nothing.
+        */
 
         virtual void pre_step() { /* default does nothing */
         }
 
+       /**
+        * @brief A method that should be used after perfoming a step of simulation.
+        *
+        * Derived classes can override this method to dump data, increment time, etc.
+        * The default implementation does nothing.
+        */
         virtual void post_step() { /* default does nothing */
         }
 
+       /**
+        * @brief A method that should be used to execute a step of simulation.
+        *
+        * Derived classes can override this method to implement their own governing equation.
+        * The default implementation does nothing.
+        */
         virtual void advance() { /* default does nothing */
         }
-        
+
+       /**
+        * @brief The main for loop fro running a simulation.
+        *
+        * This method performs a simulation run by calling pre_step, advance, and post_step
+        * in a loop for a specified number of time steps.
+        *
+        * @param nt The number of time steps to run the simulation.
+        */
         void run(int nt) {
             for (int it=0; it<nt; it++){
                 this->pre_step();
@@ -35,7 +70,7 @@ namespace ippl {
                 this->post_step();
             }
         }
-        
+
     };
 }  // namespace ippl
 
--- src/Manager/PicManager.h
+++ src/Manager/PicManager.h
@@ -8,6 +8,20 @@
 
  namespace ippl {
 
+    /**
+    * @class PicManager
+    * @brief A template class for managing Particle-in-Cell (PIC) simulations.
+    *
+    * The PicManager class is a template class that extends the functionality of the BaseManager class
+    * for Particle-in-Cell simulations. It provides methods for particle-to-grid and grid-to-particle operations,
+    * as well as a method for dumping simulation data.
+    *
+    * @tparam T The data type for simulation variables.
+    * @tparam Dim The dimensionality of the simulation (e.g., 2D or 3D).
+    * @tparam pc The particle container type.
+    * @tparam fc The field container type.
+    * @tparam orb The load balancer type.
+    */
     template <typename T, unsigned Dim, class pc, class fc, class orb>
     class PicManager : public BaseManager {
     public:
@@ -16,11 +30,29 @@
 
         virtual ~PicManager() = default;
 
-        virtual void par2grid() = 0;
+       /**
+        * @brief Particle-to-grid operation.
+        *
+        * Derived classes can override this method to perform particle-to-grid operations.
+        * The default implementation does nothing.
+        */
+        virtual void par2grid() { /* default does nothing */ };
 
-        virtual void grid2par() = 0;
+       /**
+        * @brief Grid-to-particle operation.
+        *
+        * Derived classes can override this method to perform grid-to-particle operations.
+        * The default implementation does nothing.
+        */
+        virtual void grid2par() { /* default does nothing */ };
 
-        virtual void dump() = 0;
+       /**
+        * @brief Dump simulation data.
+        *
+        * Derived classes can override this method to implement custom data dumping procedures.
+        * The default implementation does nothing.
+        */
+        virtual void dump() { /* default does nothing */ };
 
         inline std::shared_ptr<pc> getParticleContainer() {
             return pcontainer_m;
