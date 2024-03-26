22501ef1003be09838bfe0cbeb3f0990cecf0805
Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2024-03-26 16:07:02 +0100

    Remove code for skipping serial tests

--- unit_tests/Meshes/UniformCartesian.cpp
+++ unit_tests/Meshes/UniformCartesian.cpp
@@ -83,7 +83,6 @@ TYPED_TEST(UniformCartesianTest, Initialize) {
 
 int main(int argc, char* argv[]) {
     int success = 1;
-    TestParams::checkArgs(argc, argv);
     ippl::initialize(argc, argv);
     {
         ::testing::InitGoogleTest(&argc, argv);
