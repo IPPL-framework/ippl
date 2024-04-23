4f4ac1bc4f8cbef04461a4345796f3c9ad9bfc0c
Carla Lopez <calopez@student.ethz.ch>
2024-04-23 17:26:34 +0200

    comment on data folder

--- alpine/VortexInCell.cpp
+++ alpine/VortexInCell.cpp
@@ -16,6 +16,8 @@
 //     ovfactor = Over-allocation factor for the buffers used in the communication. Typical
 //                values are 1.0, 2.0. Value 1.0 means no over-allocation.
 //     Example:
+//     makdir build_*/alpine/data
+//     chmod +x data
 //     srun ./VortexInCell 128 128 128 10000 10 FFT 0.01 LeapFrog --overallocate 2.0 --info 10
 
 constexpr unsigned Dim = 3;
