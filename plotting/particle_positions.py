69a03ccc4114db504157d3b280a65a8502aab209
Carla Lopez <calopez@student.ethz.ch>
2024-04-30 19:33:03 +0200

    change color map

--- plotting/particle_positions.py
+++ plotting/particle_positions.py
@@ -20,7 +20,7 @@ ax.set_xlim((0,10))
 ax.set_ylim((0,10))
 
 # Initialize scatter plot with empty data and color map
-scat = ax.scatter([], [], c=[], s=2)
+scat = ax.scatter([], [], c=[], s=2, cmap='viridis_r')
 
 def update(frame):
     current_time = times[frame]
