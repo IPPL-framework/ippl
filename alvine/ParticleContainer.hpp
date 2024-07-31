4a673ee3c6b2de596f541225143f058c7e7b631c
Carla Lopez <calopez@student.ethz.ch>
2024-07-31 13:59:45 +0200

    add particle container 3d

--- alvine/ParticleContainer.hpp
+++ alvine/ParticleContainer.hpp
@@ -57,7 +57,7 @@ class ParticleContainer<T, 2> : public ParticleContainerBase<T, 2> {
 public:
     ippl::ParticleAttrib<T> omega;
 
-    ~ParticleContainer(){};
+    ~ParticleContainer() {};
 
     ParticleContainer(Mesh_t<2>& mesh, FieldLayout_t<2>& FL)
         : ParticleContainerBase<T, Dim>(mesh, FL) {
@@ -73,13 +73,13 @@ public:
 
     void dump(double it) override {
         Inform csvout(NULL, "particles.csv", Inform::APPEND);
-        
+
         for (unsigned i = 0; i < this->getTotalNum(); i++) {
-          csvout << it << "," << i; 
-          for (unsigned d = 0; d < Dim; d++) {
-            csvout << "," << this->R(i)[d];
-          }
-          csvout << "," << this->omega(i) << endl;
+            csvout << it << "," << i;
+            for (unsigned d = 0; d < Dim; d++) {
+                csvout << "," << this->R(i)[d];
+            }
+            csvout << "," << this->omega(i) << endl;
         }
     }
 
@@ -87,6 +87,61 @@ private:
     void registerAttributes() {
         this->addAttribute(omega);
         std::cout << "two dim register attributes" << std::endl;
+    }
+};
+
+template <typename T>
+class ParticleContainer<T, 3> : public ParticleContainerBase<T, 3> {
+public:
+    ippl::ParticleAttrib<T> omega_x;
+    ippl::ParticleAttrib<T> omega_y;
+    ippl::ParticleAttrib<T> omega_z;
+
+    ~ParticleContainer() {};
+
+    ParticleContainer(Mesh_t<3>& mesh, FieldLayout_t<3>& FL)
+        : ParticleContainerBase<T, Dim>(mesh, FL) {
+        registerAttributes();
+    }
+
+    void initDump() override {
+        Inform csvout(NULL, "particles.csv", Inform::OVERWRITE);
+        csvout.precision(16);
+        csvout.setf(std::ios::scientific, std::ios::floatfield);
+        csvout << "time,index,pos_x,pos_y,pos_z,vor_x,vor_y,vor_z" << endl;
+    }
+
+    ippl::ParticleAttrib<Vector<T, 1>> get_R(int d) {
+        ippl::ParticleAttrib<Vector<T, 1>> R_k;
+        const auto pcount = this->getTotalNum();
+        R_k.realloc(pcount);
+
+        Kokkos::parallel_for(
+            "Copy phase space", pcount,
+            KOKKOS_CLASS_LAMBDA(const size_t i) { R_k(i) = this->R(i)[d]; });
+        return R_k;
+    }
+
+    void dump(double it) override {
+        Inform csvout(NULL, "particles.csv", Inform::APPEND);
+
+        for (unsigned i = 0; i < this->getTotalNum(); i++) {
+            csvout << it << "," << i;
+            for (unsigned d = 0; d < Dim; d++) {
+                csvout << "," << this->R(i)[d];
+            }
+            csvout << "," << this->omega_x(i);
+            csvout << "," << this->omega_y(i);
+            csvout << "," << this->omega_z(i) << endl;
+        }
+    }
+
+private:
+    void registerAttributes() {
+        this->addAttribute(omega_x);
+        this->addAttribute(omega_y);
+        this->addAttribute(omega_z);
+        std::cout << "three dim register attributes" << std::endl;
     }
 };
 
