5a5d328605cf0bc263e94cc819fd9172e8cb56da
Félix Vittori <felix@Felixs-MacBook-Pro.local>
2024-08-10 14:44:12 +0200

    precommit hook

--- cosmology/StructureFormation.cpp
+++ cosmology/StructureFormation.cpp
@@ -1,11 +1,11 @@
 // StructureFormation Test
 //
-// The StructureFormation simulation is designed to model the formation and evolution of cosmic structures, 
-// such as galaxies and clusters of galaxies, in the universe. 
-// It uses initial conditions, including particle positions and velocities, 
-// to simulate the gravitational interactions and dynamics of a large number of particles over time.
-// The goal is to understand how initial density fluctuations grow and evolve under the influence of gravity, 
-// leading to the large-scale structure observed in the universe today.
+// The StructureFormation simulation is designed to model the formation and evolution of cosmic
+// structures, such as galaxies and clusters of galaxies, in the universe. It uses initial
+// conditions, including particle positions and velocities, to simulate the gravitational
+// interactions and dynamics of a large number of particles over time. The goal is to understand how
+// initial density fluctuations grow and evolve under the influence of gravity, leading to the
+// large-scale structure observed in the universe today.
 //
 //   Usage:
 //     srun ./StructureFormation
@@ -14,9 +14,8 @@
 //     path     = path to initial conditions folder containing the file Data.csv
 //     nx       = No. cell-centered points in the x-direction
 //     ny...    = No. cell-centered points in the y-, z-, ...-direction
-//     Np       = Total no. of macro-particles in the simulation. Needs to match the IC file Data.csv
-//     Nt       = Number of time steps
-//     stype    = Field solver type (FFT and CG supported)
+//     Np       = Total no. of macro-particles in the simulation. Needs to match the IC file
+//     Data.csv Nt       = Number of time steps stype    = Field solver type (FFT and CG supported)
 //     lbthres  = Load balancing threshold i.e., lbthres*100 is the maximum load imbalance
 //                percentage which can be tolerated and beyond which
 //                particle load balancing occurs. A value of 0.01 is good for many typical
@@ -24,14 +23,14 @@
 //     ovfactor = Over-allocation factor for the buffers used in the communication. Typical
 //                values are 1.0, 2.0. Value 1.0 means no over-allocation.
 //     Example:
-//     srun ./StructureFormation data/lsf_32/ 32 32 32 32768 10 FFT 1.0 LeapFrog --overallocate 1.0 --info 5 
-
-
-
+//     srun ./StructureFormation data/lsf_32/ 32 32 32 32768 10 FFT 1.0 LeapFrog --overallocate 1.0
+//     --info 5
 
 constexpr unsigned Dim = 3;
-using T = double;
-const char* TestName = "StructureFormation";
+using T                = double;
+const char* TestName   = "StructureFormation";
+
+#include "Ippl.h"
 
 #include <Kokkos_MathematicalConstants.hpp>
 #include <Kokkos_MathematicalFunctions.hpp>
@@ -43,20 +42,17 @@ const char* TestName = "StructureFormation";
 #include <string>
 #include <vector>
 
-#include "Ippl.h"
-#include "Utility/IpplTimings.h"
-#include "Manager/PicManager.h"
 #include "datatypes.h"
-#include "StructureFormationManager.h"
 
+#include "Utility/IpplTimings.h"
+
+#include "Manager/PicManager.h"
+#include "StructureFormationManager.h"
 
 using size_type = ippl::detail::size_type;
 
 template <typename T, unsigned Dim>
 using Vector_t = ippl::Vector<T, Dim>;
-
-
-
 
 int main(int argc, char* argv[]) {
     ippl::initialize(argc, argv);
@@ -69,7 +65,7 @@ int main(int argc, char* argv[]) {
 
         // Read input parameters, assign them to the corresponding members of manager
         int arg = 1;
-        // initial conditions folder 
+        // initial conditions folder
         std::string ic_folder = argv[arg++];
         // Number of gridpoints in each dimension
         Vector_t<int, Dim> nr;
@@ -79,8 +75,8 @@ int main(int argc, char* argv[]) {
         // Total number of particles
         size_type totalP = std::atoll(argv[arg++]);
         // Number of time steps
-        int nt  = std::atoi(argv[arg++]);
-        
+        int nt = std::atoi(argv[arg++]);
+
         // Solver method
         std::string solver = argv[arg++];
         // Check if the solver type is valid
@@ -94,7 +90,7 @@ int main(int argc, char* argv[]) {
 
         // Create an instance of a manager for the considered application
         StructureFormationManager<T, Dim> manager(totalP, nt, nr, lbt, solver, step_method);
-        
+
         // set initial conditions folder
         manager.setIC(ic_folder);
 
@@ -111,7 +107,7 @@ int main(int argc, char* argv[]) {
         IpplTimings::print();
 
         std::stringstream ss;
-        ss << "timing_" << ippl::Comm->size() << ".dat"; 
+        ss << "timing_" << ippl::Comm->size() << ".dat";
         std::string filename = ss.str();
 
         IpplTimings::print(manager.folder + filename);
--- cosmology/datatypes.h
+++ cosmology/datatypes.h
@@ -21,10 +21,10 @@ using size_type = ippl::detail::size_type;
 template <typename T, unsigned Dim>
 using Vector = ippl::Vector<T, Dim>;
 
-template <typename T, unsigned Dim= 3, class... ViewArgs>
+template <typename T, unsigned Dim = 3, class... ViewArgs>
 using Field = ippl::Field<T, Dim, Mesh_t<Dim>, Centering_t<Dim>, ViewArgs...>;
 
-template <typename T = double, unsigned Dim=3>
+template <typename T = double, unsigned Dim = 3>
 using ORB = ippl::OrthogonalRecursiveBisection<Field<double, Dim>, T>;
 
 template <typename T>
@@ -36,7 +36,7 @@ using Vector_t = ippl::Vector<T, Dim>;
 template <unsigned Dim, class... ViewArgs>
 using Field_t = Field<double, Dim, ViewArgs...>;
 
-template <typename T = double, unsigned Dim=3, class... ViewArgs>
+template <typename T = double, unsigned Dim = 3, class... ViewArgs>
 using VField_t = Field<Vector_t<T, Dim>, Dim, ViewArgs...>;
 
 template <typename T = double, unsigned Dim = 3>
