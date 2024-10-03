3aa99f991e2430de7f5db03278d7109a5b11b551
Jonas Meier <jomethun@gmx.ch>
2024-10-03 14:39:59 +0200

    made bufferhandler more readable

--- src/Communicate/BufferHandler.h
+++ src/Communicate/BufferHandler.h
@@ -36,59 +36,31 @@ public:
     buffer_type getBuffer(size_type size, double overallocation) override {
         size_type requiredSize = static_cast<size_type>(size * overallocation);
 
-        auto it = findSmallestSufficientBuffer(requiredSize);
+        auto freeBuffer = findFreeBuffer(requiredSize);
 
-        if (it != free_buffers.end()) {
-            buffer_type buffer = *it;
-
-            freeSize -= buffer->getBufferSize();
-            allocatedSize += buffer->getBufferSize();
-
-            free_buffers.erase(buffer);
-            used_buffers.insert(buffer);
-            return buffer;
+        if (freeBuffer != nullptr) {
+            return allocateFromFreeBuffer(freeBuffer);
         }
 
         if (!free_buffers.empty()) {
-            auto largest_it    = std::prev(free_buffers.end());
-            buffer_type buffer = *largest_it;
-
-            freeSize -= buffer->getBufferSize();
-            allocatedSize += requiredSize;
-
-            free_buffers.erase(buffer);
-            buffer->reallocBuffer(requiredSize);
-
-            used_buffers.insert(buffer);
-            return buffer;
+            return reallocateLargestFreeBuffer(requiredSize);
         }
 
-        buffer_type newBuffer = std::make_shared<archive_type>(requiredSize);
-
-        allocatedSize += newBuffer->getBufferSize();
-
-        used_buffers.insert(newBuffer);
-        return newBuffer;
+        return allocateNewBuffer(requiredSize);
     }
 
     void freeBuffer(buffer_type buffer) override {
-        auto it = used_buffers.find(buffer);
-        if (it != used_buffers.end()) {
-
-            allocatedSize -= (*it)->getBufferSize();
-            freeSize += (*it)->getBufferSize();
-
-            used_buffers.erase(*it);
-            free_buffers.insert(*it);
+        if (isBufferUsed(buffer)) {
+            releaseUsedBuffer(buffer);
         }
     }
 
     void freeAllBuffers() override {
-        freeSize += allocatedSize;
-        allocatedSize = 0;
-        
         free_buffers.insert(used_buffers.begin(), used_buffers.end());
         used_buffers.clear();
+
+        freeSize += allocatedSize;
+        allocatedSize = 0;
     }
 
     void deleteAllBuffers() override {
@@ -99,32 +71,85 @@ public:
         free_buffers.clear();
     }
 
-  size_type getAllocatedSize() const override {
-    return allocatedSize;
-  }
+    size_type getAllocatedSize() const override {
+      return allocatedSize;
+    }
 
-  size_type getFreeSize() const override {
-    return freeSize;
-  }
+    size_type getFreeSize() const override {
+      return freeSize;
+    }
 
 private:
-    struct BufferComparator {
-        bool operator()(const buffer_type& lhs, const buffer_type& rhs) const {
-            // First compare by size
-            if (lhs->getBufferSize() != rhs->getBufferSize()) {
-                return lhs->getBufferSize() < rhs->getBufferSize();
-            }
-            // If sizes are equal, compare by address
-            return lhs < rhs;
-        }
-    };
+    using buffer_comparator_type = bool (*)(const buffer_type&, const buffer_type&);
 
-    typename std::set<buffer_type, BufferComparator>::iterator findSmallestSufficientBuffer(
+    static bool bufferSizeComparator(const buffer_type& lhs, const buffer_type& rhs) {
+        // Compare by size first, then by memory address
+        if (lhs->getBufferSize() != rhs->getBufferSize()) {
+            return lhs->getBufferSize() < rhs->getBufferSize();
+        }
+        return lhs < rhs;
+    }
+
+    bool isBufferUsed(buffer_type buffer) const {
+        return used_buffers.find(buffer) != used_buffers.end();
+    }
+
+    void releaseUsedBuffer(buffer_type buffer) {
+        auto it = used_buffers.find(buffer);
+        
+        allocatedSize -= buffer->getBufferSize();
+        freeSize += buffer->getBufferSize();
+
+        used_buffers.erase(it);
+        free_buffers.insert(buffer);
+    }
+
+    buffer_type findFreeBuffer(size_type requiredSize) {
+        auto it = findSmallestSufficientBuffer(requiredSize);
+        if (it != free_buffers.end()) {
+            return *it;
+        }
+        return nullptr;
+    }
+
+    typename std::set<buffer_type, buffer_comparator_type>::iterator findSmallestSufficientBuffer(
         size_type requiredSize) {
         return std::find_if(free_buffers.begin(), free_buffers.end(),
                             [requiredSize](const buffer_type& buffer) {
                                 return buffer->getBufferSize() >= requiredSize;
                             });
+    }
+
+
+    buffer_type allocateFromFreeBuffer(buffer_type buffer) {
+        freeSize -= buffer->getBufferSize();
+        allocatedSize += buffer->getBufferSize();
+
+        free_buffers.erase(buffer);
+        used_buffers.insert(buffer);
+        return buffer;
+    }
+
+    buffer_type reallocateLargestFreeBuffer(size_type requiredSize) {
+        auto largest_it = std::prev(free_buffers.end());
+        buffer_type buffer = *largest_it;
+
+        freeSize -= buffer->getBufferSize();
+        allocatedSize += requiredSize;
+
+        free_buffers.erase(buffer);
+        buffer->reallocBuffer(requiredSize);
+
+        used_buffers.insert(buffer);
+        return buffer;
+    }
+
+    buffer_type allocateNewBuffer(size_type requiredSize) {
+        buffer_type newBuffer = std::make_shared<archive_type>(requiredSize);
+
+        allocatedSize += newBuffer->getBufferSize();
+        used_buffers.insert(newBuffer);
+        return newBuffer;
     }
     
   size_type allocatedSize;
@@ -132,8 +157,8 @@ private:
 
 
 protected:
-    std::set<buffer_type, BufferComparator> used_buffers;
-    std::set<buffer_type, BufferComparator> free_buffers;
+    std::set<buffer_type, buffer_comparator_type> used_buffers{&BufferHandler::bufferSizeComparator};
+    std::set<buffer_type, buffer_comparator_type> free_buffers{&BufferHandler::bufferSizeComparator};
 
 };
 
