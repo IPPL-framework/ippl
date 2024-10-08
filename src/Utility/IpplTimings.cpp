f6fbb380f7b7503c4c0b6abdfabb727c9d2e0a16
Andreas Adelmann <aaadelmann@users.noreply.github.com>
2024-10-08 15:35:19 +0200

    Update IpplTimings.cpp

--- src/Utility/IpplTimings.cpp
+++ src/Utility/IpplTimings.cpp
@@ -32,7 +32,7 @@
 
 #include "Utility/Inform.h"
 #include "Utility/IpplInfo.h"
-
+#define Kokkos_ENABLE_CUDA 1
 #ifdef Kokkos_ENABLE_CUDA      // \todo need a better macro 
  #include <nvtx3/nvToolsExt.h> 
 #endif
