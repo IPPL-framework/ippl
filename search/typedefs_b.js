d3b66e42727d670a965781ab92c37c0543b97508
s-mayani <s-mayani@users.noreply.github.com>
2025-01-31 08:45:32 +0000

    deploy: 9b0bde600302d907a035ccb2d3819e70576f4c50

--- search/typedefs_b.js
+++ search/typedefs_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['ndindex_5ft_0',['NDIndex_t',['../classippl_1_1FieldLayout.html#ac0e42dbf29d3b220b0652cb8559f7a4d',1,'ippl::FieldLayout']]],
   ['ndregion_5ft_1',['NDRegion_t',['../classippl_1_1detail_1_1RegionLayout.html#adfe1c0b9e8d63213cd9667e282621e85',1,'ippl::detail::RegionLayout']]],
   ['neighbor_5flist_2',['neighbor_list',['../classippl_1_1FieldLayout.html#a489d6f8b884f8193807d7704d5e106a6',1,'ippl::FieldLayout::neighbor_list()'],['../classippl_1_1ParticleSpatialLayout.html#aec1de04a618491ccc01f506351891ae1',1,'ippl::ParticleSpatialLayout::neighbor_list()']]],
-  ['neighbor_5frange_5flist_3',['neighbor_range_list',['../classippl_1_1FieldLayout.html#ab4004e828ab9973447bdc381afa90d69',1,'ippl::FieldLayout']]]
+  ['neighbor_5frange_5flist_3',['neighbor_range_list',['../classippl_1_1FieldLayout.html#ab4004e828ab9973447bdc381afa90d69',1,'ippl::FieldLayout']]],
+  ['nullsolver_5ft_4',['NullSolver_t',['../FieldSolverBase_8h.html#ace8fabc88f00df2e15d636f24772fcef',1,'NullSolver_t():&#160;FieldSolverBase.h'],['../datatypes_8h.html#ace8fabc88f00df2e15d636f24772fcef',1,'NullSolver_t():&#160;datatypes.h']]]
 ];
