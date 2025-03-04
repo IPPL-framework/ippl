e07f17c4d94aa58f9ce2d83f2f169d94e4e47d63
Andreas Adelmann <andreas.adelmann@psi.ch>
2025-03-05 00:18:43 +0200

    new test

--- test/mpi/TestCommunicator.cpp
+++ test/mpi/TestCommunicator.cpp
@@ -6,148 +6,124 @@
 #include "Communicate/Serializable.h"
 #include "Communicate/Wait.h"
 #include "Communicate/Window.h"
+#include "Communicate/Communicator.h"
 
 int main(int argc, char* argv[]) {
     ippl::initialize(argc, argv);
     {
-        //         int rank = -1;
-        //         MPI_Comm comm = *ippl::Comm.get();
-        //
-        //         MPI_Comm_rank(comm, &rank);
-        //
-        //         std::cout << "First " << rank << " " << ippl::Comm->rank() << std::endl;
-        //
-        //         ippl::mpi::Communicator comm2;
-        //         comm = comm2;
-        //         MPI_Comm_rank(comm, &rank);
-        //         std::cout << "Second " << rank << " " << ippl::Comm->rank() << std::endl;
-        //
-        //
-        //         double value = 0;
-        //
-        //         if (ippl::Comm->rank() == 0) {
-        //             value = 10;
-        //             ippl::Comm->isend(value, 1, 1, 42, requests[0]);
-        //         } else if (ippl::Comm->rank() == 1) {
-        //             ippl::Comm->irecv(value, 1, 0, 42, requests[0]);
-        //         }
-        //
-        //         ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
-        //
-        //         std::cout << ippl::Comm->rank() << " status tag    " << statuses[0].tag() <<
-        //         std::endl; std::cout << ippl::Comm->rank() << " status source " <<
-        //         statuses[0].source() << std::endl; int cnt = statuses[0].count<double>().value();
-        //         std::cout << ippl::Comm->rank() << " status count " << cnt << std::endl;
-        //
-        //         if (ippl::Comm->rank() == 0) {
-        //             std::cout << "rank 0 " << value << std::endl;
-        //         } else {
-        //             std::cout << "rank 1 " << value << std::endl;
-        //         }
+      Inform msg("TestCommunicator:: ",INFORM_ALL_NODES);
+      int rank, size;
+      rank = ippl::Comm->rank(); size = ippl::Comm->size();
+      
+      constexpr int data_size = 1000;
+      std::vector<double> send_data(data_size);
+      std::vector<double> recv_data(data_size, 0.0);
+      MPI_Request send_request, recv_request;
 
-        //         std::vector<ippl::mpi::Request> requests(2);
-        //         std::vector<ippl::mpi::Status> statuses(2);
-        //
-        //         int spaceProcs = 2;
-        //         int spaceColor = ippl::Comm->rank() / spaceProcs;
-        //         int timeColor = ippl::Comm->rank() % spaceProcs;
-        //
-        //         ippl::mpi::Communicator spaceComm = ippl::Comm->split(spaceColor,
-        //         ippl::Comm->rank()); ippl::mpi::Communicator timeComm =
-        //         ippl::Comm->split(timeColor, ippl::Comm->rank());
-        //
-        //         double value[2] = {0, 0};
-        //
-        //         if (spaceComm.rank() == 0) {
-        //             value[0] = 10 * (ippl::Comm->rank() + 1);
-        //             spaceComm.isend(value[0], 1, 1, 42, requests[0]);
-        //         } else if (spaceComm.rank() == 1) {
-        //             spaceComm.irecv(value[0], 1, 0, 42, requests[0]);
-        //         }
-        //
-        //         if (timeComm.rank() == 0) {
-        //             value[1] = 20 * (ippl::Comm->rank() + 1);
-        //             timeComm.isend(value[1], 1, 1, 42, requests[1]);
-        //         } else if (timeComm.rank() == 1) {
-        //             timeComm.irecv(value[1], 1, 0, 42, requests[1]);
-        //         }
-        //
-        //         ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
-        //
-        //         if (spaceComm.rank() == 0) {
-        //             std::cout << "space comm rank 0 value = " << value[0] << std::endl;
-        //         } else if (spaceComm.rank() == 1) {
-        //             std::cout << "space comm rank 1 value = " << value[0] << std::endl;
-        //         }
-        //
-        //         if (timeComm.rank() == 0) {
-        //             std::cout << "time  comm rank 0 value = " << value[1] << std::endl;
-        //         } else if (timeComm.rank() == 1) {
-        //             std::cout << "time  comm rank 1 value = " << value[1] << std::endl;
-        //         }
+      // Determine source and destination ranks in a circular pattern
+      int send_to = (rank + 1) % size;
+      int recv_from = (rank - 1 + size) % size;
+      
+      // Initialize data
+      for (int i = 0; i < data_size; ++i) {
+        send_data[i] = static_cast<double>(rank * 1000 + i); // Unique data per rank
+      }
 
-        //         ippl::Comm->barrier();
-        //
+      std::vector<MPI_Request> send_requests(1);
+      
+      int tag = 42;
+      
+      using memory_space=Kokkos::HostSpace;
+	
+      using buffer_type  = ippl::mpi::Communicator::buffer_type<memory_space>;
 
-        std::cout << ippl::mpi::is_serializable<std::vector<bool> >::value << std::endl;
+      buffer_type buf = ippl::Comm->template getBuffer<memory_space, double>(data_size);
 
-        //         MPI_Win win;
-        ippl::mpi::rma::Window<ippl::mpi::rma::Active> win;
+      
+      ippl::Comm->isend(send_to, tag, send_data.data(), *buf, send_requests.back(), data_size); // ippl::Comm->isend(send_data.data(), data_size, send_to, tag, send_requests.back());
+      
+      //      MPI_Isend(send_data.data(), data_size, MPI_DOUBLE, send_to, 0, MPI_COMM_WORLD, &send_request);
+      //      MPI_Irecv(recv_data.data(), data_size, MPI_DOUBLE, recv_from, 0, MPI_COMM_WORLD, &recv_request);
 
-        std::vector<double> mem(4);
-        std::vector<double> mem2(4);
+      // Wait for both operations to complete
+      //      MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+      //      MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
 
-        if (ippl::Comm->rank() == 1) {
-            mem2[0] = 42;
-            mem2[1] = 88;
-            mem2[2] = 12;
-            mem2[3] = 3;
-        }
+      // Print verification message
+      msg << " Sent data to Rank " << send_to
+	  << " and received data from Rank " << recv_from
+	  << ". First received element: " << recv_data[0]
+	  << ", Last: " << recv_data[data_size - 1] << endl;;
+      
+    }
+    ippl::finalize();
+}
 
-        //         MPI_Win_allocate((MPI_Aint)(4 * sizeof(double)), sizeof(double),
-        //                  MPI_INFO_NULL, MPI_COMM_WORLD, &mem, &win);
+/*
+      int val = 0;
+        
+      if (ippl::Comm->rank() == 0) {
+	val = 10;
+	ippl::Comm->isend(val, 1, 1, 42, requests[0],1);
+      } else if (ippl::Comm->rank() == 1) {
+	ippl::Comm->irecv(val, 1, 0, 42, requests[0]);
+      }
+        
+      ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
+        
+      std::cout << ippl::Comm->rank() << " status tag    " << statuses[0].tag() <<
+	std::endl; std::cout << ippl::Comm->rank() << " status source " << statuses[0].source() << std::endl;
+      int cnt = statuses[0].count<double>().value();
+      std::cout << ippl::Comm->rank() << " status count " << cnt << std::endl;
+      
+      if (ippl::Comm->rank() == 0) {
+	std::cout << "rank 0 " << val << std::endl;
+      } else {
+	std::cout << "rank 1 " << val << std::endl;
+      }
 
-        //         win.create(*ippl::Comm, mem.begin(), mem.end());
+        
+      int spaceProcs = 2;
+      int spaceColor = ippl::Comm->rank() / spaceProcs;
+      int timeColor = ippl::Comm->rank() % spaceProcs;
+        
+      ippl::mpi::Communicator spaceComm = ippl::Comm->split(spaceColor, ippl::Comm->rank());
+      ippl::mpi::Communicator timeComm =  ippl::Comm->split(timeColor,  ippl::Comm->rank());
+        
+      int value[2] = {0, 0};
+        
+      if (spaceComm.rank() == 0) {
+	value[0] = 10 * (ippl::Comm->rank() + 1);
+	spaceComm.isend(value[0], 1, 1, 42, requests[0],1);
+      } else if (spaceComm.rank() == 1) {
+	spaceComm.irecv(value[0], 1, 0, 42, requests[0]);
+      }
+      
+      if (timeComm.rank() == 0) {
+	value[1] = 20 * (ippl::Comm->rank() + 1);
+	timeComm.isend(value[1], 1, 1, 42, requests[1],1);
+      } else if (timeComm.rank() == 1) {
+	timeComm.irecv(value[1], 1, 0, 42, requests[1]);
+      }
+      
+      ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
+        
+      if (spaceComm.rank() == 0) {
+	std::cout << "space comm rank 0 value = " << value[0] << std::endl;
+      } else if (spaceComm.rank() == 1) {
+	std::cout << "space comm rank 1 value = " << value[0] << std::endl;
+      }
+      
+      if (timeComm.rank() == 0) {
+	std::cout << "time  comm rank 0 value = " << value[1] << std::endl;
+      } else if (timeComm.rank() == 1) {
+	std::cout << "time  comm rank 1 value = " << value[1] << std::endl;
+      }
 
-        win.attach(*ippl::Comm, mem.begin(), mem.end());
-
-        //         win.fence(0);
-
-        //         if (ippl::Comm->rank() == 0) {
-        //             win.get(mem.begin(), mem.end(), 1, 0);
-        //         }
-
-        //         win.fence(0);
-
-        std::cout << ippl::Comm->rank() << " data: " << mem[0] << " " << mem[1] << " " << mem[2]
-                  << " " << mem[3] << std::endl;
-
-        ippl::Comm->barrier();
-
-        win.fence(0);
-        //         MPI_Win_fence(0, win);
-
-        if (ippl::Comm->rank() == 1) {
-            //             mem2[2] = 100;
-            //             win.put<double>(mem2.data() + 2, 1, 2);
-            win.put(mem2.begin(), mem2.end(), 0, 0);
-            //             MPI_Put(mem2.data()+ 2, 1, MPI_DOUBLE, 1, 2, 1, MPI_DOUBLE, win);
-        }
-
-        win.fence(0);
-        //         MPI_Win_fence(0, win);
-
-        std::cout << "after put " << ippl::Comm->rank() << " data: " << mem[0] << " " << mem[1]
-                  << " " << mem[2] << " " << mem[3] << std::endl;
-
-        // // 0 data: 0 0 0 0
-        // // after put 0 data: 0 0 0 0
-        // // 1 data: 42 88 12 3
-        // // after put 1 data: 42 88 100 3
-
-        win.detach(mem.begin());
+      ippl::Comm->barrier();
     }
     ippl::finalize();
 
     return 0;
 }
+*/
