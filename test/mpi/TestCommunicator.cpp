53ac848a5238ab5a6560595d4c26b5114c8c61f9
Andreas Adelmann <andreas.adelmann@psi.ch>
2025-03-05 23:33:24 +0200

    ok on all ranks of one node. Does not work with -N2

--- test/mpi/TestCommunicator.cpp
+++ test/mpi/TestCommunicator.cpp
@@ -32,102 +32,87 @@ struct TestParticles : public ippl::ParticleBase<PLayout> {
 int main(int argc, char* argv[]) {
     ippl::initialize(argc, argv);
     {
-      Inform msg("TestCommunicator:: ",INFORM_ALL_NODES);
+      Inform msg2all("TestCommunicator:: ",INFORM_ALL_NODES);
+      Inform msg("TestCommunicator:: ");
       int rank, size;
       rank = ippl::Comm->rank(); size = ippl::Comm->size();
 
+      msg << "HostSpace available: "
+		<< Kokkos::SpaceAccessibility<Kokkos::DefaultExecutionSpace, Kokkos::HostSpace>::accessible
+		<< endl;
+
+            msg << "HIPSpace available: "
+		<< Kokkos::SpaceAccessibility<Kokkos::DefaultExecutionSpace, Kokkos::HIPSpace>::accessible
+		<< endl;
+
       constexpr int data_size = 10;
 
-        int pt = 512;
-        ippl::Index I(pt);
-        ippl::NDIndex<3> owned(I, I, I);
+      int pt = 512;
+      ippl::Index I(pt);
+      ippl::NDIndex<3> owned(I, I, I);
+      
+      std::array<bool, 3> isParallel;
+      isParallel.fill(true);
 
-        std::array<bool, 3> isParallel;
-        isParallel.fill(true);
+      // all parallel layout, standard domain, normal axis order
+      ippl::FieldLayout<3> layout(MPI_COMM_WORLD, owned, isParallel);
 
-        // all parallel layout, standard domain, normal axis order
-        ippl::FieldLayout<3> layout(MPI_COMM_WORLD, owned, isParallel);
+      double dx                      = 1.0 / double(pt);
+      ippl::Vector<double, 3> hx     = {dx, dx, dx};
+      ippl::Vector<double, 3> origin = {0, 0, 0};
+      Mesh_t mesh(owned, hx, origin);
+      
+      playout_type pl(layout, mesh);
 
-        double dx                      = 1.0 / double(pt);
-        ippl::Vector<double, 3> hx     = {dx, dx, dx};
-        ippl::Vector<double, 3> origin = {0, 0, 0};
-        Mesh_t mesh(owned, hx, origin);
+      TestParticles<playout_type> particles(pl);
 
-        playout_type pl(layout, mesh);
+      particles.create(data_size);
 
-        TestParticles<playout_type> particles(pl);
-
-        particles.create(data_size);
-
-      MPI_Request send_request; //, recv_request;
+      MPI_Request send_request; 
 
       // Determine source and destination ranks in a circular pattern
       int send_to = (rank + 1) % size;
       int recv_from = (rank - 1 + size) % size;
 
-      // Initialize data
-      for (int i = 0; i < data_size; ++i) {
-        particles.send_data(i) = static_cast<double>(rank * data_size + i); // Unique data per rank
-        particles.recv_data(i) = 0.0;
-      }
+      Kokkos::parallel_for("InitArray", data_size, KOKKOS_LAMBDA(const int i) {
+	  particles.send_data(i) = static_cast<double>(rank * data_size + i); // Unique data per rank
+	  particles.recv_data(i) = 0.0;
+        });
 
+      
       int tag = 42;
 
-      using memory_space=Kokkos::HostSpace;
+      using memory_space=Kokkos::HIPSpace;
 
-//       using buffer_type  = ippl::mpi::Communicator::buffer_type<memory_space>;
+      using buffer_type  = ippl::mpi::Communicator::buffer_type<memory_space>;
 
-//       buffer_type buf = ippl::Comm->getBuffer<memory_space, double>(data_size);
+      buffer_type buf = ippl::Comm->getBuffer<memory_space, double>(data_size);
 
       ippl::detail::Archive<memory_space> ar(sizeof(double)*data_size);
+      ippl::detail::hash_type<memory_space> hash("hash", data_size);
 
-//       std::cout << buf->getSize() << std::endl;
+      // mark which data is sent -- we send everything
+      Kokkos::parallel_for("InitArray", data_size, KOKKOS_LAMBDA(const int i) {
+	  hash(i) = i;
+	});
 
-      if (rank == 0) {
-            ippl::detail::hash_type<memory_space> hash("hash", data_size);
+      particles.send_data.pack(hash);
+	
+      ippl::Comm->isend(send_to, tag, particles.send_data, ar, send_request, data_size);
+      MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+      
+      ippl::Comm->recv(recv_from, tag, particles.recv_data, ar, sizeof(double)*data_size, data_size);
 
-            // mark which data is sent -- we send everything
-            for(int i = 0; i < data_size; ++i) {
-                hash(i) = i;
-            }
+      particles.recv_data.unpack(data_size);
 
-            particles.send_data.pack(hash);
+      auto view = particles.recv_data.getView();
 
-            ippl::Comm->isend(send_to, tag, particles.send_data, ar, send_request, data_size);
+      auto hostView = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), view);
 
-            MPI_Wait(&send_request, MPI_STATUS_IGNORE);
-
-      } else if (rank == 1) {
-
-        ippl::Comm->recv(recv_from, tag, particles.recv_data, ar, sizeof(double)*data_size, data_size);
-
-        particles.recv_data.unpack(data_size);
-
-
-        auto view = particles.recv_data.getView();
-
-        // note that this rank receives 'data_size' values. That data is appended to the
-        // existing 'recv_data' attributes, therefore the size is doubled
-        for (int i = data_size; i < 2 * data_size; ++i) {
-            std::cout << view(i) << std::endl;
-        }
-
-//       ippl::Comm->isend(send_to, tag, particles.send_data, *buf, send_request, data_size);
-
-      //      MPI_Isend(send_data.data(), data_size, MPI_DOUBLE, send_to, 0, MPI_COMM_WORLD, &send_request);
-      //      MPI_Irecv(recv_data.data(), data_size, MPI_DOUBLE, recv_from, 0, MPI_COMM_WORLD, &recv_request);
-
-    }
-      // Wait for both operations to complete
-//         MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
-
-      // Print verification message
-      /*
-      msg << " Sent data to Rank " << send_to
-	  << " and received data from Rank " << recv_from
-	  << ". First received element: " << recv_data[0]
-	  << ", Last: " << recv_data[data_size - 1] << endl;;
-      */
+      msg2all << "Sent data to Rank " << send_to
+	      << " and received data from Rank " << recv_from 
+	      << " First received element: " << hostView[data_size]
+	      << ", Last: " << hostView[(2*data_size) - 1] << endl;
     }
     ippl::finalize();
 }
@@ -144,15 +129,15 @@ int main(int argc, char* argv[]) {
 
       ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
 
-      std::cout << ippl::Comm->rank() << " status tag    " << statuses[0].tag() <<
-	std::endl; std::cout << ippl::Comm->rank() << " status source " << statuses[0].source() << std::endl;
+      msg << ippl::Comm->rank() << " status tag    " << statuses[0].tag() <<
+	std::endl; msg << ippl::Comm->rank() << " status source " << statuses[0].source() << std::endl;
       int cnt = statuses[0].count<double>().value();
-      std::cout << ippl::Comm->rank() << " status count " << cnt << std::endl;
+      msg << ippl::Comm->rank() << " status count " << cnt << std::endl;
 
       if (ippl::Comm->rank() == 0) {
-	std::cout << "rank 0 " << val << std::endl;
+	msg << "rank 0 " << val << std::endl;
       } else {
-	std::cout << "rank 1 " << val << std::endl;
+	msg << "rank 1 " << val << std::endl;
       }
 
 
@@ -182,15 +167,15 @@ int main(int argc, char* argv[]) {
       ippl::mpi::waitall(requests.begin(), requests.end(), statuses.begin());
 
       if (spaceComm.rank() == 0) {
-	std::cout << "space comm rank 0 value = " << value[0] << std::endl;
+	msg << "space comm rank 0 value = " << value[0] << std::endl;
       } else if (spaceComm.rank() == 1) {
-	std::cout << "space comm rank 1 value = " << value[0] << std::endl;
+	msg << "space comm rank 1 value = " << value[0] << std::endl;
       }
 
       if (timeComm.rank() == 0) {
-	std::cout << "time  comm rank 0 value = " << value[1] << std::endl;
+	msg << "time  comm rank 0 value = " << value[1] << std::endl;
       } else if (timeComm.rank() == 1) {
-	std::cout << "time  comm rank 1 value = " << value[1] << std::endl;
+	msg << "time  comm rank 1 value = " << value[1] << std::endl;
       }
 
       ippl::Comm->barrier();
