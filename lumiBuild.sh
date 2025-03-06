979fccdf91dd34e050670bbe1e8584668d1f851a
Andreas Adelmann <andreas.adelmann@psi.ch>
2025-03-06 14:35:12 +0200

    for lumi

---
+++ lumiBuild.sh
@@ -0,0 +1,22 @@
+mkdir -p build && cd build && rm -rf *
+cmake .. \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_CXX_STANDARD=20 \
+      -DCMAKE_CXX_COMPILER=hipcc \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_HIP_ARCHITECTURES=gfx90a \
+      -DCMAKE_HIP_FLAGS=--offload-arch=gfx90a \
+      -DKokkos_ENABLE_DEBUG_BOUNDS_CHECK=ON \
+      -DKokkos_ENABLE_DEBUG=ON \
+      -DKokkos_ARCH_ZEN3=ON \
+      -DKokkos_ARCH_AMD_GFX90A=ON \
+      -DKokkos_ENABLE_HIP=ON \
+      -DIPPL_PLATFORMS="HIP;OPENMP" \
+      -DENABLE_TESTS=ON \
+      -DENABLE_FFT=ON  \
+      -DENABLE_SOLVERS=ON \
+      -DENABLE_ALPINE=OFF \
+      -DHeffte_ENABLE_ROCM=ON\
+      -DHeffte_ENABLE_GPU_AWARE_MPI=ON \
+      -DHeffte_ENABLE_ROCM=ON \
+      -DCMAKE_EXE_LINKER_FLAGS="-L/opt/cray/pe/mpich/8.1.28/ofi/amd/5.0/lib -L/opt/cray/pe/mpich/8.1.28/gtl/lib -L/opt/cray/pe/libsci/24.03.0/AMD/5.0/x86_64/lib -L/opt/cray/pe/dsmml/0.3.0/dsmml/lib -L/opt/cray/xpmem/2.8.2-1.0_5.1__g84a27a5.shasta/lib64 -lsci_amd_mpi -lsci_amd -ldl -lmpi_amd -lmpi_gtl_hsa -ldsmml -lxpmem -L/opt/rocm-6.0.3/lib/lib -L/opt/rocm-6.0.3/lib/lib64 -L/opt/rocm-6.0.3/lib/llvm/lib"
