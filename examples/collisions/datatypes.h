1039723143dce8f89a221578b0904c596b0e837a
Jonas Bachmann <jbachmann@ethz.ch>
2025-05-30 13:42:00 +0200

    fix templated pi

--- examples/collisions/datatypes.h
+++ examples/collisions/datatypes.h
@@ -62,6 +62,7 @@ template <typename T = double, unsigned Dim = 3>
 using Solver_t = VariantFromConditionalTypes<CGSolver_t<T, Dim>, FFTSolver_t<T, Dim>,
                                              P3MSolver_t<T, Dim>, OpenSolver_t<T, Dim>>;
 
+template <typename T = double>
 const double pi = Kokkos::numbers::pi_v<T>;
 
 extern const char* TestName;
