afeefbd9e9069946499b1d544dd3414226236d02
Jonas Bachmann <jbachmann@ethz.ch>
2025-06-03 12:07:23 +0200

    implement particle particle interaction

---
+++ src/Interaction/TruncatedGreenInteraction.h
@@ -0,0 +1,33 @@
+#ifndef IPPL_TRUNCATEDGREEN_SHORTRANGE_H
+#define IPPL_TRUNCATEDGREEN_SHORTRANGE_H
+
+#include "ParticleInteractionBase.h"
+
+namespace ippl {
+    template<typename ParticleContainer, typename VectorAttribute, typename ScalarAttribute>
+    class TruncatedGreenInteraction : public ParticleInteractionBase<ParticleContainer> /*Energy, Position, Charge/Mass*/ {
+    public:
+        using Base = ParticleInteractionBase<ParticleContainer>;
+        using Vector_t = typename VectorAttribute::value_type;
+        using Scalar_t = typename ScalarAttribute::value_type;
+
+    public:
+        TruncatedGreenInteraction(const ParticleContainer &pc, VectorAttribute &E, const VectorAttribute &R,
+                                 const ScalarAttribute &QM, const ParameterList &params) : Base(pc, params),
+            E_m(&E), R_m(&R), QM_m(&QM) {
+        }
+
+        ~TruncatedGreenInteraction() override = default;
+
+        void solve() override;
+
+    private:
+        VectorAttribute *E_m;
+        const VectorAttribute *R_m;
+        const ScalarAttribute *QM_m;
+    };
+}
+
+#include "TruncatedGreenInteraction.hpp"
+
+#endif //IPPL_TRUNCATEDGREEN_SHORTRANGE_H
---
+++ src/Interaction/TruncatedGreenInteraction.hpp
@@ -0,0 +1,61 @@
+
+
+namespace ippl {
+    template<typename ParticleContainer, typename ScalarAttribute, typename VectorAttribute>
+    void TruncatedGreenInteraction<ParticleContainer, ScalarAttribute, VectorAttribute>::solve() {
+        // get particle data
+        auto R = *R_m;
+        auto E = *E_m;
+        auto QM = *QM_m;
+
+        // get simulation specific data
+        auto rcut = this->params_m.template get<Scalar_t>("rcut");
+        auto alpha = this->params_m.template get<Scalar_t>("alpha");
+        auto forceConstant = this->params_m.template get<Scalar_t>("force_constant");
+
+        const auto nloc = this->pc_m.getLocalNum();
+        // TODO where to get this info from? Should it take PC as template param instead?
+        const auto &particleLayout = this->pc_m.getLayout();
+        auto data = particleLayout.getNeighborData();
+
+        using team_policy_t = Kokkos::TeamPolicy<typename ScalarAttribute::execution_space>;
+        using team_t = typename team_policy_t::member_type;
+
+        // calculate interaction force
+        Kokkos::parallel_for("Particle-Particle", team_policy_t(nloc, Kokkos::AUTO()),
+                             KOKKOS_LAMBDA(const team_t &team) {
+                                 const size_type particleIndex = team.league_rank();
+
+
+                                 typename ParticleContainer::NList_t neighborList;
+
+                                 particleLayout.getNeighbors(R(particleIndex), data, neighborList);
+
+                                 Kokkos::parallel_for(Kokkos::TeamThreadRange(team, neighborList.extent(0)),
+                                                      [&](const int &neighborIdx) {
+                                                          if (neighborIdx == particleIndex) { return; }
+                                                          Vector_t dist_ij = R(particleIndex) - R(neighborIdx);
+                                                          const auto rsq_ij = dist_ij.dot(dist_ij);
+                                                          double r_ij = Kokkos::sqrt(rsq_ij);
+
+                                                          if (r_ij >= rcut) { return; }
+
+                                                          // TODO make this KOKKOS inline function?
+                                                          // calculate and apply force
+                                                          Vector_t F_ij =
+                                                                  forceConstant * (dist_ij / r_ij) * (2.0 * alpha * Kokkos::exp(
+                                                                          -alpha * alpha * rsq_ij) /
+                                                                      (Kokkos::sqrt(Kokkos::numbers::pi) * r_ij) + (
+                                                                          1.0 - Kokkos::erf(alpha * r_ij)) / rsq_ij);
+                                                          Kokkos::atomic_sub(&E(particleIndex), F_ij * QM(neighborIdx));
+                                                          // Kokkos::atomic_add(&E(neighborList), F_ij * Q(particleIndex));
+                                                      }
+                                 );
+                             });
+
+        // Kokkos::fence();
+        // ippl::Comm->barrier();
+
+        // std::cerr << "Particle-Particle Interaction finished" << std::endl;
+    }
+}
