6a5af1dcde69f77b80674dd4bfd2fa7a2d4543ed
Jonas Bachmann <jbachmann@ethz.ch>
2025-07-01 17:15:54 +0200

    only one example as they all do the same thing

--- examples/collisions/P3MHeating.cpp
+++ examples/collisions/P3MHeating.cpp
@@ -1,34 +1,34 @@
-/******************************************************************************************************
- *                                          TestP3MHeating
- * ====================================================================================================
+/***************************************************************************************************
+ *                                       P3MHeating
+ * =================================================================================================
  * This program tests the P3M Method, applied to the Disorder-Induced-Heating problem.
  * The parameters were chosen according to B.Ulmers Thesis -
  * "The P3M Model on Emerging Computer Architectures With Application to Microbunching"
  * (http://amas.web.psi.ch/people/aadelmann/ETH-Accel-Lecture-1/projectscompleted/cse/thesisBUlmer.pdf)
- * 
+ *
  * This program and corresponding methods were implemented as part of a Bachelors Thesis
  * "A Performance Portable Version of the P3M Algorithm"
  * By Timo Schwab, ETH Zurich (2024)
- * 
- * Usage: srun ./TestP3MHeating <nx> <ny> <nz>
- *  nx  =   No. PM grid points in x-direction
- *  ny  =   No. PM grid points in y-direction
- *  nz  =   No. PM grid points in z-direction
- * 
- * CURRENTLY WORK IN PROGRESS !!!
-*/
-
-
+ *
+ * Usage: srun ./P3MHeating <nx> <ny> <nz> <factor> --info 10
+ *  nx      =   No. PM grid points in x-direction
+ *  ny      =   No. PM grid points in y-direction
+ *  nz      =   No. PM grid points in z-direction
+ *  factor  =   factor to multiply the grid size in x direction to get the cutoff radius
+ */
 
 constexpr unsigned Dim = 3;
-using T = double;
+using T                = double;
 
 #include "Ippl.h"
+
+#include "P3MHeating.hpp"
+
 #include "datatypes.h"
-#include "P3MHeatingManager.hpp"
-#include "P3MParticleContainer.hpp"
 
 #include "Utility/IpplTimings.h"
+
+#include "P3MParticleContainer.hpp"
 
 int main(int argc, char* argv[]) {
     ippl::initialize(argc, argv);
@@ -36,21 +36,11 @@ int main(int argc, char* argv[]) {
         Inform msg(argv[0]);
         Inform msg2all(argv[0], INFORM_ALL_NODES);
 
-        static IpplTimings::TimerRef mainTimer = IpplTimings::getTimer("total");
-        static IpplTimings::TimerRef preTimer = IpplTimings::getTimer("pre run");
-        IpplTimings::startTimer(mainTimer);
-
-        // TODO: Change to input
-        const T beam_rad       = 0.001774;
         const T boxlen         = 0.01;
-        const unsigned int np       = 156055;
-        // const T rcut           = 0.0003125;    // 8 * PM grid spacing
-        // const T alpha          = 2./rcut;      // choice motivated by B. Ulmer
+        const T beam_rad       = 0.001774;
+        const unsigned np      = 156055;
         const T dt             = 2.15623e-13;
-        // const T eps            = 0;
-        const unsigned int nt       = 1000;
-        // const T m_e            = 1;
-        // const T q_e            = 1;
+        const unsigned nt      = 1000;
         const T focus_strength = 1.5;
 
         int arg = 1;
@@ -59,26 +49,25 @@ int main(int argc, char* argv[]) {
             nr[d] = std::atoi(argv[arg++]);
         }
 
-        const T rcut = 4.0 * (boxlen/static_cast<T>(nr[0]));
-        const T alpha = 2./rcut;
+        const unsigned factor = std::atoi(argv[arg]);
 
-        P3M3DHeatingManager<T, Dim> manager(np, nt, dt, nr, rcut, alpha, beam_rad, focus_strength);
-        IpplTimings::startTimer(preTimer);
+        const T rcut  = factor * (boxlen / static_cast<T>(nr[0]));
+        const T alpha = 2. / rcut;
+
+        P3MHeatingManager<T, Dim> manager(np, nt, dt, nr, rcut, alpha, beam_rad, focus_strength,
+                                          boxlen);
         manager.pre_run();
-        IpplTimings::stopTimer(preTimer);
 
-        // set time to 0
         manager.setTime(0.0);
 
         msg << "Starting iterations ..." << endl;
 
+        static IpplTimings::TimerRef simulationTimer =
+            IpplTimings::getTimer("Total Simulation Time");
+        IpplTimings::startTimer(simulationTimer);
         manager.run(manager.getNt());
-
-        msg << "End." << endl;
-	    IpplTimings::stopTimer(mainTimer);
+        IpplTimings::stopTimer(simulationTimer);
         IpplTimings::print();
-        IpplTimings::print(std::string("timing.dat"));
-
     }
     ippl::finalize();
 
--- examples/collisions/P3MHeating.hpp
+++ examples/collisions/P3MHeating.hpp
@@ -25,14 +25,14 @@
 #include "datatypes.h"
 
 /**
- * @class P3M3DBenchManager
+ * @class P3MHeatingManager
  * @brief A class that benchmarks the P3M Method
- * 
+ *
  * @tparam T the data dype for simulation variables
  * @tparam Dim the dimensionality of the simulation
 */
 template<typename T, unsigned Dim>
-class P3M3DBenchManager
+class P3MHeatingManager
         : public P3M3DManager<T, Dim, FieldContainer<T, Dim> > {
 public:
     using ParticleContainer_t = P3MParticleContainer<T, Dim>;
@@ -51,14 +51,14 @@ protected:
     T boxlen_m; // box length
 
 public:
-    P3M3DBenchManager(size_type totalP_, int nt_, T dt_, Vector_t<int, Dim> &nr_, T rcut_, T alpha_, T beamRad_,
-                      T focusingF_, T boxlen_)
+    P3MHeatingManager(size_type totalP_, int nt_, T dt_, Vector_t<int, Dim> &nr_, T rcut_, T alpha_,
+                      T beamRad_, T focusingF_, T boxlen_)
         : P3M3DManager<T, Dim, FieldContainer<T, Dim> >()
           , totalP_m(totalP_), nt_m(nt_), dt_m(dt_), nr_m(nr_), rcut_m(rcut_), solver_m("P3M"), beamRad_m(beamRad_),
           focusingF_m(focusingF_), boxlen_m(boxlen_), alpha_m(alpha_) {
     }
 
-    ~P3M3DBenchManager() {
+    ~P3MHeatingManager() {
     }
 
 protected:
@@ -104,12 +104,14 @@ public:
         this->decomp_m.fill(true);
         // this->alpha_m = 2./this->rcut_m;
         T box_length = this->boxlen_m;
-        this->rmin_m = -box_length / 2.;
-        this->rmax_m = box_length / 2.;
+        this->rmin_m = Vector_t<T, Dim>(-box_length / 2.);
+        this->rmax_m = Vector_t<T, Dim>(box_length / 2.);
         this->origin_m = rmin_m;
         this->isAllPeriodic_m = true;
 
-        this->hr_m = box_length / static_cast<T>(this->nr_m[0]);
+        for (unsigned d = 0; d < Dim; ++d) {
+            this->hr_m[d] = box_length / this->nr_m[d];
+        }
 
         m << "hr: " << this->hr_m << endl;
 
@@ -150,6 +152,7 @@ public:
         sp.add("alpha", this->alpha_m);
         sp.add("force_constant", static_cast<T>(2.532638e8)); // ke
 
+
         this->setFieldSolver(
             std::make_shared<P3MSolver_t<T, Dim> >(
                 this->fcontainer_m->getE(), this->fcontainer_m->getRho(), sp
@@ -168,37 +171,120 @@ public:
             )
         );
 
-        T initTimerStart = MPI_Wtime();
+
+        static IpplTimings::TimerRef particleInitTimer = IpplTimings::getTimer("particle_init");
+        IpplTimings::startTimer(particleInitTimer);
         initializeParticles();
-        T initTimerEnd = MPI_Wtime();
-        m << "Particle Initialization Time: " << initTimerEnd - initTimerStart << endl;
+        IpplTimings::stopTimer(particleInitTimer);
 
         this->fcontainer_m->getRho() = 0.0;
 
-        T PMTimerStart = MPI_Wtime();
         this->par2grid();
 
         this->fsolver_m->solve();
 
         this->grid2par();
-        T PMTimerEnd = MPI_Wtime();
 
-        m << "Field Solver Time: " << PMTimerEnd - PMTimerStart << endl;
-
-        T PPTimerStart = MPI_Wtime();
         this->isolver_m->solve();
-        T PPTimerEnd = MPI_Wtime();
-        m << "PP Interaction Time: " << PPTimerEnd - PPTimerStart << endl;
 
-        // this->focusingF_m *= this->computeAvgSpaceChargeForces();
-
-        // this->pcontainer_m->update();
+        this->focusingF_m *= this->computeAvgSpaceChargeForces();
 
         m << "Pre Run finished" << endl;
     }
 
+    void computeBeamStatistics() {
+        Inform m("ComputeBeamStatistics");
+
+        const auto R = this->pcontainer_m->R.getView();
+        const auto nLoc = this->pcontainer_m->getLocalNum();
+        const auto P = this->pcontainer_m->P.getView();
+        // const T beamRad = this->beamRad_m;
+
+        Vector_t<T, 12> stats(0.0);
+
+        Kokkos::parallel_reduce("compute sigma x", nLoc,
+                                KOKKOS_LAMBDA(const size_type i, Vector_t<T, 12> &sum) {
+                                    sum[0] += R(i)[0] * R(i)[0];
+                                    sum[1] += P(i)[0] * P(i)[0];
+                                    sum[2] += R(i)[0] * P(i)[0];
+                                    sum[3] += R(i)[1] * R(i)[1];
+                                    sum[4] += P(i)[1] * P(i)[1];
+                                    sum[5] += R(i)[1] * P(i)[1];
+                                    sum[6] += R(i)[2] * R(i)[2];
+                                    sum[7] += P(i)[2] * P(i)[2];
+                                    sum[8] += R(i)[2] * P(i)[2];
+                                    sum[9] += R(i)[0];
+                                    sum[10] += R(i)[1];
+                                    sum[11] += R(i)[2];
+                                }, stats
+        );
+        Kokkos::fence();
+
+        Vector_t<T, 12> global_stats(0.0);
+
+        ippl::Comm->reduce(&stats[0], &global_stats[0], 12, std::plus<T>(), 0);
+
+        global_stats /= this->totalP_m;
+
+        const T sigma_x = Kokkos::sqrt(global_stats[0] - global_stats[9] * global_stats[9]);
+        const T sigma_y = Kokkos::sqrt(global_stats[3] - global_stats[10] * global_stats[10]);
+        const T sigma_z = Kokkos::sqrt(global_stats[6] - global_stats[11] * global_stats[11]);
+        const T emit_x = Kokkos::sqrt(global_stats[0] * global_stats[1] - global_stats[2] * global_stats[2]);
+        const T emit_y = Kokkos::sqrt(global_stats[3] * global_stats[4] - global_stats[5] * global_stats[5]);
+        const T emit_z = Kokkos::sqrt(global_stats[6] * global_stats[7] - global_stats[8] * global_stats[8]);
+        // T beta = avg_xsq / emit_x;
+        // T sigma_x = Kokkos::sqrt(avg_xsq);
+
+        m << "Beam Statistics: " << endl;
+        m << "RMS Beam Size: " << sigma_x << " , " << sigma_y << " , " << sigma_z << endl;
+        m << "RMS Emittance: " << emit_x << " , " << emit_y << " , " << emit_z << endl;
+    }
+
+    void computeTemperature() {
+        Inform m("computeTemperature");
+        Vector_t<T, 3> avgVel(0.0);
+        const auto nLoc = this->pcontainer_m->getLocalNum();
+        const auto P = this->pcontainer_m->P.getView();
+
+        Kokkos::parallel_reduce("compute average velocity", nLoc,
+                                KOKKOS_LAMBDA(const size_type i, Vector_t<T, 3> &sum) {
+                                    sum += P(i);
+                                }, avgVel
+        );
+        Kokkos::fence();
+
+        ippl::Comm->allreduce(avgVel[0], 3, std::plus<T>());
+
+        ippl::Comm->barrier();
+
+        const auto totalP = this->totalP_m;
+
+        avgVel /= totalP;
+        m << "Average Velocity: " << avgVel << endl;
+
+        Vector_t<T, 3> localTemperature(0.0);
+        Vector_t<T, 3> globalTemperature(0.0);
+
+        Kokkos::parallel_reduce("compute temperature", nLoc,
+                                KOKKOS_LAMBDA(const size_type i, Vector_t<T, 3> &sum) {
+                                    sum += (P(i) - avgVel) * (P(i) - avgVel); // remove mean transportation
+                                }, localTemperature
+        );
+        Kokkos::fence();
+
+        ippl::Comm->reduce(&localTemperature[0], &globalTemperature[0], 3, std::plus<T>(), 0);
+
+        globalTemperature /= totalP;
+
+        m << "Temperature: " << globalTemperature << endl;
+        // l2 norm
+        T temperature = Kokkos::sqrt(globalTemperature.dot(globalTemperature));
+        m << "L2-Norm of Temperature: " << temperature << endl;
+    }
+
     void dump() {
-        // return 0;
+        computeBeamStatistics();
+        computeTemperature();
     }
 
     void initializeParticles() {
@@ -220,63 +306,58 @@ public:
         this->Q_m = np;
 
         // make sure all particles are accounted for
-        if (rank == commSize - 1) {
-            nloc = np - (commSize - 1) * nloc;
+        //if(rank == commSize-1){
+        //    nloc = np - (commSize-1)*nloc;
+        //}
+
+        // initialize on one rank
+        if (rank == 0) {
+            nloc = np;
+        } else {
+            nloc = 0;
         }
 
         IpplTimings::startTimer(CTimer);
         this->pcontainer_m->create(nloc);
         IpplTimings::stopTimer(CTimer);
 
-
         auto P = this->pcontainer_m->P.getView();
         auto R = this->pcontainer_m->R.getView();
         auto Q = this->pcontainer_m->Q.getView();
 
-        auto hLocalRegions = this->pcontainer_m->getLayout().getRegionLayout().gethLocalRegions();
-        Vector_t<T, Dim> domainMin, domainLength;
-
-        for (unsigned d = 0; d < Dim; ++d) {
-            domainMin[d] = hLocalRegions(rank)[d].min();
-            domainLength[d] = hLocalRegions(rank)[d].length();
-        }
+        T beamRad = this->beamRad_m;
 
         Kokkos::fence();
 
         // make sure this runs on the host, device does not work yet
-        Kokkos::Random_XorShift64_Pool rand_pool(static_cast<size_type>(42 + 24 * rank));
+        Kokkos::Random_XorShift64_Pool rand_pool(42 + 24 * rank);
 
         IpplTimings::startTimer(GTimer);
-        Kokkos::parallel_for("initialize particles", nloc,
-                             KOKKOS_LAMBDA(const size_t index) {
-                                 Vector_t<T, Dim> x(0.0);
+        Kokkos::parallel_for(
+            "initialize particles", nloc,
+            KOKKOS_LAMBDA(const size_t index) {
+                Vector_t<T, Dim> x(0.0);
 
-                                 auto generator = rand_pool.get_state();
+                auto generator = rand_pool.get_state();
 
-                                 // obtain random numbers
-                                 Vector_t<T, Dim> u;
-                                 for (unsigned d = 0; d < Dim; ++d) {
-                                     u[d] = generator.drand();
-                                 }
+                // obtain random numbers
+                T u = generator.drand();
+                for (unsigned i = 0; i < Dim; ++i) {
+                    x[i] = generator.normal(0.0, 1.0);
+                }
 
-                                 // for(int i = 0; i < Dim; ++i){
-                                 //     x[i] = generator.normal(0.0, 1.0);
-                                 // }
+                rand_pool.free_state(generator);
 
-                                 rand_pool.free_state(generator);
+                // calculate position
+                T normsq = x[0] * x[0] + x[1] * x[1] + x[2] * x[2];
+                Vector_t<T, Dim> pos = beamRad * (Kokkos::pow(u, 1. / 3.) / Kokkos::sqrt(normsq)) * x;
 
-                                 // calculate position
-                                 // T normsq = x[0] * x[0] + x[1] * x[1] + x[2] * x[2];
-                                 // if (sign < 0.5) u = -u;
-                                 Vector_t<T, Dim> pos = domainMin + domainLength * u;
-
-                                 for (unsigned d = 0; d < Dim; ++d) {
-                                     P(index)[d] = 0; // initialize with zero momentum
-                                     R(index)[d] = pos[d];
-                                 }
-                                 Q(index) = 1.0;
-                             }
-        );
+                for (unsigned d = 0; d < Dim; ++d) {
+                    P(index)[d] = 0; // initialize with zero momentum
+                    R(index)[d] = pos[d];
+                }
+                Q(index) = 1;
+            });
 
         // we need to wait for all other ranks to have finished the particle initialization
         // before we can update them to their corresponding rank
@@ -291,11 +372,7 @@ public:
 
         ippl::Comm->barrier();
         IpplTimings::stopTimer(ITimer);
-
-        // debug output, can be ignored
-        m << this->pcontainer_m->getLocalNum() << endl;
     }
-
 
     void pre_step() override {
         Inform m("pre step");
@@ -303,6 +380,8 @@ public:
 
     void post_step() override {
         Inform m("post step");
+
+        dump();
 
         this->time_m += this->dt_m;
         this->it_m++;
@@ -315,7 +394,6 @@ public:
     void par2grid() override {
         scatterCIC();
     }
-
 
     void advance() override {
         LeapFrogStep();
@@ -353,7 +431,7 @@ public:
         this->isolver_m->solve();
         IpplTimings::stopTimer(PPSolveTimer);
 
-        // this->applyConstantFocusing();
+        this->applyConstantFocusing();
 
         IpplTimings::startTimer(PTimer);
         pc->P = pc->P - dt * pc->E;
@@ -364,10 +442,11 @@ public:
 
     T computeAvgSpaceChargeForces() {
         Inform m("computeAvgSpaceChargeForces");
-        auto totalP = this->totalP_m;
-        auto nLoc = this->pcontainer_m->getLocalNum();
-        auto E = this->pcontainer_m->E.getView();
-        Vector_t<T, Dim> avgE = 0.0;
+
+        const auto totalP = this->totalP_m;
+        const auto nLoc = this->pcontainer_m->getLocalNum();
+        const auto E = this->pcontainer_m->E.getView();
+        Vector_t<T, Dim> avgE(0.0);
 
         Kokkos::parallel_reduce("compute average space charge forces", nLoc,
                                 KOKKOS_LAMBDA(const size_type i, Vector_t<T, Dim> &sum) {
@@ -376,40 +455,36 @@ public:
                                     sum[2] += Kokkos::abs(E(i)[2]);
                                 }, avgE
         );
+        Kokkos::fence();
 
+        ippl::Comm->allreduce(avgE[0], 3, std::plus<T>());
+        avgE /= totalP;
         m << "Average Space Charge Forces: " << avgE << endl;
 
-        Vector_t<T, Dim> globE = 0.0;
-
-        ippl::Comm->reduce(&avgE[0], &globE[0], 3, std::plus<T>(), 0);
-
-        globE /= totalP;
-
-        T focusingf = 0.0;
-        for (unsigned d = 0; d < Dim; ++d) {
-            focusingf += globE[d] * globE[d];
-        }
+        T focusingf = avgE.dot(avgE);
 
         return std::sqrt(focusingf);
     }
 
     void applyConstantFocusing() {
         Inform m("applyConstantFocusing");
-        auto E = this->pcontainer_m->E.getView();
-        auto R = this->pcontainer_m->R.getView();
+        const auto E = this->pcontainer_m->E.getView();
+        const auto R = this->pcontainer_m->R.getView();
 
-        T beamRad = this->beamRad_m;
-        T focusStrength = this->focusingF_m;
-        auto nLoc = this->pcontainer_m->getLocalNum();
+        const T beamRad = this->beamRad_m;
+        const T focusStrength = this->focusingF_m;
+        const auto nLoc = this->pcontainer_m->getLocalNum();
 
         m << "Focusing Force " << focusStrength << endl;
 
         Kokkos::parallel_for("apply constant focusing", nLoc,
                              KOKKOS_LAMBDA(const size_type &i) {
                                  Vector_t<T, Dim> F = focusStrength * (R(i) / beamRad);
-                                 Kokkos::atomic_add(&E(i), F);
+                                 // TODO if Q would not be all ones, this should involve Q somehow?
+                                 E(i) += F;
                              }
         );
+        Kokkos::fence();
     }
 
     void gatherCIC() {
@@ -425,7 +500,7 @@ public:
 
         ippl::ParticleAttrib<T> *q = &this->pcontainer_m->Q;
         typename ParticleContainer_t::particle_position_type *R = &this->pcontainer_m->R;
-        Field_t<Dim> *rho = &this->fcontainer_m->getRho();
+        auto rho = &this->fcontainer_m->getRho();
         T Q = this->Q_m;
         Vector_t<T, Dim> rmin = this->rmin_m;
         Vector_t<T, Dim> rmax = this->rmax_m;
@@ -434,7 +509,7 @@ public:
         scatter(*q, *rho, *R);
         T relError = std::fabs((Q - (*rho).sum()) / Q);
 
-        m << "Relative Error: " << relError << endl;
+        // m << "Relative Error: " << relError << endl;
 
         size_type TotalParticles = 0;
         size_type localParticles = this->pcontainer_m->getLocalNum();
