7065abb39ee8cd627d2cc2a28a49b3006d352326
JoMee <jonmeier@ethz.ch>
2025-08-21 14:45:25 +0200

    cmake: replace *_INIT flags with config-specific compile options - Use generator expressions for -O/-g flags so they reliably apply across reconfigures. - Add sane defaults for CMAKE_BUILD_TYPE if not set. - Enable compile-commands (this is mainly for IDEs and other tooling) - Globally set the CMAKE_POSITION_INDEPENDENT_CODE flag to ON

--- cmake/ProjectSetup.cmake
+++ cmake/ProjectSetup.cmake
@@ -16,11 +16,21 @@
 #   - External dependencies               → Dependencies.cmake
 # -----------------------------------------------------------------------------
 
+if(PROJECT_IS_TOP_LEVEL)
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+  if(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING
+        "Build type (Debug, Release, RelWithDebInfo)")
+  endif()
+endif()
+
 # === C++ Standard ===
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_CUDA_EXTENSIONS OFF)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 set(IPPL_LIB_TYPE STATIC)
 
