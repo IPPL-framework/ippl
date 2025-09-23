e0d9e35c2ad5e4123872c3b79d68bd8464c86a06
S.Klapproth <severikl@student.ethz.ch>
2025-09-23 13:26:50 +0200

    update run_penningtrap visualisation bash example

--- run_penningtrap_eg.sh
+++ run_penningtrap_eg.sh
@@ -5,49 +5,25 @@ export IPPL_DIR=/.../ippl
 # or similar
 
 
+
 export PENNINGTRAP_BINDIR=${IPPL_DIR}/build/alpine
-
-
-
-
-
-echo "Using PareView at at: ${PV_PREFIX}"
-
-
-
 export PVSCRIPT=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/catalyst-script.py
 export PVSCRIPT=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/pipeline.py
 export  PVPROXY=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/proxy.xml
-
-
-
 # Ascent Adaptor will try fetch from environment, else swap to default
 export ASCENT_ACTIONS_PATH=${IPPL_DIR}/src/Stream/InSitu/ascent_scripts/ascent_actions_default.yaml
-
-
 # Catalyst Adaptor will fetch from environment
 export CATALYST_IMPLEMENTATION_PATHS="${PV_PREFIX}/lib/catalyst"
 export CATALYST_IMPLEMENTATION_NAME="paraview"
 
 
-
-
-
-
 cd ${PENNINGTRAP_BINDIR}
-
 rm -rd data
 mkdir data
 
 
-# export MPIEXEC=/home/klappi/AddApp/paraview_versions/ParaView-5.12.0-MPI-Linux-Python3.10-x86_64/lib/mpiexec
-# exec $MPIEXEC -np 1 
-# ./PenningTrap 4 4 4 512 30 FFT 0.05 LeapFrog --overallocate 1.0 --pvscript $PVSCRIPT --pvproxy  $PVPROXY --info 5
-
-
-
+# currently run with both ascent and catalyst 
 ./PenningTrap 4 4 4 512 10 FFT 0.05 LeapFrog --overallocate 1.0  --pvscript $PVSCRIPT --pvproxy  $PVPROXY --frequency 3 --info 5
-# 
 cd $IPPL_DIR
 
 
