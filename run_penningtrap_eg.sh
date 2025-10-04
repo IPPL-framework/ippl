d6bb47b0c55806524a623d0d77e46903b6cdff38
S.Klapproth <severikl@student.ethz.ch>
2025-10-04 17:49:52 +0200

    update run example and Registry cmake

--- run_penningtrap_eg.sh
+++ run_penningtrap_eg.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-export PV_PREFIX=/.../ParaView-5.12.0-MPI-Linux-Python3.10-x86_64
-export IPPL_DIR=/.../ippl
-# or similar
-
-
-
+export IPPL_DIR=/.../ippl-frk
 export PENNINGTRAP_BINDIR=${IPPL_DIR}/build/alpine
-export PVSCRIPT=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/catalyst-script.py
-export PVSCRIPT=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/pipeline.py
-export  PVPROXY=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/proxy.xml
-# Ascent Adaptor will try fetch from environment, else swap to default
-export ASCENT_ACTIONS_PATH=${IPPL_DIR}/src/Stream/InSitu/ascent_scripts/ascent_actions_default.yaml
-# Catalyst Adaptor will fetch from environment
+
+PV_PREFIX=".../ParaView-5.12.0-MPI-Linux-Python3.10-x86_64"
+
+export  CATALYST_PIPELINE_PATH=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/pipeline_default.py
+
+export  CATALYST_EXTRACTOR_SCRIPT_P=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/catalyst_extractors/png_ext_particle.py
+export    CATALYST_EXTRACTOR_SCRIPT_S=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/catalyst_extractors/png_ext_sfield.py
+export    CATALYST_EXTRACTOR_SCRIPT_V=${IPPL_DIR}/src/Stream/InSitu/catalyst_scripts/catalyst_extractors/png_ext_vfield.py
+
+
 export CATALYST_IMPLEMENTATION_PATHS="${PV_PREFIX}/lib/catalyst"
 export CATALYST_IMPLEMENTATION_NAME="paraview"
 
+# export ASCENT_ACTIONS_PATH=${IPPL_DIR}/src/Stream/InSitu/ascent_scripts/ascent_actions_default.yaml
+# Ascent Adaptor will try fetch from environment, else swap to default
+# export ASCENT_ACTIONS_PATH=${IPPL_DIR}/src/Stream/InSitu/ascent_scripts/ascent_actions_default.yaml
+
+
 
 cd ${PENNINGTRAP_BINDIR}
+
 rm -rd data
 mkdir data
 
+# export MPIEXEC=/.../ParaView-5.12.0-MPI-Linux-Python3.10-x86_64/lib/mpiexec
+# exec $MPIEXEC -np 1 
 
-# currently run with both ascent and catalyst 
-./PenningTrap 4 4 4 512 10 FFT 0.05 LeapFrog --overallocate 1.0  --pvscript $PVSCRIPT --pvproxy  $PVPROXY --frequency 3 --info 5
+# ./PenningTrap 4 4 4 512 21 FFT 0.05 LeapFrog --overallocate 1.0  --info 5
+./PenningTrap 8 8 8 4096 21 FFT 0.05 LeapFrog --overallocate 1.0  --info 5
 cd $IPPL_DIR
-
-
-
-
-
 
 
