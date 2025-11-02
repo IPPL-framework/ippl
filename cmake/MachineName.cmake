f63d9b4c4a4f1d40122b76c9919251ed56f94288
John Biddiscombe <biddisco@cscs.ch>
2025-11-02 17:20:55 +0100

    Improve script generator for landau benchmarking to handle different machines

--- cmake/MachineName.cmake
+++ cmake/MachineName.cmake
@@ -12,22 +12,20 @@
 # -----------------------------------------------------------------------------
 
 if(NOT "$ENV{CLUSTER_NAME}" STREQUAL "")
-  set(MACHINE_NAME "$ENV{CLUSTER_NAME}")
+  set(IPPL_MACHINENAME "$ENV{CLUSTER_NAME}")
 elseif(NOT "$ENV{LMOD_SYSTEM_NAME}" STREQUAL "")
-  set(MACHINE_NAME "$ENV{LMOD_SYSTEM_NAME}")
+  set(IPPL_MACHINENAME "$ENV{LMOD_SYSTEM_NAME}")
 elseif(NOT "$ENV{APPS}" STREQUAL "")
   # basename of $APPS then append -xc
   get_filename_component(_apps_basename "$ENV{APPS}" NAME)
-  set(MACHINE_NAME "${_apps_basename}-xc")
+  set(IPPL_MACHINENAME "${_apps_basename}-xc")
 elseif(NOT "$ENV{LUMI_LMOD_FAMILY_COMPILER}" STREQUAL "")
-  set(MACHINE_NAME "lumi")
+  set(IPPL_MACHINENAME "lumi")
 else()
   execute_process(COMMAND hostname OUTPUT_VARIABLE _host OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(MACHINE_NAME "${_host}")
+  set(IPPL_MACHINENAME "${_host}")
 endif()
 
 # export to environment and make available in cache for other CMake code
-set(ENV{MACHINE_NAME} "${MACHINE_NAME}")
-set(MACHINE_NAME "${MACHINE_NAME}" CACHE STRING "Detected machine name")
-
-message(STATUS "MACHINE_NAME=${MACHINE_NAME}")
+set(ENV{IPPL_MACHINENAME} "${IPPL_MACHINENAME}")
+set(IPPL_MACHINENAME "${IPPL_MACHINENAME}" CACHE STRING "Detected machine name")
