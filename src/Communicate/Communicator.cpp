1fb6d1b78f5f9cb733788302d7d47726db92f3df
John Biddiscombe <biddisco@cscs.ch>
2025-12-05 17:09:45 +0100

    Use a singleton BufferHandler to avoid duplication of buffers

--- src/Communicate/Communicator.cpp
+++ src/Communicate/Communicator.cpp
@@ -1,42 +1,54 @@
 
 #include "Communicate/Communicator.h"
 
-namespace ippl {
-    namespace mpi {
+namespace ippl::mpi {
 
-        Communicator::Communicator()
-            : comm_m(new MPI_Comm(MPI_COMM_WORLD)) {
-            MPI_Comm_rank(*comm_m, &rank_m);
-            MPI_Comm_size(*comm_m, &size_m);
-        }
+    Communicator::Communicator()
+        : buffer_handlers_m(get_buffer_handler_instance())
+        , comm_m(new MPI_Comm(MPI_COMM_WORLD)) {
+        MPI_Comm_rank(*comm_m, &rank_m);
+        MPI_Comm_size(*comm_m, &size_m);
+    }
 
-        Communicator::Communicator(MPI_Comm comm) {
-            comm_m = std::make_shared<MPI_Comm>(comm);
-            MPI_Comm_rank(*comm_m, &rank_m);
-            MPI_Comm_size(*comm_m, &size_m);
-        }
+    Communicator::Communicator(MPI_Comm comm) {
+        buffer_handlers_m = get_buffer_handler_instance();
+        comm_m            = std::make_shared<MPI_Comm>(comm);
+        MPI_Comm_rank(*comm_m, &rank_m);
+        MPI_Comm_size(*comm_m, &size_m);
+    }
 
-        Communicator& Communicator::operator=(MPI_Comm comm) {
-            comm_m = std::make_shared<MPI_Comm>(comm);
-            MPI_Comm_rank(*comm_m, &rank_m);
-            MPI_Comm_size(*comm_m, &size_m);
-            return *this;
-        }
+    Communicator& Communicator::operator=(MPI_Comm comm) {
+        buffer_handlers_m = get_buffer_handler_instance();
+        comm_m            = std::make_shared<MPI_Comm>(comm);
+        MPI_Comm_rank(*comm_m, &rank_m);
+        MPI_Comm_size(*comm_m, &size_m);
+        return *this;
+    }
 
-        Communicator Communicator::Communicator::split(int color, int key) const {
-            MPI_Comm newcomm;
-            MPI_Comm_split(*comm_m, color, key, &newcomm);
-            return Communicator(newcomm);
-        }
+    Communicator Communicator::Communicator::split(int color, int key) const {
+        MPI_Comm newcomm;
+        MPI_Comm_split(*comm_m, color, key, &newcomm);
+        return Communicator(newcomm);
+    }
 
-        void Communicator::probe(int source, int tag, Status& status) {
-            MPI_Probe(source, tag, *comm_m, status);
-        }
+    void Communicator::probe(int source, int tag, Status& status) {
+        MPI_Probe(source, tag, *comm_m, status);
+    }
 
-        bool Communicator::iprobe(int source, int tag, Status& status) {
-            int flag = 0;
-            MPI_Iprobe(source, tag, *comm_m, &flag, status);
-            return (flag != 0);
+    bool Communicator::iprobe(int source, int tag, Status& status) {
+        int flag = 0;
+        MPI_Iprobe(source, tag, *comm_m, &flag, status);
+        return (flag != 0);
+    }
+
+    // ---------------------------------------
+    // singleton access to buffer manager
+    // ---------------------------------------
+    std::shared_ptr<Communicator::buffer_handler_type> Communicator::get_buffer_handler_instance() {
+        static std::shared_ptr<Communicator::buffer_handler_type> comm_buff_handler_ptr{nullptr};
+        if (comm_buff_handler_ptr == nullptr) {
+            comm_buff_handler_ptr = std::make_shared<Communicator::buffer_handler_type>();
         }
-    }  // namespace mpi
-}  // namespace ippl
+        return comm_buff_handler_ptr;
+    }
+}  // namespace ippl::mpi
