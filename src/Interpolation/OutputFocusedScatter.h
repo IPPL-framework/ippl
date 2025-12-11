65a30e78eeaa5959727d9d891b95d43d56c4faf0
pfischill <pfischill@student.ethz.ch>
2025-12-11 19:00:44 +0100

    more lumi

--- src/Interpolation/OutputFocusedScatter.h
+++ src/Interpolation/OutputFocusedScatter.h
@@ -214,7 +214,7 @@ namespace ippl {
 
                             // Use local indices for grid access
                             if constexpr (grid_is_complex) {
-#ifdef KOKKOS_ENABLE_CUDA
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
                                 if constexpr (std::is_same_v<ExecSpace, Kokkos::Cuda>) {
                                     double* addr_as_double = reinterpret_cast<double*>(&grid(
                                         local_x + nghost, local_y + nghost, local_z + nghost));
