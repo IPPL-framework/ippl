ffe9467665f63fdbf4fd61db8d6267656959fd36
mayani_s <sonali.mayani@psi.ch>
2026-01-08 09:49:12 +0100

    remove dumpVTK which was added for debugging

--- alpine/LandauDampingManager.h
+++ alpine/LandauDampingManager.h
@@ -16,43 +16,6 @@
 
 using view_type = typename ippl::detail::ViewType<ippl::Vector<double, Dim>, 1>::view_type;
 
-template <typename T>
-void dumpVTK(VField_t<T, 3>& E, int nx, int ny, int nz, int iteration, double dx, double dy,
-             double dz) {
-    typename VField_t<T, 3>::view_type::host_mirror_type host_view = E.getHostMirror();
-
-    std::stringstream fname;
-    fname << "data/ef_";
-    fname << std::setw(4) << std::setfill('0') << iteration;
-    fname << ".vtk";
-
-    Kokkos::deep_copy(host_view, E.getView());
-
-    Inform vtkout(NULL, fname.str().c_str(), Inform::OVERWRITE);
-    vtkout.precision(10);
-    vtkout.setf(std::ios::scientific, std::ios::floatfield);
-
-    // start with header
-    vtkout << "# vtk DataFile Version 2.0" << endl;
-    vtkout << TestName << endl;
-    vtkout << "ASCII" << endl;
-    vtkout << "DATASET STRUCTURED_POINTS" << endl;
-    vtkout << "DIMENSIONS " << nx + 3 << " " << ny + 3 << " " << nz + 3 << endl;
-    vtkout << "ORIGIN " << -dx << " " << -dy << " " << -dz << endl;
-    vtkout << "SPACING " << dx << " " << dy << " " << dz << endl;
-    vtkout << "CELL_DATA " << (nx + 2) * (ny + 2) * (nz + 2) << endl;
-
-    vtkout << "VECTORS E-Field float" << endl;
-    for (int z = 0; z < nz + 2; z++) {
-        for (int y = 0; y < ny + 2; y++) {
-            for (int x = 0; x < nx + 2; x++) {
-                vtkout << host_view(x, y, z)[0] << "\t" << host_view(x, y, z)[1] << "\t"
-                       << host_view(x, y, z)[2] << endl;
-            }
-        }
-    }
-}
-
 // define functions used in sampling particles
 struct CustomDistributionFunctions {
     struct CDF {
