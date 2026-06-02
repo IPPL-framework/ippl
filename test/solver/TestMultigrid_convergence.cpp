1dcd48faf32153d7b2663af37808a816aa8761b2
nschaller <nschaller@student.ethz.ch>
2026-06-02 15:20:01 +0200

    fix include

--- test/solver/TestMultigrid_convergence.cpp
+++ test/solver/TestMultigrid_convergence.cpp
@@ -28,6 +28,8 @@
 #include "Utility/Inform.h"
 #include "Utility/IpplTimings.h"
 
+#include "PoissonSolvers/PoissonCG.h"
+
 int main(int argc, char* argv[]) {
     ippl::initialize(argc, argv);
     {
