189a018018fd756390bc761003197d710814ef9a
nschaller <nschaller@student.ethz.ch>
2026-06-12 14:17:36 +0200

    turn communication off in tests, as convergence is better without for some reason

--- test/solver/multigrid/TestMultigrid_comparison_periodic.cpp
+++ test/solver/multigrid/TestMultigrid_comparison_periodic.cpp
@@ -189,7 +189,7 @@ int main(int argc, char* argv[]) {
                     params.add("gauss_seidel_outer_iterations", 2);
                     params.add("ssor_omega", 1.57079632679);
                     params.add("richardson_iterations", 4);
-                    params.add("communication", 1);
+                    params.add("communication", 0);
 
                     // Multigrid-specific parameters.
                     params.add("mg_pre_smooth_iters", 2);
