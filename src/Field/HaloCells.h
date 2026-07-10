00583172fa8ca0f90176fad008e165e7c50fcaa5
Alexander Liemen <47395086+aliemen@users.noreply.github.com>
2026-07-10 10:46:37 +0200

    Revert "Force no-inline templated exchangeBoundaries. Perhaps the fence only worked because it prevented inlining"
    This reverts commit b1dbe43194d17edf4947848f4266798bc5e9119b.

--- src/Field/HaloCells.h
+++ src/Field/HaloCells.h
@@ -139,9 +139,6 @@ namespace ippl {
              * unpack function call
              */
             template <class Op>
-#if defined(__GNUC__) || defined(__clang__)
-            __attribute__((noinline))
-#endif
             void exchangeBoundaries(view_type& view, Layout_t* layout, SendOrder order, int nghost = 1);
 
             /*!
