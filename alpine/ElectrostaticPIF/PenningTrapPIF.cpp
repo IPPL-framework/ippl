6172fd1e3168507f7767705050bf16b6188bd1ef
Andreas Adelmann <andreas.adelmann@psi.ch>
2026-07-23 21:07:31 +0200

    Reapply "Add Electrostatic PIF Alpine examples"
    This reverts commit 7b8d451ddc4e88e69199084ee1ab43b3df8e0ef0.

---
+++ alpine/ElectrostaticPIF/PenningTrapPIF.cpp
@@ -0,0 +1,419 @@
+// Electrostatic Penning trap test with Particle-in-Fourier schemes
+//   Usage:
+//     srun ./PenningTrapPIF <nx> <ny> <nz> <Np> <Nt> <dt> <ShapeType> <degree> <tol> --info 5
+//     nx       = No. of Fourier modes in the x-direction
+//     ny       = No. of Fourier modes in the y-direction
+//     nz       = No. of Fourier modes in the z-direction
+//     Np       = Total no. of macro-particles in the simulation
+//     Nt       = Number of time steps
+//     dt       = Time stepsize
+//     ShapeType = Shape function type B-spline only for the moment
+//     degree = B-spline degree (-1 for delta function)
+//     tol = tolerance of NUFFT
+//     Example:
+//     srun ./PenningTrapPIF 32 32 32 655360 20 0.05 B-spline 1 1e-4 --info 5
+//
+// Copyright (c) 2023, Sriramkrishnan Muralikrishnan,
+// Jülich Supercomputing Centre, Jülich, Germany.
+// All rights reserved
+//
+// This file is part of IPPL.
+//
+// IPPL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with IPPL. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <Kokkos_Random.hpp>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "Utility/IpplTimings.h"
+
+#include "ChargedParticlesPIF.hpp"
+
+#ifdef ENABLE_CATALYST
+#include "CatalystAdaptor.h"
+#endif
+
+template <typename T>
+struct Newton1D {
+    double tol   = 1e-12;
+    int max_iter = 20;
+    double pi    = Kokkos::numbers::pi_v<double>;
+
+    T mu, sigma, u;
+
+    KOKKOS_INLINE_FUNCTION Newton1D() {}
+
+    KOKKOS_INLINE_FUNCTION Newton1D(const T& mu_, const T& sigma_, const T& u_)
+        : mu(mu_)
+        , sigma(sigma_)
+        , u(u_) {}
+
+    KOKKOS_INLINE_FUNCTION ~Newton1D() {}
+
+    KOKKOS_INLINE_FUNCTION T f(T& x) {
+        T F;
+        F = Kokkos::erf((x - mu) / (sigma * Kokkos::sqrt(2.0))) - 2 * u + 1;
+        return F;
+    }
+
+    KOKKOS_INLINE_FUNCTION T fprime(T& x) {
+        T Fprime;
+        Fprime =
+            (1 / sigma) * Kokkos::sqrt(2 / pi)
+            * Kokkos::exp(-0.5 * (Kokkos::pow(((x - mu) / sigma), 2)));
+        return Fprime;
+    }
+
+    KOKKOS_FUNCTION
+    void solve(T& x) {
+        int iterations = 0;
+        while ((iterations < max_iter) && (Kokkos::fabs(f(x)) > tol)) {
+            x = x - (f(x) / fprime(x));
+            iterations += 1;
+        }
+    }
+};
+
+template <typename T, class GeneratorPool, unsigned Dim>
+struct generate_random {
+    using view_type  = typename ippl::detail::ViewType<T, 1>::view_type;
+    using value_type = typename T::value_type;
+    // Output View for the random numbers
+    view_type x, v;
+
+    // The GeneratorPool
+    GeneratorPool rand_pool;
+
+    T mu, sigma, minU, maxU;
+
+    double pi = Kokkos::numbers::pi_v<double>;
+
+    // Initialize all members
+    generate_random(view_type x_, view_type v_, GeneratorPool rand_pool_, T& mu_, T& sigma_,
+                    T& minU_, T& maxU_)
+        : x(x_)
+        , v(v_)
+        , rand_pool(rand_pool_)
+        , mu(mu_)
+        , sigma(sigma_)
+        , minU(minU_)
+        , maxU(maxU_) {}
+
+    KOKKOS_INLINE_FUNCTION void operator()(const size_t i) const {
+        // Get a random number state from the pool for the active thread
+        typename GeneratorPool::generator_type rand_gen = rand_pool.get_state();
+
+        value_type u;
+        for (unsigned d = 0; d < Dim; ++d) {
+            u       = rand_gen.drand(minU[d], maxU[d]);
+            x(i)[d] = (Kokkos::sqrt(pi / 2) * (2 * u - 1)) * sigma[d] + mu[d];
+            Newton1D<value_type> solver(mu[d], sigma[d], u);
+            solver.solve(x(i)[d]);
+            v(i)[d] = rand_gen.normal(0.0, 1.0);
+        }
+
+        // Give the state back, which will allow another thread to acquire it
+        rand_pool.free_state(rand_gen);
+    }
+};
+
+double CDF(const double& x, const double& mu, const double& sigma) {
+    double cdf = 0.5 * (1.0 + std::erf((x - mu) / (sigma * std::sqrt(2))));
+    return cdf;
+}
+
+const char* TestName = "PenningTrapPIF";
+
+int main(int argc, char* argv[]) {
+    ippl::initialize(argc, argv);
+    {
+#ifdef ENABLE_CATALYST
+        char* script = nullptr;
+        for (int i = 1; i < argc; ++i) {
+            if (std::string(argv[i]) == "--pvscript" && i + 1 < argc) {
+                script = argv[i + 1];
+                i++;
+            }
+        }
+        char* reducedArgv[] = {argv[0], script};
+        CatalystAdaptor::Initialize(2, reducedArgv);
+#endif
+        Inform msg(TestName);
+        Inform msg2all(TestName, INFORM_ALL_NODES);
+
+        ippl::Vector<int, Dim> nr = {std::atoi(argv[1]), std::atoi(argv[2]), std::atoi(argv[3])};
+
+        static IpplTimings::TimerRef mainTimer        = IpplTimings::getTimer("mainTimer");
+        static IpplTimings::TimerRef particleCreation = IpplTimings::getTimer("particlesCreation");
+        static IpplTimings::TimerRef dumpDataTimer    = IpplTimings::getTimer("dumpData");
+        static IpplTimings::TimerRef PTimer           = IpplTimings::getTimer("kick");
+        static IpplTimings::TimerRef RTimer           = IpplTimings::getTimer("drift");
+        static IpplTimings::TimerRef BCTimer          = IpplTimings::getTimer("particleBC");
+        static IpplTimings::TimerRef initializeShapeFunctionPIF =
+            IpplTimings::getTimer("initializeShapeFunctionPIF");
+
+        IpplTimings::startTimer(mainTimer);
+
+        const size_type totalP = std::atoll(argv[4]);
+        const unsigned int nt  = std::atoi(argv[5]);
+        const double dt        = std::atof(argv[6]);
+
+        double factor             = 1.0 / ippl::Comm->size();
+        size_type nloc            = (size_type)(factor * totalP);
+        size_type Total_particles = 0;
+
+        MPI_Allreduce(&nloc, &Total_particles, 1, MPI_UNSIGNED_LONG, MPI_SUM,
+                      ippl::Comm->getCommunicator());
+
+        msg << TestName << endl
+            << "nt " << nt << " Np= " << Total_particles << " Fourier modes = " << nr << endl;
+
+        using bunch_type = ChargedParticlesPIF<PLayout_t>;
+
+        std::shared_ptr<bunch_type> P;
+
+        ippl::NDIndex<Dim> domain;
+        for (unsigned i = 0; i < Dim; i++) {
+            domain[i] = ippl::Index(nr[i]);
+        }
+
+        std::array<bool, Dim> isParallel;  // Specifies SERIAL, PARALLEL dims
+        isParallel.fill(false);
+
+        // create mesh and layout objects for this problem domain
+        Vector_t rmin(0.0);
+        Vector_t rmax(25.0);
+        double dx = rmax[0] / nr[0];
+        double dy = rmax[1] / nr[1];
+        double dz = rmax[2] / nr[2];
+
+        Vector_t length = rmax - rmin;
+
+        Vector_t mu, sd;
+
+        for (unsigned d = 0; d < Dim; d++) {
+            mu[d] = 0.5 * length[d];
+        }
+        // sd[0] = 0.15*length[0];
+        // sd[1] = 0.05*length[1];
+        // sd[2] = 0.20*length[2];
+        sd[0] = 0.10 * 20.0;  // length[0];
+        sd[1] = 0.05 * 20.0;  // length[1];
+        sd[2] = 0.15 * 20.0;  // length[2];
+
+        Vector_t hr     = {dx, dy, dz};
+        Vector_t origin = {rmin[0], rmin[1], rmin[2]};
+
+        Mesh_t mesh(domain, hr, origin);
+        FieldLayout_t FL(*ippl::Comm, domain, isParallel);
+        PLayout_t PL(FL, mesh);
+
+        double Q    = -1562.5;
+        double Bext = 5.0;
+        // P = std::make_unique<bunch_type>(PL,hr,rmin,rmax,decomp,Q,Total_particles);
+        P = std::make_shared<bunch_type>(PL, hr, rmin, rmax, isParallel, Q, Total_particles);
+
+        P->nr_m = nr;
+
+        P->rho_m.initialize(mesh, FL);
+        P->Sk_m.initialize(mesh, FL);
+
+        ////////////////////////////////////////////////////////////
+        // Initialize an FFT object for getting rho in real space and
+        // doing charge conservation check
+
+        ippl::ParameterList fftParams;
+        fftParams.add("use_heffte_defaults", false);
+        fftParams.add("use_pencils", true);
+        fftParams.add("use_reorder", false);
+        fftParams.add("use_gpu_aware", true);
+        fftParams.add("comm", ippl::p2p_pl);
+        fftParams.add("r2c_direction", 0);
+
+        ippl::NDIndex<Dim> domainPIFhalf;
+
+        for (unsigned d = 0; d < Dim; ++d) {
+            domainPIFhalf[d] = ippl::Index(domain[d].length());
+        }
+
+        FieldLayout_t FLPIFhalf(*ippl::Comm, domainPIFhalf, isParallel);
+
+        ippl::Vector<double, 3> hDummy      = {1.0, 1.0, 1.0};
+        ippl::Vector<double, 3> originDummy = {0.0, 0.0, 0.0};
+        Mesh_t meshPIFhalf(domainPIFhalf, hDummy, originDummy);
+
+        ippl::Vector<double, 3> hFourier      = {2 * pi / length[0], 2 * pi / length[1],
+                                                 2 * pi / length[2]};
+        ippl::Vector<double, 3> originFourier = {-pi / hr[0], -pi / hr[1], -pi / hr[2]};
+        Mesh_t meshFourier(domain, hFourier, originFourier);
+
+        P->rhoPIFreal_m.initialize(mesh, FL);
+        P->rhoPIFhalf_m.initialize(meshPIFhalf, FLPIFhalf);
+        P->rhoPIFFourierMag_m.initialize(meshFourier, FL);
+
+        P->time_m = 0.0;
+
+        P->shapetype_m   = argv[7];
+        P->shapedegree_m = std::atoi(argv[8]);
+
+        IpplTimings::startTimer(particleCreation);
+
+        Vector_t minU, maxU;
+        for (unsigned d = 0; d < Dim; ++d) {
+            minU[d] = CDF(rmin[d], mu[d], sd[d]);
+            maxU[d] = CDF(rmax[d], mu[d], sd[d]);
+        }
+
+
+        P->create(nloc);
+        Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42 + 100 * ippl::Comm->rank()));
+        Kokkos::parallel_for(nloc,
+                             generate_random<Vector_t, Kokkos::Random_XorShift64_Pool<>, Dim>(
+                                 P->R.getView(), P->P.getView(), rand_pool64, mu, sd, minU, maxU));
+
+        Kokkos::fence();
+        ippl::Comm->barrier();
+        IpplTimings::stopTimer(particleCreation);
+
+        P->q = P->Q_m / Total_particles;
+        msg << "particles created and initial conditions assigned " << endl;
+
+        IpplTimings::startTimer(initializeShapeFunctionPIF);
+        P->initializeShapeFunctionPIF();
+        IpplTimings::stopTimer(initializeShapeFunctionPIF);
+
+        double tol = std::atof(argv[9]);
+        P->initNUFFT(FL, tol);
+
+        P->scatter();
+
+        P->gather();
+
+        IpplTimings::startTimer(dumpDataTimer);
+        // P->dumpEnergy();
+#ifdef ENABLE_CATALYST
+        P->rhoPIFreal_m = (1 / (hr[0] * hr[1] * hr[2])) * P->rhoPIFreal_m;
+        std::vector<CatalystAdaptor::FieldPair> fields = {
+            {"rhoK", CatalystAdaptor::FieldVariant(&P->rhoPIFFourierMag_m)},
+            {"rhoR", CatalystAdaptor::FieldVariant(&P->rhoPIFreal_m)}};
+        CatalystAdaptor::Execute(0, P->time_m, Ippl::Comm->rank(), P, fields);
+#endif
+        IpplTimings::stopTimer(dumpDataTimer);
+
+        double alpha = -0.5 * dt;
+        double DrInv = 1.0 / (1 + (std::pow((alpha * Bext), 2)));
+        // begin main timestep loop
+        msg << "Starting iterations ..." << endl;
+        for (unsigned int it = 0; it < nt; it++) {
+            // Staggered Leap frog or Boris algorithm as per
+            // https://www.sciencedirect.com/science/article/pii/S2590055219300526
+            // eqns 4(a)-4(c). Note we don't use the Boris trick here and do
+            // the analytical matrix inversion which is not complex in this case.
+            // Here, we assume a constant charge-to-mass ratio of -1 for
+            // all the particles hence eliminating the need to store mass as
+            // an attribute
+            // kick
+            IpplTimings::startTimer(PTimer);
+            auto Rview = P->R.getView();
+            auto Pview = P->P.getView();
+            auto Eview = P->E.getView();
+            double V0  = 30 * rmax[2];
+            Kokkos::parallel_for(
+                "Kick1", P->getLocalNum(), KOKKOS_LAMBDA(const size_t j) {
+                    double Eext_x =
+                        -(Rview(j)[0] - 0.5 * rmax[0]) * (V0 / (2 * Kokkos::pow(rmax[2], 2)));
+                    double Eext_y =
+                        -(Rview(j)[1] - 0.5 * rmax[1]) * (V0 / (2 * Kokkos::pow(rmax[2], 2)));
+                    double Eext_z = (Rview(j)[2] - 0.5 * rmax[2]) * (V0 / (Kokkos::pow(rmax[2], 2)));
+
+                    Eext_x += Eview(j)[0];
+                    Eext_y += Eview(j)[1];
+                    Eext_z += Eview(j)[2];
+
+                    Pview(j)[0] += alpha * (Eext_x + Pview(j)[1] * Bext);
+                    Pview(j)[1] += alpha * (Eext_y - Pview(j)[0] * Bext);
+                    Pview(j)[2] += alpha * Eext_z;
+                });
+            IpplTimings::stopTimer(PTimer);
+
+            // drift
+            IpplTimings::startTimer(RTimer);
+            P->R = P->R + dt * P->P;
+            IpplTimings::stopTimer(RTimer);
+
+            // Apply particle BC
+            IpplTimings::startTimer(BCTimer);
+            PL.applyBC(P->R, PL.getRegionLayout().getDomain());
+            IpplTimings::stopTimer(BCTimer);
+
+            // scatter the charge onto the underlying grid
+            P->scatter();
+
+            // Solve for and gather E field
+            P->gather();
+
+            // kick
+            IpplTimings::startTimer(PTimer);
+            auto R2view = P->R.getView();
+            auto P2view = P->P.getView();
+            auto E2view = P->E.getView();
+            Kokkos::parallel_for(
+                "Kick2", P->getLocalNum(), KOKKOS_LAMBDA(const size_t j) {
+                    double Eext_x =
+                        -(R2view(j)[0] - 0.5 * rmax[0]) * (V0 / (2 * Kokkos::pow(rmax[2], 2)));
+                    double Eext_y =
+                        -(R2view(j)[1] - 0.5 * rmax[1]) * (V0 / (2 * Kokkos::pow(rmax[2], 2)));
+                    double Eext_z = (R2view(j)[2] - 0.5 * rmax[2]) * (V0 / (Kokkos::pow(rmax[2], 2)));
+
+                    Eext_x += E2view(j)[0];
+                    Eext_y += E2view(j)[1];
+                    Eext_z += E2view(j)[2];
+
+                    P2view(j)[0] =
+                        DrInv
+                        * (P2view(j)[0]
+                           + alpha * (Eext_x + P2view(j)[1] * Bext + alpha * Bext * Eext_y));
+                    P2view(j)[1] =
+                        DrInv
+                        * (P2view(j)[1]
+                           + alpha * (Eext_y - P2view(j)[0] * Bext - alpha * Bext * Eext_x));
+                    P2view(j)[2] += alpha * Eext_z;
+                });
+            IpplTimings::stopTimer(PTimer);
+
+            P->time_m += dt;
+            IpplTimings::startTimer(dumpDataTimer);
+            // P->dumpEnergy();
+#ifdef ENABLE_CATALYST
+            P->rhoPIFreal_m = (1 / (hr[0] * hr[1] * hr[2])) * P->rhoPIFreal_m;
+            CatalystAdaptor::Execute(it, P->time_m, Ippl::Comm->rank(), P, fields);
+#endif
+            IpplTimings::stopTimer(dumpDataTimer);
+            msg << "Finished time step: " << it + 1 << " time: " << P->time_m << endl;
+        }
+
+        msg << TestName << " End." << endl;
+
+#ifdef ENABLE_CATALYST
+        CatalystAdaptor::Finalize();
+#endif
+
+        IpplTimings::stopTimer(mainTimer);
+        IpplTimings::print();
+        IpplTimings::print(std::string("timing.dat"));
+    }
+    ippl::finalize();
+    return 0;
+}
